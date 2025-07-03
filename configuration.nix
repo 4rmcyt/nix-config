c4ce556006facb5e06120a7bf74d2fe81b945938
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-02 21:09:08 -0600

    feat: Enable SSH service with root login and password authentication

--- configuration.nix
+++ configuration.nix
@@ -1,66 +1,69 @@
-{
-  description = "NixOS configuration for homeserver";
+{ config, pkgs, inputs, ... }:
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
-    sops-nix.url = "github:Mic92/sops-nix";
-    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nix-index-database.url = "github:nix-community/nix-index-database";
-    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
-    vscode-server.url = "github:nix-community/nixos-vscode-server";
-    nix4nvchad.url = "github:nix-community/nix4nvchad";
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Enable SSH - ADDED
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "yes";  # Allow root login during setup
+      PasswordAuthentication = true;
+      PubkeyAuthentication = true;
+    };
+    openFirewall = true;
   };
 
-  outputs = { self, nixpkgs, disko, sops-nix, home-manager, nix-index-database, vscode-server, nix4nvchad, ... }@inputs: {
-    nixosConfigurations.homeserver = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      
-      modules = [
-        # Core configuration files
-        ./configuration.nix
-        ./disko.nix
-        ./networking.nix
-        
-        # Centralized services
-        ./services/database.nix
-        ./services/nextdns.nix
+  # Enable VSCode Server
+  services.vscode-server.enable = true;
 
-        # Basic services (no secrets required)
-        ./services/caddy.nix
-        ./services/fail2ban.nix
-        ./services/homepage.nix
-        ./services/jellyfin.nix
-        ./services/samba.nix
-        
-        # TEMPORARILY COMMENTED OUT - Services that require secrets
-        # ./services/audiobookshelf.nix
-        # ./services/cloudflared.nix
-        # ./services/deluge-vpn.nix
-        # ./services/home-assistant.nix
-        # ./services/keycloak.nix
-        # ./services/media-content.nix
-        # ./services/microbin.nix              
-        # ./services/nextcloud.nix
-        # ./services/radicale.nix
-        # ./services/tailscale.nix
-        # ./services/tg-notify.nix
-        # ./services/yubikey.nix
-        
-        # Scripts
-        # ./scripts/keycloak-yubikey-setup.nix
-        
-        # Enable modules from flake inputs
-        disko.nixosModules.disko
-        sops-nix.nixosModules.sops
-        home-manager.nixosModules.home-manager
-        nix-index-database.nixosModules.nix-index
-        vscode-server.nixosModules.default
-      ];
+  # User configuration
+  users.users.zeev = {
+    isNormalUser = true;
+    description = "Zeev";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    # Temporarily use a plain password until SOPS is working
+    password = "temppassword123";  # Change this after fixing SOPS
+    shell = pkgs.bash;
+    createHome = true;
+    home = "/home/zeev";
+  };
+
+  # System packages
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    wget
+    curl
+    htop
+    tmux
+    age
+    sops
+  ];
+
+  # Enable Home Manager with inputs passed
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      zeev = import ./home.nix;
     };
   };
+
+  # TEMPORARILY DISABLE SOPS until we fix the encryption
+  # sops.defaultSopsFile = ./secrets.yaml;
+  # sops.defaultSopsFormat = "yaml";
+  # sops.age.keyFile = "/home/zeev/.config/sops/age/keys.txt";
+  # sops.age.generateKey = true;
+  # sops.secrets.zeev_password = {
+  #   neededForUsers = true;
+  # };
+
+  # System configuration
+  system.stateVersion = "25.05";
 }
