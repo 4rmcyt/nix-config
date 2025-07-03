39eff88e8296a93c1f1ce32205c7a6e75e6db23a
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-03 15:00:08 -0600

    feat(deluge-vpn): Add Deluge service configuration with VPN routing support

--- configuration.nix
+++ configuration.nix
@@ -60,5 +60,54 @@
     };
   };
 
+  # Deluge BitTorrent with VPN routing
+  services.deluge = {
+    enable = true;
+    web.enable = true;
+    web.port = 8112;
+    declarative = true;
+    config = {
+      daemon_port = 58846;
+      listen_ports = [6881 6891];
+      random_port = false;
+      outgoing_ports = [0 0];
+      allow_remote = true;
+      download_location = "/var/lib/deluge/Downloads";
+      torrentfiles_location = "/var/lib/deluge/Torrents";
+    };
+    authFile = pkgs.writeText "deluge-auth" ''
+      localclient::10
+      admin:password123:10
+    '';
+    openFirewall = false;
+  };
+
+  # Override systemd services for VPN routing
+  systemd.services.deluged = {
+    serviceConfig = {
+      Type = lib.mkForce "simple";
+      ExecStart = lib.mkForce "${pkgs.iproute2}/bin/ip netns exec pia ${pkgs.deluge}/bin/deluged --do-not-daemonize -c /var/lib/deluge/.config/deluge -l /var/lib/deluge/daemon.log -L info";
+      PIDFile = lib.mkForce null;
+      Restart = "always";
+      RestartSec = "5";
+      TimeoutStartSec = "30";
+    };
+    wants = ["deluge-vpn-routing.service"];
+    after = ["deluge-vpn-routing.service" "network.target"];
+  };
+
+  systemd.services.deluge-web = {
+    serviceConfig = {
+      Type = lib.mkForce "simple";
+      ExecStart = lib.mkForce "${pkgs.deluge}/bin/deluge-web --do-not-daemonize -c /var/lib/deluge/.config/deluge -l /var/lib/deluge/web.log -L info";
+      PIDFile = lib.mkForce null;
+      Restart = "always";
+      RestartSec = "5";
+      TimeoutStartSec = "30";
+    };
+    wants = ["deluged.service"];
+    after = ["deluged.service" "network.target"];
+  };
+
   system.stateVersion = "25.05";
 }
