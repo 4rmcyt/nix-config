7e8b2d13e35bc38a90e12267a3bd816545fdbf9f
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-04 12:36:55 -0600

    fix(caddy): Refactor extraConfig for clarity and remove unused HTTP configurations

--- services/caddy.nix
+++ services/caddy.nix
@@ -3,34 +3,40 @@
 {
   services.caddy = {
     enable = true;
-
+    
     globalConfig = ''
       auto_https off
     '';
-
+    
     extraConfig = ''
-      # Homepage ONLY on 80/443
-      http://homeserver.local:80, http://192.168.1.165:80 {
-        root * /var/www/homepage
-        file_server
-      }
-
-      # Microbin ONLY on 8083
-      http://localhost:8083 {
-        reverse_proxy localhost:8083
-      }
-
-      # Jellyfin ONLY on 8096
-      http://localhost:8096 {
-        reverse_proxy localhost:8096
-      }
-
-      # Home Assistant ONLY on 8123
-      http://localhost:8123 {
-        reverse_proxy localhost:8123
+      # Main homepage       
+        # Microbin
+        handle_path /microbin* {
+          reverse_proxy localhost:8083
+        }
+        
+        # Home Assistant
+        handle_path /hass* {
+          reverse_proxy localhost:8123
+        }
+        
+        # Homepage
+        handle_path /homepage* {
+          reverse_proxy localhost:8082
+        }
+        
+        # Jellyfin  
+        handle_path /jellyfin* {
+          reverse_proxy localhost:8096
+        }
+        
+        # Default to homepage
+        handle {
+          reverse_proxy localhost:80
+        }
       }
     '';
   };
-
-  networking.firewall.allowedTCPPorts = [ 80 443 8083 8096 8123 ];
+  
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
 }
