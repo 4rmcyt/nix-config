73a451f05afa843765b6d1726cb0791dfd410129
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-04 13:47:08 -0600

    Add custom Keycloak theme styles and properties
    - Created a new CSS file for custom styles, hiding the header and setting a background image.
    - Added theme properties to link the new CSS file and set viewport meta tag.

--- services/keycloak.nix
+++ services/keycloak.nix
@@ -1,19 +1,18 @@
 { config, pkgs, ... }:
 
 {
+  # SOPS secrets
   sops.secrets.keycloak_db_password = {
     owner = "root";
     group = "root";
     mode = "0400";
   };
-
-  sops.secrets.keycloak_default_user = {
+  sops.secrets.keycloak_admin_user = {
     owner = "root";
     group = "root";
     mode = "0400";
   };
-
-  sops.secrets.keycloak_default_password = {
+  sops.secrets.keycloak_admin_password = {
     owner = "root";
     group = "root";
     mode = "0400";
@@ -23,7 +22,7 @@
     enable = true;
     settings = {
       hostname = "keycloak.example.com";
-      http-host = "0.0.0.0"; # TODO: Fix it after reverse proxy will be ready
+      http-host = "0.0.0.0";
       http-port = 8080;
       hostname-strict-https = false;
       proxy-headers = "xforwarded";
@@ -43,10 +42,30 @@
       passwordFile = config.sops.secrets.keycloak_db_password.path;
     };
 
-    initialAdminUser = config.sops.secrets.keycloak_default_user.path;
-    initialAdminPasswordFile = config.sops.secrets.keycloak_default_password.path;
-  };
+    # Only include these if your NixOS version supports them!
+    initialAdminUser = builtins.readFile config.sops.secrets.keycloak_admin_user.path;
+    initialAdminPasswordFile = config.sops.secrets.keycloak_admin_password.path;
 
+    # Add your custom theme (replace ./keycloak-theme with your theme path)
+    themes = [
+      (pkgs.stdenv.mkDerivation {
+        name = "keycloak-theme";
+        src = ./keycloak-theme;
+        installPhase = ''
+          mkdir -p $out
+          cp -r * $out/
+        '';
+      })
+    ];
+
+    # Add trusted device provider (update sha256 after first build error)
+    extraProviders = [
+      (pkgs.fetchurl {
+        url = "https://github.com/wouterh-dev/keycloak-spi-trusted-device/releases/download/v0.0.2/keycloak-spi-trusted-device-0.0.2.jar";
+        sha256 = "e84f6e3f5b7ce4f33115b7080fb1671d91e00c629ed80ae7d97d8c1c9af62dcf"; # <-- replace with real hash after first build
+      })
+    ];
+  };
 
   networking.firewall.allowedTCPPorts = [ 8080 ];
 }
