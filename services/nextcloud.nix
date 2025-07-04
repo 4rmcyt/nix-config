a4c68d2066c9d1f4257be93f7ad1797e06074a76
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-04 10:45:31 -0600

    fix(nextcloud): Remove unused webserver configuration for Nextcloud service

--- services/nextcloud.nix
+++ services/nextcloud.nix
@@ -28,7 +28,6 @@
   
   # SOPS secrets for Nextcloud
   sops.secrets.nextcloud_admin_password = {};
-  services.nextcloud.webserver = "";
   # Open firewall for Nextcloud
   networking.firewall.allowedTCPPorts = [ 8081 ];
 }
