25dd6e69b1c97dd746943f6585d4522c3b0e7ff5
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-04 13:10:14 -0600

    fix(nextcloud): Clean up configuration by removing commented-out sections and unnecessary nginx settings

--- services/nextcloud.nix
+++ services/nextcloud.nix
@@ -4,10 +4,7 @@
   services.nextcloud = {
     enable = true;
     package = pkgs.nextcloud31;
-
     hostName = "192.168.1.165";
-
-    # Use local database with trust authentication
     database.createLocally = true;
     config = {
       dbtype = "pgsql";
@@ -16,23 +13,13 @@
       adminpassFile = config.sops.secrets.nextcloud_admin_password.path;
       adminuser = "admin";
     };
-
-    # System settings
     settings = {
       overwriteprotocol = "http";
       trusted_domains = [ "192.168.1.165" "homeserver.local" ];
       trusted_proxies = [ "127.0.0.1" ];
     };
-
-    nginx = {
-      # Listen on 8081 instead of 80/443
-      listen = [
-        { addr = "127.0.0.1"; port = 8081; ssl = false; }
-      ];
-    };
   };
 
-  # Enable nginx for Nextcloud
   services.nginx.enable = true;
 
   services.nginx.virtualHosts."nextcloud.local" = {
@@ -56,9 +43,6 @@
     '';
   };
 
-  # SOPS secrets for Nextcloud
   sops.secrets.nextcloud_admin_password = {};
-
-  # Open firewall for Nextcloud and nginx
   networking.firewall.allowedTCPPorts = [ 80 443 8081 ];
 }
