e5bfb2d1913408f7d76dc63979b337ecf3fe107f
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-04 14:13:42 -0600

    fix(keycloak): Update theme.nix to clarify theme files directory and remove unused code

--- services/theme.nix
+++ services/theme.nix
@@ -2,15 +2,9 @@
 
 stdenv.mkDerivation {
   name = "zeev-theme";
-  src = ./keycloak-theme; # directory with your theme
+  src = ./keycloak-theme; # directory with your theme files
   installPhase = ''
     mkdir -p $out
     cp -r * $out/
   '';
 }
-
-(final: prev: {
-  custom_keycloak_themes = {
-    zeev = prev.callPackage ./theme.nix { };
-  };
-})
