78be0c98ce5839015b0ebdc36262f56eeecd1d28
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-04 14:00:30 -0600

    fix(keycloak): Remove initial admin user configuration and streamline Keycloak setup with theme and plugin packaging

--- services/keycloak.nix
+++ services/keycloak.nix
@@ -7,21 +7,47 @@
     group = "root";
     mode = "0400";
   };
-  sops.secrets.keycloak_admin_user = {
-    owner = "root";
-    group = "root";
-    mode = "0400";
-  };
   sops.secrets.keycloak_admin_password = {
     owner = "root";
     group = "root";
     mode = "0400";
   };
 
+  # Package the trusted device plugin as a derivation (like notthebee)
+  keycloak_trusted_device_plugin = pkgs.stdenv.mkDerivation {
+    name = "keycloak-spi-trusted-device";
+    src = pkgs.fetchurl {
+      url = "https://github.com/wouterh-dev/keycloak-spi-trusted-device/releases/download/v0.0.2/keycloak-spi-trusted-device-0.0.2.jar";
+      sha256 = "e84f6e3f5b7ce4f33115b7080fb1671d91e00c629ed80ae7d97d8c1c9af62dcf";
+    };
+    installPhase = ''
+      mkdir -p $out
+      cp $src $out/
+    '';
+  };
+
+  # Package your theme as a derivation (like notthebee)
+  keycloak_theme = pkgs.stdenv.mkDerivation {
+    name = "keycloak-theme";
+    src = ./keycloak-theme;
+    installPhase = ''
+      mkdir -p $out
+      cp -r * $out/
+    '';
+  };
+
   services.keycloak = {
     enable = true;
-    initialAdminUser = "admin";
-    initialAdminPassword = builtins.readFile config.sops.secrets.keycloak_admin_password.path;
+    initialAdminPassword = config.sops.secrets.keycloak_admin_password.path;
+    database = {
+      createLocally = false;
+      host = "localhost";
+      port = 5432;
+      username = "keycloak";
+      passwordFile = config.sops.secrets.keycloak_db_password.path;
+    };
+    themes = [ keycloak_theme ];
+    plugins = [ keycloak_trusted_device_plugin ];
     settings = {
       hostname = "keycloak.labhome.work";
       http-host = "0.0.0.0";
@@ -33,36 +59,8 @@
       log-level = "INFO";
       log-console-output = "default";
       http-enabled = true;
+      # Add any other settings you need
     };
-
-    database = {
-      createLocally = false;
-      host = "localhost";
-      port = 5432;
-      username = "keycloak";
-      passwordFile = config.sops.secrets.keycloak_db_password.path;
-    };
-
-    # Add your custom theme (replace ./keycloak-theme with your theme path)
-    themes = [
-      (pkgs.stdenv.mkDerivation {
-        name = "keycloak-theme";
-        src = ./keycloak-theme;
-        installPhase = ''
-          mkdir -p $out
-          cp -r * $out/
-        '';
-      })
-    ];
-
-    # Add trusted device and other plugins
-    plugins = [
-      # Trusted Device SPI
-      (pkgs.fetchurl {
-        url = "https://github.com/wouterh-dev/keycloak-spi-trusted-device/releases/download/v0.0.2/keycloak-spi-trusted-device-0.0.2.jar";
-        sha256 = "e84f6e3f5b7ce4f33115b7080fb1671d91e00c629ed80ae7d97d8c1c9af62dcf";
-      })
-    ];
   };
 
   networking.firewall.allowedTCPPorts = [ 8080 ];
