eca684f29eeeff79c02766681aa0525cc4e1d5be
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-04 09:51:31 -0600

    fix(cloudflared): Clean up whitespace and improve formatting in Cloudflare Tunnel configuration

--- services/cloudflared.nix
+++ services/cloudflared.nix
@@ -6,7 +6,7 @@
     group = "cloudflared";
     mode = "0400";
   };
-  
+
   users.users.cloudflared = {
     isSystemUser = true;
     group = "cloudflared";
@@ -15,46 +15,46 @@
 
   users.groups.cloudflared = {};
 
-  # Cloudflare Tunnel configuration with direct routing
   systemd.services.cloudflared = {
-  description = "Cloudflare Tunnel";
-  after = [ "network.target" "systemd-tmpfiles-setup.service" ];
-  requires = [ "systemd-tmpfiles-setup.service" ];
-  wantedBy = [ "multi-user.target" ];
-  serviceConfig = {
-    ExecStart = "${pkgs.cloudflared}/bin/cloudflared tunnel --no-autoupdate run --token-file ${config.sops.secrets.cloudflare_tunnel_token.path}";
-    Restart = "always";
-    RestartSec = "5s";
-    User = "cloudflared";
-    Group = "cloudflared";
+    description = "Cloudflare Tunnel";
+    after = [ "network.target" "systemd-tmpfiles-setup.service" ];
+    requires = [ "systemd-tmpfiles-setup.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.cloudflared}/bin/cloudflared tunnel --no-autoupdate run --token-file ${config.sops.secrets.cloudflare_tunnel_token.path}";
+      Restart = "always";
+      RestartSec = "5s";
+      User = "cloudflared";
+      Group = "cloudflared";
+    };
+    preStart = ''
+      cat > /var/lib/cloudflared/config.yml << EOF
+      tunnel: $(cat ${config.sops.secrets.cloudflare_tunnel_token.path})
+
+      ingress:
+        - hostname: nextcloud.labhome.work
+          service: http://localhost:8081
+        - hostname: keycloak.labhome.work
+          service: http://localhost:8080
+        - hostname: jellyfin.labhome.work
+          service: http://localhost:8096
+        - hostname: paperless.labhome.work
+          service: http://localhost:8082
+        - hostname: home.labhome.work
+          service: http://localhost:8123
+        - hostname: rss.labhome.work
+          service: http://localhost:8083
+        - hostname: deluge.labhome.work
+          service: http://localhost:8112
+        - hostname: cal.labhome.work
+          service: http://localhost:5232
+        - hostname: audiobookshelf.labhome.work
+          service: http://localhost:8085
+        - hostname: paste.labhome.work
+          service: http://localhost:8087
+        - service: http_status:404
+
+      EOF
+    '';
   };
-  preStart = ''
-    cat > /var/lib/cloudflared/config.yml << EOF
-    tunnel: $(cat ${config.sops.secrets.cloudflare_tunnel_token.path})
-
-    ingress:
-      - hostname: nextcloud.labhome.work
-        service: http://localhost:8081
-      - hostname: keycloak.labhome.work
-        service: http://localhost:8080
-      - hostname: jellyfin.labhome.work
-        service: http://localhost:8096
-      - hostname: paperless.labhome.work
-        service: http://localhost:8082
-      - hostname: home.labhome.work
-        service: http://localhost:8123
-      - hostname: rss.labhome.work
-        service: http://localhost:8083
-      - hostname: deluge.labhome.work
-        service: http://localhost:8112
-      - hostname: cal.labhome.work
-        service: http://localhost:5232
-      - hostname: audiobookshelf.labhome.work
-        service: http://localhost:8085
-      - hostname: paste.labhome.work
-        service: http://localhost:8087
-      - service: http_status:404
-
-    EOF
-  '';
-};
+}
