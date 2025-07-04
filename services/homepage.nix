6a8c5104f264dc884d7b3b1c39c3abbb70adc12d
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-04 09:35:05 -0600

    fix(homepage): Update service configurations and add new services

--- services/homepage.nix
+++ services/homepage.nix
@@ -16,15 +16,43 @@
               icon = "jellyfin";
             };
           }
+          {
+            "Audiobookshelf" = {
+              href = "http://192.168.1.165:8085";
+              description = "Audiobook & Podcast Server";
+              icon = "audiobookshelf";
+            };
+          }
         ];
       }
       {
         "Storage" = [
           {
             "Nextcloud" = {
-              href = "http://192.168.1.165:80/nextcloud";
+              href = "http://192.168.1.165:8081";
               description = "File Storage & Collaboration";
               icon = "nextcloud";
+            };
+          }
+          {
+            "Paperless" = {
+              href = "http://192.168.1.165:8888";
+              description = "Document Management";
+              icon = "paperless-ngx";
+            };
+          }
+          {
+            "Simple File Server" = {
+              href = "http://192.168.1.165:8084";
+              description = "Static File Server";
+              icon = "folder";
+            };
+          }
+          {
+            "Samba" = {
+              href = "smb://192.168.1.165";
+              description = "Network File Share";
+              icon = "samba";
             };
           }
         ];
@@ -33,9 +61,23 @@
         "Tools" = [
           {
             "Microbin" = {
-              href = "http://192.168.1.165:80/microbin";
+              href = "http://192.168.1.165:8083";
               description = "Pastebin Service";
               icon = "microbin";
+            };
+          }
+          {
+            "Miniflux" = {
+              href = "http://192.168.1.165:8086";
+              description = "RSS Reader";
+              icon = "rss";
+            };
+          }
+          {
+            "Radicale" = {
+              href = "http://192.168.1.165:5232";
+              description = "Calendar & Contacts";
+              icon = "calendar";
             };
           }
         ];
@@ -51,21 +93,37 @@
           }
         ];
       }
-    ];
-    
-    widgets = [
       {
-        resources = {
-          cpu = true;
-          memory = true;
-          disk = "/";
-        };
-      }
-      {
-        search = {
-          provider = "duckduckgo";
-          target = "_blank";
-        };
+        "Networking" = [
+          {
+            "Keycloak" = {
+              href = "http://192.168.1.165:8080";
+              description = "Identity & Access Management";
+              icon = "keycloak";
+            };
+          }
+          {
+            "Caddy" = {
+              href = "http://192.168.1.165:2019";
+              description = "Reverse Proxy Admin";
+              icon = "caddy";
+            };
+          }
+          {
+            "Tailscale" = {
+              href = "https://login.tailscale.com/admin/machines";
+              description = "Mesh VPN";
+              icon = "tailscale";
+            };
+          }
+          {
+            "Fail2ban" = {
+              href = "http://192.168.1.165:8082/fail2ban"; # adjust if you have a UI
+              description = "Ban List";
+              icon = "shield";
+            };
+          }
+        ];
       }
     ];
   };
