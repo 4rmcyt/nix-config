e085598fbe673585e4958cb88086e668a1235c56
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-04 13:02:45 -0600

    fix(nextcloud): Clean up whitespace and remove commented-out nginx user/group configuration

--- services/nextcloud.nix
+++ services/nextcloud.nix
@@ -4,9 +4,9 @@
   services.nextcloud = {
     enable = true;
     package = pkgs.nextcloud31;
-    
+
     hostName = "192.168.1.165";
-    
+
     # Use local database with trust authentication
     database.createLocally = true;
     config = {
@@ -16,23 +16,21 @@
       adminpassFile = config.sops.secrets.nextcloud_admin_password.path;
       adminuser = "admin";
     };
-    
+
     # System settings
     settings = {
       overwriteprotocol = "http";
       trusted_domains = [ "192.168.1.165" "homeserver.local" ];
       trusted_proxies = [ "127.0.0.1" ];
     };
-    
-    # phpfpmPoolConfig = {
-    #   user = "nginx";
-    #   group = "nginx";
-    # };
   };
-  
+
+  # Enable nginx for Nextcloud
+  services.nginx.enable = true;
+
   # SOPS secrets for Nextcloud
   sops.secrets.nextcloud_admin_password = {};
-  
-  # Open firewall for Nextcloud
-  networking.firewall.allowedTCPPorts = [ 8081 ];
+
+  # Open firewall for Nextcloud and nginx
+  networking.firewall.allowedTCPPorts = [ 80 443 8081 ];
 }
