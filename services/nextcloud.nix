34e7b9e89d0412d16fccc6cf872df71a551b9b8b
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-04 11:29:31 -0600

    fix(nextcloud): Refactor Nextcloud configuration for clarity and consistency

--- services/nextcloud.nix
+++ services/nextcloud.nix
@@ -4,30 +4,38 @@
   services.nextcloud = {
     enable = true;
     package = pkgs.nextcloud31;
-    
     hostName = "nextcloud.example.com";
-    
-    # Use local database with trust authentication
+
     database.createLocally = true;
     config = {
       dbtype = "pgsql";
       dbuser = "nextcloud";
       dbname = "nextcloud";
-      
-      adminpassFile = config.sops.secrets.nextcloud_admin_password.path;
       adminuser = "admin";
+      adminpassFile = config.sops.secrets.nextcloud_admin_password.path;
     };
-    
-    # System settings
+
     settings = {
-      overwriteprotocol = "http";
-      trusted_domains = [ "nextcloud.example.com" "192.168.1.165" "homeserver.local" ];
+      overwriteprotocol = "https"; # Use "https" if behind Cloudflare Tunnel
+      trusted_domains = [
+        "nextcloud.example.com"
+        "192.168.1.165"
+        "homeserver.local"
+      ];
       trusted_proxies = [ "127.0.0.1" ];
     };
   };
-  
-  # SOPS secrets for Nextcloud
+
   sops.secrets.nextcloud_admin_password = {};
-  # Open firewall for Nextcloud
+
   networking.firewall.allowedTCPPorts = [ 8081 ];
+
+  services.fail2ban-cloudflare = lib.mkIf config.services.fail2ban-cloudflare.enable {
+    jails = {
+      nextcloud = {
+        serviceName = "phpfpm-nextcloud"; # Confirm this matches your PHP-FPM service
+        failRegex = "^.*Login failed:.*(Remote IP: <HOST>).*$";
+      };
+    };
+  };
 }
