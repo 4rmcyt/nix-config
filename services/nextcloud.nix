c2fce9a695eb65a45017721666e75f103184dc55
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-04 13:12:10 -0600

    fix(nextcloud): Update hostname and trusted domains for Nextcloud configuration

--- services/nextcloud.nix
+++ services/nextcloud.nix
@@ -4,7 +4,7 @@
   services.nextcloud = {
     enable = true;
     package = pkgs.nextcloud31;
-    hostName = "192.168.1.165";
+    hostName = "nextcloud.local"; # or your public domain
     database.createLocally = true;
     config = {
       dbtype = "pgsql";
@@ -15,32 +15,19 @@
     };
     settings = {
       overwriteprotocol = "http";
-      trusted_domains = [ "192.168.1.165" "homeserver.local" ];
+      trusted_domains = [ "nextcloud.local" "192.168.1.165" "homeserver.local" ];
       trusted_proxies = [ "127.0.0.1" ];
     };
   };
 
   services.nginx.enable = true;
 
+  # Override the default vhost to listen only on localhost:8081
   services.nginx.virtualHosts."nextcloud.local" = {
     listen = [
       { addr = "127.0.0.1"; port = 8081; ssl = false; }
     ];
-    root = "/var/lib/nextcloud";
-    locations."/" = {
-      proxyPass = "http://unix:/run/nextcloud/php-fpm.sock";
-      extraConfig = ''
-        include ${pkgs.nginx}/conf/fastcgi_params;
-        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-        fastcgi_param PATH_INFO $fastcgi_path_info;
-        fastcgi_param HTTPS off;
-        fastcgi_pass unix:/run/nextcloud/php-fpm.sock;
-        fastcgi_index index.php;
-      '';
-    };
-    extraConfig = ''
-      client_max_body_size 512M;
-    '';
+    # No need to set root/locations, Nextcloud module will do it
   };
 
   sops.secrets.nextcloud_admin_password = {};
