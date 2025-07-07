dd5f47a1df609aca7f343d2d910200be3e3f0b2d
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-07 15:47:42 -0600

    fix(miniflux): remove adminCredentialsFile and streamline configuration using environment variables

--- services/miniflux.nix
+++ services/miniflux.nix
@@ -1,33 +1,38 @@
 { config, pkgs, ... }:
 
-let
-
-  minifluxCredentials = pkgs.writeText "miniflux-credentials" ''
-    admin:$(cat ${config.sops.secrets.miniflux_admin_password.path})
-  '';
-in
 {
   sops.secrets.miniflux_admin_password = {};
 
   services.miniflux = {
     enable = true;
-      adminCredentialsFile = minifluxCredentials;
-      config = {
-        BASE_URL = "https://miniflux.labhome.work";
-        CREATE_ADMIN = "1";
-        LISTEN_ADDR = "127.0.0.1:8086";
-        OAUTH2_PROVIDER = "oidc";
-        OAUTH2_CLIENT_ID = "miniflux";
-        OAUTH2_REDIRECT_URL = "https://miniflux.labhome.work/oauth2/oidc/callback";
-        OAUTH2_OIDC_DISCOVERY_ENDPOINT = "https://keycloak.labhome.work/realms/master";
-        OAUTH2_USER_CREATION = "1";
-        DISABLE_LOCAL_AUTH = "true";
-      };
+    # Remove adminCredentialsFile if you're using ADMIN_USERNAME and ADMIN_PASSWORD directly.
+    # adminCredentialsFile = minifluxCredentials; # <-- Remove this line
+
+    config = {
+      BASE_URL = "https://miniflux.labhome.work";
+      CREATE_ADMIN = "1";
+      LISTEN_ADDR = "127.0.0.1:8086";
+      OAUTH2_PROVIDER = "oidc";
+      OAUTH2_CLIENT_ID = "miniflux";
+      OAUTH2_REDIRECT_URL = "https://miniflux.labhome.work/oauth2/oidc/callback";
+      OAUTH2_OIDC_DISCOVERY_ENDPOINT = "https://keycloak.labhome.work/realms/master";
+      OAUTH2_USER_CREATION = "1";
+      DISABLE_LOCAL_AUTH = "true";
+    };
+
+    # Add these explicitly
+    environmentVariables = {
+      ADMIN_USERNAME = "admin"; # Explicitly set the admin username
+      ADMIN_PASSWORD = "${config.sops.secrets.miniflux_admin_password.path}"; # Path to the decrypted password
+      # Miniflux will read the content of this file as the password.
+    };
   };
-    # Ensure the credentials file is created with the correct permissions
+
+  # The tmpfiles rule for 'miniflux-credentials' is no longer needed if you remove adminCredentialsFile
+  # and use environmentVariables.
   systemd.tmpfiles.rules = [
     "d /var/lib/miniflux 0755 miniflux miniflux - -"
-    "f ${minifluxCredentials} 0640 miniflux miniflux -"
+    # "f ${minifluxCredentials} 0640 miniflux miniflux -" # <-- Remove this line
   ];
 
   # Ensure the user and group exist
@@ -38,5 +43,4 @@ in
   };
 
   users.groups.miniflux = {};
-
 }
