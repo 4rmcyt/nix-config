4eda1702dafad7077276a0c9f54082523d0fee82
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-07 08:30:29 -0600

    refactor(transmission-vpn): streamline PIA credentials handling by using SOPS-encrypted file

--- services/transmission-vpn.nix
+++ services/transmission-vpn.nix
@@ -22,20 +22,12 @@ let
     ${pkgs.sudo}/bin/sudo -u transmission ${pkgs.transmission}/bin/transmission-remote --peerport "$PORT"
   '';
 
-  # This creates a credentials file in the format required by nix-pia-vpn
-  # by combining your existing sops secrets for username and password.
-  pia-credentials-file = pkgs.writeText "pia-credentials" ''
-    PIA_USER=$(cat ${config.sops.secrets.pia_username.path})
-    PIA_PASS=$(cat ${config.sops.secrets.pia_password.path})
-  '';
-
 in
 {
   # == 1. PIA VPN Configuration ==
   # This section leverages the nix-pia-vpn flake to manage the connection.
   imports = [
     # Import the module from the flake input.
-    # Make sure you have 'nix-pia-vpn' defined in your flake.nix inputs.
     inputs.nix-pia-vpn.nixosModules.default
   ];
 
@@ -47,8 +39,11 @@ in
     # ensuring all its traffic is routed through the VPN.
     user = "transmission";
 
-    # Point to the credentials file we just generated declaratively.
-    credentialsFile = pia-credentials-file;
+    # --- CORRECTED OPTION ---
+    # Point directly to your SOPS-encrypted credentials file.
+    # The module handles decryption automatically.
+    # The path is relative to this .nix file.
+    sopsFile = ../secrets/pia_credentials.txt;
 
     # Specify your desired server location.
     # A list of locations can be found here: https://github.com/rcambrj/nix-pia-vpn
@@ -58,50 +53,37 @@ in
     onPortForward = update-transmission-port-script;
   };
 
-  # Define permissions for your existing SOPS secrets.
-  # This ensures the 'transmission' user can read them to create the credentials file.
-  sops.secrets.pia_username = {
-    owner = config.services.pia-vpn.user;
-    group = config.users.users.transmission.group;
-  };
-  sops.secrets.pia_password = {
-    owner = config.services.pia-vpn.user;
-    group = config.users.users.transmission.group;
-  };
+  # The sops.secrets block for pia_credentials is no longer needed here,
+  # as the nix-pia-vpn module manages the secret itself via the sopsFile option.
 
 
   # == 2. Transmission Service Configuration ==
   services.transmission = {
     enable = true;
-    # Run the service as the 'transmission' user. Because this user is in the
-    # 'pia-vpn' group, its traffic is automatically firewalled and routed.
+    # Run the service as the 'transmission' user.
     user = "transmission";
     group = "transmission";
 
     # For security, only allow access to the Web UI from the local machine.
-    # Use a reverse proxy (like Caddy or Nginx) to expose it securely.
     rpc-bind-address = "127.0.0.1";
 
     # Set the default download directory.
     download-dir = "/var/lib/transmission/downloads";
 
-    # Initial port setting. This will be immediately updated by the VPN hook
-    # once a connection is established.
-    peer-port = 51413; # Default, will be changed by hook.
+    # Initial port setting. This will be immediately updated by the VPN hook.
+    peer-port = 51413;
   };
 
   # Create the 'transmission' user and group.
   users.users.transmission = {
     isSystemUser = true;
     group = "transmission";
-    home = "/var/lib/transmission"; # Home dir for config files.
+    home = "/var/lib/transmission";
   };
   users.groups.transmission = {};
 
 
   # == 3. Firewall Configuration ==
-  # This allows you to access the Transmission Web UI (port 9091)
-  # from other devices on your local network.
-  # NOTE: This does NOT expose the port to the internet.
+  # Allows access to the Transmission Web UI (port 9091) from your local network.
   networking.firewall.allowedTCPPorts = [ 9091 ];
 }
