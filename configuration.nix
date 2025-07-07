1b40ee0b5e7bec3851942ee59fba1a51ab42de54
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-07 10:54:09 -0600

    fix(sops): correct pia_credentials path to use age configuration

--- configuration.nix
+++ configuration.nix
@@ -49,7 +49,7 @@
   sops.defaultSopsFile = ./secrets.yaml;
   sops.defaultSopsFormat = "yaml";
   sops.age.keyFile = "/home/zeev/.config/sops/age/keys.txt";
-  sops.pia_credentials = "./secrets/pia_credentials.txt";
+  sops.age.pia_credentials = "./secrets/pia_credentials.txt";
 
 
   # CLEANED: Only central secrets (removed service-specific duplicates)
