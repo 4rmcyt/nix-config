5fd189ef25c816184d87ff69398680ab13a98edd
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-07 08:16:37 -0600

    refactor(vpn): replace Deluge service with Transmission service and update related configurations

---
+++ services/transmission-vpn.nix
@@ -0,0 +1,98 @@
+# /etc/nixos/services/transmission-vpn.nix
+
+{ config, pkgs, ... }:
+
+let
+  # This script is automatically executed by the nix-pia-vpn module whenever
+  # PIA assigns a new forwarded port. The port number is passed as the first
+  # argument ($1).
+  update-transmission-port-script = pkgs.writeShellScript "update-transmission-port.sh" ''
+    #!${pkgs.runtimeShell}
+    
+    # The new port provided by PIA
+    PORT="$1"
+
+    # Log the event to the system journal for easy debugging.
+    # You can view these logs with: journalctl -t update-transmission-port
+    echo "Received new PIA port: $PORT. Updating Transmission." | ${pkgs.systemd}/bin/systemd-cat -t update-transmission-port
+
+    # Use the transmission-remote tool to instantly update the running daemon.
+    # This is more efficient than restarting the whole service.
+    # We run this as the 'transmission' user.
+    ${pkgs.sudo}/bin/sudo -u transmission ${pkgs.transmission}/bin/transmission-remote --peerport "$PORT"
+  '';
+
+in
+{
+  # == 1. PIA VPN Configuration ==
+  # This section leverages the nix-pia-vpn flake to manage the connection.
+  imports = [
+    # Import the module from the flake input.
+    # Make sure you have 'nix-pia-vpn' defined in your flake.nix inputs.
+    inputs.nix-pia-vpn.nixosModules.default
+  ];
+
+  # Enable the PIA VPN service.
+  services.pia-vpn = {
+    enable = true;
+    # The user that the Transmission service will run as.
+    # This user will be automatically added to the 'pia-vpn' group,
+    # ensuring all its traffic is routed through the VPN.
+    user = "transmission";
+
+    # Credentials for PIA. These are securely managed by sops.
+    credentialsFile = config.sops.secrets.pia_credentials.path;
+
+    # Specify your desired server location.
+    # A list of locations can be found here: https://github.com/rcambrj/nix-pia-vpn
+    location = "ca_ontario";
+
+    # Hook to automatically update Transmission's port.
+    onPortForward = update-transmission-port-script;
+  };
+
+  # Define the sops secret containing your PIA username and password.
+  # The file should contain:
+  # PIA_USER=p1234567
+  # PIA_PASS=YourPassword
+  sops.secrets.pia_credentials = {
+    owner = config.services.pia-vpn.user; # Allow the vpn user to read it.
+    group = config.users.users.transmission.group;
+  };
+
+
+  # == 2. Transmission Service Configuration ==
+  services.transmission = {
+    enable = true;
+    # Run the service as the 'transmission' user. Because this user is in the
+    # 'pia-vpn' group, its traffic is automatically firewalled and routed.
+    user = "transmission";
+    group = "transmission";
+
+    # For security, only allow access to the Web UI from the local machine.
+    # Use a reverse proxy (like Caddy or Nginx) to expose it securely.
+    rpc-bind-address = "127.0.0.1";
+
+    # Set the default download directory.
+    download-dir = "/var/lib/transmission/downloads";
+
+    # Initial port setting. This will be immediately updated by the VPN hook
+    # once a connection is established.
+    peer-port = 51413; # Default, will be changed by hook.
+  };
+
+  # Create the 'transmission' user and group.
+  users.users.transmission = {
+    isSystemUser = true;
+    group = "transmission";
+    home = "/var/lib/transmission"; # Home dir for config files.
+  };
+  users.groups.transmission = {};
+
+
+  # == 3. Firewall Configuration ==
+  # This allows you to access the Transmission Web UI (port 9091)
+  # from other devices on your local network.
+  # NOTE: This does NOT expose the port to the internet.
+  networking.firewall.allowedTCPPorts = [ 9091 ];
+}
