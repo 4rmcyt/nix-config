df5df3b2ef6cf949055212587f808558f6eb5c23
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-07 23:01:57 -0600

    Disable tg-notify service for clarity and future reference

--- configuration.nix
+++ configuration.nix
@@ -169,7 +169,7 @@
   # Enable VSCode server
   services.vscode-server.enable = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  services.tg-notify.enable = true;
+  services.tg-notify.enable = false;
 
   system.stateVersion = "25.05";
 }
