6ca1cced83df8e578af2998c413358e25a0bfd79
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-07 20:19:50 -0600

    Update Transmission RPC whitelist to allow broader IP range for VPN access

--- configuration.nix
+++ configuration.nix
@@ -40,8 +40,8 @@
     settings = {
       "download-dir" = "/home/zeev/Downloads";
       "rpc-bind-address" = "0.0.0.0";
-      "rpc-whitelist-enabled" = "false"; # Disable whitelist to allow access through VPN
-      "rpc-whitelist" = "127.0.0.1,192.168.1.0/24,100.64.0.0/10,localhost";
+      "rpc-whitelist-enabled" = "false"; 
+      "rpc-whitelist" = "127.0.0.1,192.168.1.*,100.64.0.*,localhost";
     };
     vpn.enable = true;
   };
