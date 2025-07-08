22f981c0b8b6a6fa80f5c21920597db53fff2da9
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-08 12:22:51 -0600

    Refactor Transmission configuration to remove unused bind address setting and streamline systemd service dependencies for improved clarity and functionality.

--- services/transmission.nix
+++ services/transmission.nix
@@ -11,16 +11,14 @@ in
   };
 
   config = mkIf (cfg.enable && cfg.vpn.enable) {
-    services.transmission.settings."bind-address-ipv4" = mkForce null;
-
-    # Keep the essential systemd settings for service order and the network kill-switch.
-    systemd.services.transmission = {
-      after = [ "pia-vpn.service" ];
-      wantedBy = [ "pia-vpn.service" ];
-      serviceConfig.BindToDevice = "wg0";
-    };
-
-    # User group permissions.
     users.users.${cfg.user}.extraGroups = [ "pia-vpn" "media" ];
+    systemd.services.transmission-daemon.bindsTo = [ "pia-vpn.service" ];
+    systemd.services.transmission-daemon.after = [ "pia-vpn.service" ];
+
+
+    systemd.services.pia-vpn-portforward.path = [
+      pkgs.transmission_4
+      pkgs.systemd # for systemd-cat
+    ];
   };
 }
