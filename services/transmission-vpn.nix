f3750c9b35dffad0cad6c6519eb4a20e67fc808e
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-09 03:40:23 -0600

    Fix bind address in startTransmission script to use interface instead of IP

--- services/transmission-vpn.nix
+++ services/transmission-vpn.nix
@@ -6,10 +6,9 @@ let
   
   startTransmission = pkgs.writeScript "start-transmission" ''
     #!${pkgs.stdenv.shell}
-    IP=$(${pkgs.iproute2}/bin/ip addr show dev ${piaInterface} | ${pkgs.gnugrep}/bin/grep "inet" | ${pkgs.gawk}/bin/awk '{print $2}')
     ${pkgs.transmission_4}/bin/transmission-daemon -f \
     -g "${config.services.transmission.home}/.config/transmission-daemon" \
-    --bind-address-ipv4 $IP
+    --bind-address-interface ${piaInterface}
   '';
 
 in
