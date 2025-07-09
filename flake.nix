a2e3016d52688398f79bc30163c7f00f85b76d5b
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-09 12:04:43 -0600

    Add Nixarr service configuration with VPN support and transmission settings

--- flake.nix
+++ flake.nix
@@ -54,6 +54,7 @@
         ./services/audiobookshelf.nix
         ./services/miniflux.nix
         # ./services/kavita.nix
+        ./services/nixarr.nix
 
         # Productivity & Personal services
         ./services/nextcloud.nix
