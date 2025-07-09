cbef058d4433b52ed4b2a8d742461643381d6270
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-09 12:19:28 -0600

    Update nixarr input to use the 'dev' branch in flake.nix

--- flake.nix
+++ flake.nix
@@ -3,17 +3,25 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
+    
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+    
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+    
     vscode-server.url = "github:nix-community/nixos-vscode-server";
+    
     nix4nvchad.url = "github:nix-community/nix4nvchad";
-    nixarr.url = "github:rasmus-kirk/nixarr";
+
+    nixarr.url = "github:rasmus-kirk/nixarr/dev";
+    nixarr.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, disko, sops-nix, home-manager, nix-index-database, vscode-server, nix4nvchad, nixarr, ... }@inputs: {
