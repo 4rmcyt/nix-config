e945e6572afca9d8d5a2b8782d66d14d709b9af3
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-09 12:15:52 -0600

    Comment out unused media service configurations in configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -51,13 +51,13 @@
       };
     };
 
-    bazarr.enable = true;
-    lidarr.enable = true;
-    prowlarr.enable = true;
-    radarr.enable = true;
-    readarr.enable = true;
-    sonarr.enable = true;
-    jellyseerr.enable = true;
+    # bazarr.enable = true;
+    # lidarr.enable = true;
+    # prowlarr.enable = true;
+    # radarr.enable = true;
+    # readarr.enable = true;
+    # sonarr.enable = true;
+    # jellyseerr.enable = true;
   };
   
 
