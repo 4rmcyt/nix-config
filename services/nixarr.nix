bad760c9ac6260251eae6d26c6ed81ecf7e65480
4rmcyt <4rmcyt@gmail>
2025-07-10 15:12:02 -0600

    refactor: streamline Nixarr configuration structure and fix wgConf assignment

--- services/nixarr.nix
+++ services/nixarr.nix
@@ -1,42 +1,40 @@
 # In ~/src/server/services/nixarr.nix
-{ config, pkgs, ... }:
-
-{
-  # The entire configuration is now correctly placed under 'services.nixarr'
-  services.nixarr = {
+{ config, pkgs, ... }: {
+  nixarr = {
     enable = true;
-    group = "media"; # This option will now be found correctly
     mediaUsers = [ "zeev" ];
     mediaDir = "/home/zeev/media";
     stateDir = "/home/zeev/media/.state/nixarr";
 
+    # It is possible for this module to run the *Arrs through a VPN, but it
+    # is generally not recommended, as it can cause rate-limiting issues.
     vpn = {
       enable = true;
-      # This should be managed by sops-nix for security
-      # wgConfFile = config.sops.secrets.nixarr_wg_conf.path;
-      wgConfFile = "/home/zeev/src/wg.conf";
+      wgConf = "/home/zeev/src/wg.conf";
     };
+
 
     transmission = {
-      enable = true;
-      vpn.enable = true;
-      peerPort = 63998;
-      extraSettings = {
-        "download-dir" = "/home/zeev/Downloads";
-        "script-torrent-added-enabled" = true;
-        "script-torrent-added-filename" = "/etc/nixos/scripts/add-trackers.sh";
-        "blocklist-enabled" = true;
-        "blocklist-url" = "https://raw.githubusercontent.com/Naunter/BT_BlockLists/master/bt_blocklists.gz";
+        enable = true;
+        vpn.enable = true;
+        peerPort = 63998;
+        extraSettings = {
+          download-dir = "/home/zeev/Downloads";
+          script-torrent-added-enabled = true;
+          script-torrent-added-filename = "/etc/nixos/scripts/add-trackers.sh";
+          blocklist-enabled = true;
+          blocklist-url = "https://raw.githubusercontent.com/Naunter/BT_BlockLists/master/bt_blocklists.gz";
+        };
       };
-    };
-
+     
     sabnzbd = {
       enable = true;
       vpn.enable = true;
+      #openFirewall = true;
     };
 
     audiobookshelf.enable = true;
-    jellyfin.enable = true;
+    jellyfin.enable = true;  
     bazarr.enable = true;
     lidarr.enable = true;
     prowlarr.enable = true;
