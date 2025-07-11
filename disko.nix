7197d79d83b2f69959f9aef3f8de6666cfc4afa4
4rmcyt <4rmcyt@gmail>
2025-07-11 13:42:10 -0600

    refactor: simplify disk configuration and streamline partition definitions in disko

--- disko.nix
+++ disko.nix
@@ -2,78 +2,45 @@
 
 {
   disko.devices = {
-    # This 'disk' attribute set is required for your version
     disk = {
-      # The fast NVMe SSD for the OS (/dev/nvme0n1)
       nvme = {
         device = "/dev/disk/by-id/nvme-SAMSUNG_MZVLW256HEHP-000L7_S35ENX0K543315";
         type = "disk";
         content = {
           type = "gpt";
           partitions = {
-            boot = {
-              size = "1G";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
-            };
+            boot = { size = "1G"; type = "EF00"; content = { type = "filesystem"; format = "vfat"; mountpoint = "/boot"; }; };
             root = {
               size = "100%";
               content = {
                 type = "btrfs";
                 extraArgs = [ "-f" ];
                 subvolumes = {
-                  "/@" = {
-                    mountpoint = "/";
-                    mountOptions = [ "compress=zstd" "noatime" ];
-                  };
-                  "/@nix" = {
-                    mountpoint = "/nix";
-                    mountOptions = [ "compress=zstd" "noatime" ];
-                  };
-                  "/@log" = {
-                    mountpoint = "/var/log";
-                    mountOptions = [ "compress=zstd" "noatime" ];
-                  };
-                  "/@swap" = {
-                    mountpoint = "/.swapvol";
-                    mountOptions = [ "noatime" ];
-                  };
+                  "/@root" = { mountpoint = "/"; mountOptions = [ "compress=zstd" "noatime" ]; };
+                  "/@home" = { mountpoint = "/home"; mountOptions = [ "compress=zstd" "noatime" ]; };
+                  "/@nix" = { mountpoint = "/nix"; mountOptions = [ "compress=zstd" "noatime" ]; };
+                  "/@log" = { mountpoint = "/var/log"; mountOptions = [ "compress=zstd" "noatime" ]; };
+                  "/@swap" = { mountpoint = "/.swapvol"; mountOptions = [ "noatime" ]; };
                 };
               };
             };
           };
         };
       };
-
-      # The larger SATA SSD for home directories (/dev/sda)
+      
       sata = {
         device = "/dev/disk/by-id/ata-Patriot_P210_1024GB_P210EDCB23011109345";
         type = "disk";
         content = {
           type = "gpt";
           partitions = {
-            home = {
+            data = {
               size = "100%";
               content = {
                 type = "btrfs";
                 extraArgs = [ "-f" ];
                 subvolumes = {
-                  "/@home" = {
-                    mountpoint = "/home";
-                    mountOptions = [ "compress=zstd" "compress=zstd" "noatime" ];
-                  };
-                  "/@media" = {
-                    mountpoint = "/home/zeev/media";
-                    mountOptions = [ "compress=zstd" "noatime" ];
-                  };
-                  "/@downloads" = {
-                    mountpoint = "/home/zeev/downloads";
-                    mountOptions = [ "compress=zstd" "noatime" ];
-                  };
+                  "/@data" = { mountpoint = "/data"; mountOptions = [ "compress=zstd" "noatime" ]; };
                 };
               };
             };
@@ -83,15 +50,5 @@
     };
   };
 
-  # The rest of your configuration is correct
-  swapDevices = [
-    {
-      device = "/.swapvol/swapfile";
-      size = 16384;
-    }
-  ];
-
-  fileSystems."/" = { options = [ "compress=zstd" "noatime" ]; };
-  fileSystems."/nix" = { options = [ "compress=zstd" "noatime" ]; };
-  fileSystems."/home" = { options = [ "compress=zstd" "noatime" ]; };
+  swapDevices = [{ device = "/.swapvol/swapfile"; size = 16384; }];
 }
