a043551ed57d79ce203e8ce1bd58278131a35c13
4rmcyt <4rmcyt@gmail>
2025-07-11 11:24:27 -0600

    refactor: remove unused extraPackages in home-assistant configuration

--- services/home-assistant.nix
+++ services/home-assistant.nix
@@ -21,7 +21,7 @@
       configWritable = true;
       # This block has been rewritten to be more explicit and correct
       extraPackages = ps: [
-        psycopg
+        python3.13-psycopg
         ps.flatdict
         ps.pyatv
         ps.getmac
