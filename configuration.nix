bb9ca9f223ad712f3149b39528344bc714c608f7
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-14 08:09:41 -0600

    fix: add GPG recipient and streamline SOPS secrets configuration

--- configuration.nix
+++ configuration.nix
@@ -90,25 +90,18 @@
 
   sops = {
     defaultSopsFile = ./secrets.yaml;
+    gnupg.recipients = [
+      "REDACTED"
+    ];
     secrets = {
       zeev_password.neededForUsers = true;
+      sh_host_ed25519_key = { owner = "root"; group = "root"; mode = "0600"; };
+      ssh_host_rsa_key = { owner = "root"; group = "root"; mode = "0600"; };
       nextcloud_admin_password = {};
       microbin_admin_password = {};
       tailscale_auth_key = {};
       telegram_bot_token = {};
       telegram_chat_id = {};
-      
-      # Add these definitions for your host keys
-      "ssh_host_ed25519_key" = {
-        owner = "root";
-        group = "root";
-        mode = "0600";
-      };
-      "ssh_host_rsa_key" = {
-        owner = "root";
-        group = "root";
-        mode = "0600";
-      };
     };
   };
 
