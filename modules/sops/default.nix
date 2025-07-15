422d65c6153a24441964084db25b6689d323ff12
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-15 11:42:02 -0600

    refactor: remove unused homepage_keycloak_key and homepage_radicale_key from secrets_backup.yaml

--- modules/sops/default.nix
+++ modules/sops/default.nix
@@ -58,9 +58,7 @@
       "homepage_miniflux_key"           = { owner = "homepage-dashboard"; };
       "homepage_nextcloud_key"          = { owner = "homepage-dashboard"; };
       "homepage_microbin_key"           = { owner = "homepage-dashboard"; };
-      "homepage_keycloak_key"           = { owner = "homepage-dashboard"; };
       "homepage_hass_key"               = { owner = "homepage-dashboard"; };
-      "homepage_radicale_key"           = { owner = "homepage-dashboard"; };
       "homepage_kavita_key"             = { owner = "homepage-dashboard"; };
       "homepage_cloudflared_key"        = { owner = "homepage-dashboard"; };
       "homepage_cloudflared_account_id" = { owner = "homepage-dashboard"; };
