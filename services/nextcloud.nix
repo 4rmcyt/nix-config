b8999d9fb6d8ba05cb13a239be5263d12faf6d2e
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-16 16:51:49 -0600

    fix: update Nextcloud configuration for improved domain handling and package version

--- services/nextcloud.nix
+++ services/nextcloud.nix
@@ -3,31 +3,25 @@
 {
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud28;
-    hostName = "nextcloud.example.com";
-    
-    # Let the module handle the nginx configuration
-    nginx.enable = true;
-    https = true; # Let the module handle SSL via ACME
-
+    package = pkgs.nextcloud31;
+    hostName = "nextcloud.local";
     database.createLocally = false;
     config = {
       dbtype = "pgsql";
-      dbhost = "localhost";
+      dbhost = "localhost";       
       dbuser = "nextcloud";
       dbname = "nextcloud";
-      dbpassFile = config.sops.secrets.nextcloud_db_password.path;
+      dbpassFile = config.sops.secrets.nextcloud_db_password.path;      
       adminpassFile = config.sops.secrets.nextcloud_admin_password.path;
       adminuser = "admin";
     };
     settings = {
-      overwriteprotocol = "https";
-      trusted_domains = [ "nextcloud.example.com" ];
-      trusted_proxies = [ "127.0.0.1" "::1" ];
+      overwriteprotocol = "http";
+      trusted_domains = [ "nextcloud.local" "192.168.1.165" "nextcloud.example.com" ];
+      trusted_proxies = [ "127.0.0.1" ];
     };
   };
 
-  # This ensures the Nextcloud setup process waits for the database
   systemd.services.nextcloud-setup = {
     after = [ "postgresql.service" ];
     requires = [ "postgresql.service" ];
@@ -38,4 +32,14 @@
     after = [ "postgresql.service" ];
     requires = [ "postgresql.service" ];
   };
-}
+
+  services.nginx.enable = true;
+  
+
+  services.nginx.virtualHosts."nextcloud.local" = {
+    listen = [
+      { addr = "127.0.0.1"; port = 8081; ssl = false; }
+    ];
+  };
+
+} 
