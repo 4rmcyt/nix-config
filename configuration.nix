9a1bda4b734355a22773f5e455bf09ea5bd5f322
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-17 16:10:28 -0600

    fix: enable auto-optimise-store in Nix settings for improved performance

--- configuration.nix
+++ configuration.nix
@@ -19,6 +19,7 @@
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
       warn-dirty = false;
+      auto-optimise-store = true;
       cores = 0;
       show-trace = true;
       download-buffer-size = 1073741824; # 1 GiB
