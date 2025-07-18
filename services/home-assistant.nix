d1350fd7e9e3693535e5f44d77e18c7a61e69768
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-18 15:16:03 -0600

    fix: remove db_url from recorder configuration for security

--- services/home-assistant.nix
+++ services/home-assistant.nix
@@ -53,7 +53,6 @@
         };
 
         recorder = {
-          db_url = "postgresql://hass:${config.sops.secrets.database_passwords.hass_db_password}@localhost/hass";
           include = {
             domains = [
               "switch"
