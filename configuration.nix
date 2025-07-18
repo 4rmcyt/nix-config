87ba8ca5d4eb1d7b02ed23d722df802af017f929
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-18 10:46:30 -0600

    fix: update nextdns widget configuration to include report-client-info argument

--- configuration.nix
+++ configuration.nix
@@ -75,7 +75,7 @@
 
     nextdns = {
       enable = true;
-      arguments = [ "-config" "nextdns0" "-cache-size" "10MB" ];
+      arguments = [ "-config" "nextdns0" "-cache-size" "10MB" "--report-client-info" ];
     };
     vscode-server.enable = true;
   };
@@ -100,7 +100,7 @@
     after = [ "nextdns.service" ];
     wantedBy = [ "multi-user.target" ];
   };
-  
+
   nixpkgs.config.allowUnfree = true;
   system.stateVersion = "25.05";
 }
