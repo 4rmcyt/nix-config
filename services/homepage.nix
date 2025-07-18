deef1375539e0a0659c91ce735b724663c8aa427
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-18 09:59:32 -0600

    fix: update Cloudflare widget configuration to use unquoted credentials

--- services/homepage.nix
+++ services/homepage.nix
@@ -301,9 +301,9 @@
               icon = "cloudflare-zero-trust";
               widget = {
                 type = "cloudflared"; 
-                accountId = "8239dd1bb0d0bfedf13673a195df59cf";
-                tunnelId = "f7876e26-87a8-4bdd-9798-3986b0f7cebc";
-                key = "bRmxobo3pZ56Ulrd7UL3RC5DNDsnVnVn8gGmFtLn";
+                accountId = 8239dd1bb0d0bfedf13673a195df59cf;
+                tunnelId = f7876e26-87a8-4bdd-9798-3986b0f7cebc;
+                key = bRmxobo3pZ56Ulrd7UL3RC5DNDsnVnVn8gGmFtLn;
               };
             };
           }
