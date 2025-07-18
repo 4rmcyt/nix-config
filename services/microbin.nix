ebbf2fe92bc174e694d10b9cf6227ce7e25fc6ec
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-18 16:09:00 -0600

    fix: move environment file configuration to systemd service settings

--- services/microbin.nix
+++ services/microbin.nix
@@ -1,16 +1,24 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
   services.microbin = {
     enable = true;
     listenAddr = "0.0.0.0";
     port = 8084;
-    environmentFile = config.sops.secrets.microbin_secrets.path;
 
     settings = {
       admin_username = "admin";
       private = true;
       no_listing = true;
+    };
+
+    systemd.services.microbin.serviceConfig = {
+      EnvironmentFile = config.sops.secrets.microbin_secrets.path;
     };
   };
 }
