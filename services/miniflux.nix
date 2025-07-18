cfb79296fae34be1958efc51bc00c42c9440ec00
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-18 15:01:34 -0600

    fix: update DATABASE_URL in miniflux configuration to reference the correct password from the database_passwords secret group

--- services/miniflux.nix
+++ services/miniflux.nix
@@ -3,11 +3,14 @@
 {
   services.miniflux = {
     enable = true;
+    # This correctly points to the file containing the admin user/pass.
     adminCredentialsFile = config.sops.secrets.miniflux_secrets.path;
 
     config = {
       BASE_URL = "https://rss.labhome.work";
-      DATABASE_URL = "postgres://miniflux:${config.sops.secrets.miniflux_db_password.path}@/miniflux";
+      # This is the corrected line. It now correctly gets the password
+      # value from within your 'database_passwords' secret group.
+      DATABASE_URL = "postgres://miniflux:${config.sops.secrets.database_passwords.miniflux_db_password}@/miniflux";
       PORT = "8086";
       RUN_AS_USER = "miniflux";
     };
