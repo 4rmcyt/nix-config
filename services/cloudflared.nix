1acedc68781edc346f33acfe8ce8156d1f9ee40f
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-17 20:33:51 -0600

    fix: update cloudflared service configuration for ingress syntax consistency

--- services/cloudflared.nix
+++ services/cloudflared.nix
@@ -4,22 +4,22 @@ services.cloudflared = {
     enable = true;
     tunnels = {
       "f7876e26-87a8-4bdd-9798-3986b0f7cebc" = {
-        credentials-file = ${config.sops.secrets.cloudflare_tunnel_credentials.path};
+        credentials-file = config.sops.secrets.cloudflare_tunnel_credentials.path;
         default = "http_status:404";
         ingress = {
-          "nextcloud.example.com" = { service = "http://localhost:8081"; };
-          "keycloak.example.com" = { service = "http://localhost:8080"; };
-          "jellyfin.example.com" = { service = "http://localhost:8096"; };
-          "paperless.example.com" = { service = "http://localhost:8888"; };
-          "home.example.com" = { service = "http://localhost:8082"; };
-          "rss.example.com" = { service = "http://localhost:8086"; };
-          "transmission.example.com" = { service = "http://localhost:9091"; };
-          "cal.example.com" = { service = "http://localhost:5232"; };
-          "audiobookshelf.example.com" = { service = "http://localhost:8085"; };
-          "paste.example.com" = { service = "http://localhost:8083"; };
-          "kavita.example.com" = { service = "http://localhost:5000"; };
-          "microbin.example.com" = { service = "http://localhost:8084"; };
-          "hass.example.com" = { service = "http://localhost:8123"; };
+          "nextcloud.example.com" =  "http://localhost:8081";
+          "keycloak.example.com" =  "http://localhost:8080";
+          "jellyfin.example.com" =  "http://localhost:8096";
+          "paperless.example.com" =  "http://localhost:8888";
+          "home.example.com" =  "http://localhost:8082";
+          "rss.example.com" =  "http://localhost:8086";
+          "transmission.example.com" =  "http://localhost:9091";
+          "cal.example.com" =  "http://localhost:5232";
+          "audiobookshelf.example.com" =  "http://localhost:8085";
+          "paste.example.com" =  "http://localhost:8083";
+          "kavita.example.com" =  "http://localhost:5000";
+          "microbin.example.com" =  "http://localhost:8084";
+          "hass.example.com" =  "http://localhost:8123";
           default = "http_status:404";
         };
       };
