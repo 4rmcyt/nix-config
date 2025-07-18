b63c7fc4f0244adc16451b4d3ffff42ded686bfb
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-17 20:31:00 -0600

    fix: update ingress configuration syntax for cloudflared service

--- services/cloudflared.nix
+++ services/cloudflared.nix
@@ -7,20 +7,20 @@ services.cloudflared = {
         credentials-file = "${config.sops.secrets.cloudflare_tunnel_credentials.path}";
         default = "http_status:404";
         ingress = {
-          { hostname = "nextcloud.labhome.work";      service = "http://localhost:8081"; }
-          { hostname = "keycloak.labhome.work";       service = "http://localhost:8080"; }
-          { hostname = "jellyfin.labhome.work";       service = "http://localhost:8096"; }
-          { hostname = "paperless.labhome.work";      service = "http://localhost:8888"; }
-          { hostname = "home.labhome.work";           service = "http://localhost:8082"; }
-          { hostname = "rss.labhome.work";            service = "http://localhost:8086"; }
-          { hostname = "transmission.labhome.work";   service = "http://localhost:9091"; }
-          { hostname = "cal.labhome.work";            service = "http://localhost:5232"; }
-          { hostname = "audiobookshelf.labhome.work"; service = "http://localhost:8085"; }
-          { hostname = "paste.labhome.work";          service = "http://localhost:8083"; }
-          { hostname = "kavita.labhome.work";         service = "http://localhost:5000"; }
-          { hostname = "microbin.labhome.work";       service = "http://localhost:8084"; }
-          { hostname = "hass.labhome.work";           service = "http://localhost:8123"; }
-          { default = "http_status:404"; }
+          "nextcloud.labhome.work" = { service = "http://localhost:8081"; };
+          "keycloak.labhome.work" = { service = "http://localhost:8080"; };
+          "jellyfin.labhome.work" = { service = "http://localhost:8096"; };
+          "paperless.labhome.work" = { service = "http://localhost:8888"; };
+          "home.labhome.work" = { service = "http://localhost:8082"; };
+          "rss.labhome.work" = { service = "http://localhost:8086"; };
+          "transmission.labhome.work" = { service = "http://localhost:9091"; };
+          "cal.labhome.work" = { service = "http://localhost:5232"; };
+          "audiobookshelf.labhome.work" = { service = "http://localhost:8085"; };
+          "paste.labhome.work" = { service = "http://localhost:8083"; };
+          "kavita.labhome.work" = { service = "http://localhost:5000"; };
+          "microbin.labhome.work" = { service = "http://localhost:8084"; };
+          "hass.labhome.work" = { service = "http://localhost:8123"; };
+          default = "http_status:404";
         };
       };
     };
