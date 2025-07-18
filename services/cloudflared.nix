9a4aec99a9519a1fe7c78b2559d48d509632c327
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-17 20:14:00 -0600

    fix: streamline ingress configuration for cloudflared service

--- services/cloudflared.nix
+++ services/cloudflared.nix
@@ -1,65 +1,28 @@
 { config, pkgs, ... }:
 {
-  services.cloudflared = {
+services.cloudflared = {
     enable = true;
+    # You MUST add this config block
     config = {
       tunnel = "f7876e26-87a8-4bdd-9798-3986b0f7cebc";
       "credentials-file" = config.sops.secrets.cloudflare_tunnel_credentials.path;
+      
 
       ingress = [
-        {
-          hostname = "nextcloud.labhome.work";
-          service = "http://localhost:8081";
-        }
-        {
-          hostname = "keycloak.labhome.work";
-          service = "http://localhost:8080";
-        }
-        {
-          hostname = "jellyfin.labhome.work";
-          service = "http://localhost:8096";
-        }
-        {
-          hostname = "paperless.labhome.work";
-          service = "http://localhost:8888";
-        }
-        {
-          hostname = "home.labhome.work";
-          service = "http://localhost:8082";
-        }
-        {
-          hostname = "rss.labhome.work";
-          service = "http://localhost:8086";
-        }
-        {
-          hostname = "transmission.labhome.work";
-          service = "http://localhost:9091";
-        }
-        {
-          hostname = "cal.labhome.work";
-          service = "http://localhost:5232";
-        }
-        {
-          hostname = "audiobookshelf.labhome.work";
-          service = "http://localhost:8085";
-        }
-        {
-          hostname = "paste.labhome.work";
-          service = "http://localhost:8083";
-        }
-        {
-          hostname = "kavita.labhome.work";
-          service = "http://localhost:5000";
-        }
-        {
-          hostname = "microbin.labhome.work";
-          service = "http://localhost:8084";
-        }
-        {
-          hostname = "hass.labhome.work";
-          service = "http://localhost:8123";
-        }
-        { service = "http_status:404"; }
+          { hostname = "nextcloud.labhome.work";      service = "http://localhost:8081"; }
+          { hostname = "keycloak.labhome.work";       service = "http://localhost:8080"; }
+          { hostname = "jellyfin.labhome.work";       service = "http://localhost:8096"; }
+          { hostname = "paperless.labhome.work";      service = "http://localhost:8888"; }
+          { hostname = "home.labhome.work";           service = "http://localhost:8082"; }
+          { hostname = "rss.labhome.work";            service = "http://localhost:8086"; }
+          { hostname = "transmission.labhome.work";   service = "http://localhost:9091"; }
+          { hostname = "cal.labhome.work";            service = "http://localhost:5232"; }
+          { hostname = "audiobookshelf.labhome.work"; service = "http://localhost:8085"; }
+          { hostname = "paste.labhome.work";          service = "http://localhost:8083"; }
+          { hostname = "kavita.labhome.work";         service = "http://localhost:5000"; }
+          { hostname = "microbin.labhome.work";       service = "http://localhost:8084"; }
+          { hostname = "hass.labhome.work";           service = "http://localhost:8123"; }
+          { service = "http_status:404"; }
       ];
     };
   };
