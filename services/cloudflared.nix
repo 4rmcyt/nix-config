b6d14b5101c056224eddeae548c68b96e71c1d0a
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-07-18 08:54:29 -0600

    fix: reorganize cloudflared service configuration for consistency

--- services/cloudflared.nix
+++ services/cloudflared.nix
@@ -1,24 +1,26 @@
 { config, pkgs, ... }:
 {
-  services.cloudflared.tunnels."f7876e26f7876e26-87a8-4bdd-9798-3986b0f7cebc" = {
-    enable = true;
-    credentialsFile = config.sops.secrets.cloudflare_tunnel_credentials.path;
-    default = "http_status:404";
-    ingress = {
-      "nextcloud.example.com" = "http://localhost:8081";
-      "keycloak.example.com" = "http://localhost:8080";
-      "jellyfin.example.com" = "http://localhost:8096";
-      "paperless.example.com" = "http://localhost:8888";
-      "home.example.com" = "http://localhost:8082";
-      "rss.example.com" = "http://localhost:8086";
-      "hass.example.com" = "http://localhost:8123";
-      "miniflux.example.com" = "http://localhost:8086";
-      "transmission.example.com" = "http://localhost:9091";
-      "cal.example.com" = "http://localhost:5232";
-      "audiobookshelf.example.com" = "http://localhost:8085";
-      "paste.example.com" = "http://localhost:8083";
-      "kavita.example.com" = "http://localhost:5000";
-      "microbin.example.com" = "http://localhost:8084";
+  services.cloudflared.tunnels = {
+    "f7876e26f7876e26-87a8-4bdd-9798-3986b0f7cebc" = {
+      enable = true;
+      credentialsFile = config.sops.secrets.cloudflare_tunnel_credentials.path;
+      default = "http_status:404";
+      ingress = {
+        "nextcloud.example.com" = "http://localhost:8081";
+        "keycloak.example.com" = "http://localhost:8080";
+        "jellyfin.example.com" = "http://localhost:8096";
+        "paperless.example.com" = "http://localhost:8888";
+        "home.example.com" = "http://localhost:8082";
+        "rss.example.com" = "http://localhost:8086";
+        "hass.example.com" = "http://localhost:8123";
+        "miniflux.example.com" = "http://localhost:8086";
+        "transmission.example.com" = "http://localhost:9091";
+        "cal.example.com" = "http://localhost:5232";
+        "audiobookshelf.example.com" = "http://localhost:8085";
+        "paste.example.com" = "http://localhost:8083";
+        "kavita.example.com" = "http://localhost:5000";
+        "microbin.example.com" = "http://localhost:8084";
+      };
     };
   };
 }
