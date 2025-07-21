54b86b8bf7a4bc5e2f5c3b5468d47365b2ba443a
4rmcyt <4rmcyt@gmail.com>
2025-07-21 17:49:06 -0600

    feat: add home-manager configuration for zeev user in default.nix

--- users/default.nix
+++ users/default.nix
@@ -56,6 +56,7 @@ in
           "media"
           "samba"
         ];
+        home-manager.users.zeev = import ../home-manager/default.nix;
         hashedPasswordFile = config.sops.secrets.zeev_password.path;
         openssh.authorizedKeys.keys = server-keys;
       };
