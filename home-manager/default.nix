a8b5e87044fad85cd9b861922daf9ebcd19b2de3
4rmcyt <4rmcyt@gmail.com>
2025-07-21 17:38:13 -0600

    Implement code changes to enhance functionality and improve performance

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,36 +1,39 @@
-{ pkgs, inputs, config, ... }:
+# This function MUST accept 'config' to use sops secrets
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+
 {
   home.username = "zeev";
   home.homeDirectory = "/home/zeev";
+
   home.packages = with pkgs; [
     git
     nixfmt-rfc-style
-    gnupg 
+    gnupg
   ];
- 
+
   imports = [
     ./dots/nvim/default.nix
   ];
-
 
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
   };
 
-  home.activation.import-gpg-key = pkgs.lib.hm.dag.entryAfter ["writeBoundary"] ''
-    # The GPG key is imported from the path provided by sops-nix
-    $DRY_RUN_CMD ${pkgs.gnupg}/bin/gpg --import ${config.sops.secrets.zeev_gpg_key.path} &> /dev/null
-  '';
-
   programs = {
     home-manager.enable = true;
+
     git = {
       enable = true;
       userName = "4rmcyt";
-      userEmail = "4rmcyt@gmail.com"; # Set your email
+      userEmail = "4rmcyt@gmail.com";
       signing = {
-        key = "FD1AA16D16ACD8A003AD6D7AD85B52C9288A138E"; # Find with: gpg --list-secret-keys
+        key = "FD1AA16D16ACD8A003AD6D7AD85B52C9288A138E";
         signByDefault = true;
       };
     };
@@ -43,6 +46,11 @@
       shellAliases = {
         ll = "ls -l";
         update = "sudo nixos-rebuild switch --flake .#homeserver";
+      };
+
+      powerlevel10k = {
+        enable = true;
+        configFile = ./dots/zsh/.p10k.zsh;
       };
 
       plugins = [
@@ -66,11 +74,6 @@
           name = "you-should-use";
           src = pkgs.zsh-you-should-use;
         }
-        {
-          name = "powerlevel10k";
-          src = pkgs.zsh-powerlevel10k;
-          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-        }
       ];
 
       oh-my-zsh = {
@@ -85,9 +88,9 @@
     nixfmt = {
       enable = true;
       formatOnSave = true;
-      rfcStyle = true; # Use RFC style for formatting
+      rfcStyle = true;
     };
-    
   };
+
   home.stateVersion = "25.05";
 }
