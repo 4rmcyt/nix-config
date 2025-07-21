44f1b0255dc9d3bc77adbeaa11be32c724ee3962
4rmcyt <4rmcyt@gmail.com>
2025-07-21 17:14:10 -0600

    fix: update home-manager import path for zeev user configuration

--- users/default.nix
+++ users/default.nix
@@ -57,7 +57,7 @@ in
           "media"
           "samba"
         ];
-        home-manager.users.zeev = import ../home.nix;
+        home-manager.users.zeev = import ../home-manager/default.nix;
         hashedPasswordFile = config.sops.secrets.zeev_password.path;
         openssh.authorizedKeys.keys = server-keys;
       };
