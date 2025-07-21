28920cc40a12f118fb4a65bcf1f7bedfbe181306
4rmcyt <4rmcyt@gmail.com>
2025-07-21 14:36:51 -0600

    refactor: consolidate homepage widget credentials and update service configuration

--- modules/sops/default.nix
+++ modules/sops/default.nix
@@ -35,33 +35,33 @@
       miniflux_db_password = { sopsFile = ../../secrets/postgres.yaml; key = "miniflux_db_password"; };
       keycloak_db_password = { sopsFile = ../../secrets/postgres.yaml; key = "keycloak_db_password"; };
       
-
-      # --- Homepage Widget Credentials (from homepage.yaml) ---
-      homepage_hass_key = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_hass_key"; };
-      homepage_jellyseerr_key = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_jellyseerr_key"; };
-      homepage_lidarr_key     = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_lidarr_key"; };
-      homepage_prowlarr_key   = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_prowlarr_key"; };
-      homepage_radarr_key     = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_radarr_key"; };
-      homepage_readarr_key    = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_readarr_key"; };
-      homepage_sonarr_key     = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_sonarr_key"; };
-      homepage_bazarr_key     = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_bazarr_key"; };
-      homepage_paperless_key  = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_paperless_key"; };
-      homepage_miniflux_key   = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_miniflux_key"; };
-      homepage_nextcloud_key  = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_nextcloud_key"; };
-      homepage_tailscale_key         = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_tailscale_key"; };
-      homepage_tailscale_device_id   = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_tailscale_device_id"; };
-      homepage_cloudflared_account_id = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_cloudflared_account_id"; };
-      homepage_cloudflared_key        = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_cloudflared_key"; };
-      homepage_cloudflared_tunnel_id  = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_cloudflared_tunnel_id"; };
-      homepage_nextdns_profile_id = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_nextdns_profile_id"; };
-      homepage_nextdns_key        = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_nextdns_key"; };
-      homepage_grafana_key        = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_grafana_key"; };
-      homepage_jellyfin_key       = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_jellyfin_key"; };
-      homepage_audiobookshelf_key = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_audiobookshelf_key"; };
-      homepage_kavita_key         = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_kavita_key"; };
-      homepage_latitude  = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_latitude"; };
-      homepage_longitude = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_longitude"; };
-      homepage_grafana_admin_password = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_grafana_admin_password"; };
+      homepage_secrets = { sopsFile = ../../secrets/homepage.yaml; };
+      # # --- Homepage Widget Credentials (from homepage.yaml) ---
+      # hass_key = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_hass_key"; };
+      # jellyseerr_key = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_jellyseerr_key"; };
+      # lidarr_key     = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_lidarr_key"; };
+      # prowlarr_key   = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_prowlarr_key"; };
+      # radarr_key     = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_radarr_key"; };
+      # readarr_key    = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_readarr_key"; };
+      # sonarr_key     = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_sonarr_key"; };
+      # bazarr_key     = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_bazarr_key"; };
+      # paperless_key  = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_paperless_key"; };
+      # miniflux_key   = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_miniflux_key"; };
+      # nextcloud_key  = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_nextcloud_key"; };
+      # tailscale_key         = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_tailscale_key"; };
+      # tailscale_device_id   = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_tailscale_device_id"; };
+      # cloudflared_account_id = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_cloudflared_account_id"; };
+      # cloudflared_key        = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_cloudflared_key"; };
+      # cloudflared_tunnel_id  = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_cloudflared_tunnel_id"; };
+      # nextdns_profile_id = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_nextdns_profile_id"; };
+      # nextdns_key        = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_nextdns_key"; };
+      # grafana_key        = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_grafana_key"; };
+      # jellyfin_key       = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_jellyfin_key"; };
+      # audiobookshelf_key = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_audiobookshelf_key"; };
+      # kavita_key         = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_kavita_key"; };
+      # latitude           = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_latitude"; };
+      # longitude          = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_longitude"; };
+      # grafana_admin_password = { sopsFile = ../../secrets/homepage.yaml; key = "homepage_grafana_admin_password"; };
     };
   };
 }
