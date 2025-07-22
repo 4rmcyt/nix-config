5ab4f2e0bdd0a720205859abfc2eb47a62ab08fe
4rmcyt <4rmcyt@gmail.com>
2025-07-21 18:47:07 -0600

    fix: update powerlevel10k plugin source path and file reference in zsh configuration

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -63,9 +63,8 @@
           src = pkgs.zsh-you-should-use;
         }
         {
-          name = "zsh-powerlevel10k";
-          src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
-          file = "powerlevel10k.zsh-theme";
+          name = "powerlevel10k/powerlevel10k";
+          src = pkgs.powerlevel10k;
         }
         {
           name = "powerlevel10k-config";
