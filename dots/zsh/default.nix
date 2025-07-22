866380b315e668f5053ad2c9e83e64848530d75b
4rmcyt <4rmcyt@gmail.com>
2025-07-22 09:22:55 -0600

    fix: update gnupg agent configuration and improve zsh prompt initialization

--- dots/zsh/default.nix
+++ dots/zsh/default.nix
@@ -49,18 +49,10 @@
         # this act as your ~/.zshrc but for all users (/etc/zshrc)
         source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
         source /etc/powerlevel10k/p10k.zsh
-
-        # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-        # Initialization code that may require console input (password prompts, [y/n]
-        # confirmations, etc.) must go above this block; everything else may go below.
+        # Enable instant prompt for faster shell startup
         if [[ -r "''${XDG_CACHE_HOME:-''$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
         source "''${XDG_CACHE_HOME:-''$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
         fi
-
-        # uncomment if you want to customize your LS_COLORS
-        # https://manpages.ubuntu.com/manpages/plucky/en/man5/dir_colors.5.html
-        #LS_COLORS='...'
-        #export LS_COLORS
       '';
     };
     oh-my-zsh = {
