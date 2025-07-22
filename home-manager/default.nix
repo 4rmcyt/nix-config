877aef13705e3be625ee6cf6822addc091c8f9d6
4rmcyt <4rmcyt@gmail.com>
2025-07-21 19:08:11 -0600

    fix: update import paths for Zsh and Neovim configurations in home-manager

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -33,8 +33,8 @@
     };
 
     imports = [
-      ./dots/zsh/default.nix
-      ./dots/nvim/default.nix
+      ../dots/zsh/default.nix
+      ../dots/nvim/default.nix
     ];
     programs.nix-index = {
       enable = true;
