98847acf2ae851d9f760e4b84aaeefd313142105
4rmcyt <4rmcyt@gmail.com>
2025-07-22 21:06:31 -0600

    fix: refactor admin credentials handling in miniflux configuration

--- services/miniflux.nix
+++ services/miniflux.nix
@@ -7,8 +7,15 @@
 {
   services.miniflux = {
     enable = true;
-
+    adminCredentialsFile = builtins.toFile "miniflux-initial-credentials" ''
+      ADMIN_USERNAME="admin"
+      ADMIN_PASSWORD="${config.sops.secrets.miniflux_admin_password.path}"
+    '';
     config = {
+      WORKER_POOL_SIZE = "5"; # number of background workers
+      POLLING_FREQUENCY = "60"; # feed refresh interval in minutes
+      BATCH_SIZE = "100"; # number of feeds sent to queue each interval
+      CLEANUP_ARCHIVE_READ_DAYS = "60"; # read items are removed after x days
       BASE_URL = "https://rss.example.com";
       CREATE_ADMIN = 0;
       LISTEN_ADDR = "localhost:8086";
@@ -19,9 +26,6 @@
       # OAUTH2_OIDC_DISCOVERY_ENDPOINT = "https://keycloak.example.com/realms/master";
       # OAUTH2_USER_CREATION = "1";
       # DISABLE_LOCAL_AUTH = "false";
-
-      ADMIN_USERNAME = "admin";
-      ADMIN_PASSWORD = config.sops.secrets.miniflux_admin_password.path;
     };
   };
 }
