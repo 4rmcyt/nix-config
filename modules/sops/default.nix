ca9f1c1ecc43bc31734e8a329155c8570f19ca64
4rmcyt <4rmcyt@gmail.com>
2025-07-23 11:54:13 -0600

    fix: remove owner and group from homepage_env in default.nix

--- modules/sops/default.nix
+++ modules/sops/default.nix
@@ -37,7 +37,7 @@
       
 
       # --- Homepage Widget Credentials (from homepage.yaml) ---
-       homepage_env = { sopsFile = ../../secrets/homepage.yaml; owner = "homepage-dashboard"; group = "homepage-dashboard"; format = "dotenv"; };
+       homepage_env = { sopsFile = ../../secrets/homepage.yaml; format = "dotenv"; };
     };
   };
 }
