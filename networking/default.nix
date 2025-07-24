9f16703af5cd11f4eb0c9baf063d6811e48446ab
4rmcyt <4rmcyt@gmail.com>
2025-07-24 17:33:49 -0600

    fix: comment out unused Ollama API and WebUI ports in firewall configuration

--- networking/default.nix
+++ networking/default.nix
@@ -46,8 +46,8 @@
         8888  # Paperless
         5232  # Radicale
         5000  # Kavita
-        11434 # Ollama API
-        11435 # Ollama WebUI
+        # 11434 # Ollama API
+        # 11435 # Ollama WebUI
 
         # System services
         8082  # Homepage dashboard
