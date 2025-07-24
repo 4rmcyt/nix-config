001f0008e6da34c0d79b022b853238bc55a23d47
4rmcyt <4rmcyt@gmail.com>
2025-07-24 15:47:22 -0600

    fix: add missing ports configuration for tplink and nextdns exporters in default.nix

--- containers/default.nix
+++ containers/default.nix
@@ -2,10 +2,16 @@
 
 {
   virtualisation.oci-containers.containers = {
+    backend = "podman"; 
     tplink-exporter-living-room = {
       image = "thelastguardian/tplinkexporter"; # Use the full sha256 digest
       autoStart = true;
       networks =  [ "host" ];
+      ports = [
+        {
+          "127.0.0.1:9266:9266"
+        }
+      ];
       environmentFiles = [ config.sops.secrets.containers_env.path ];
     };
 
@@ -13,6 +19,11 @@
       image = "thelastguardian/tplinkexporter"; # Use the full sha256 digest
       autoStart = true;
       networks =  [ "host" ];
+      ports = [
+        {
+          "127.0.0.1:9267:9267"
+        }
+      ];
       environmentFiles = [ config.sops.secrets.containers_env.path ];
     };
 
@@ -20,6 +31,11 @@
       image = "ghcr.io/raylas/nextdns-exporter:latest"; # Pin to a specific version, not :latest
       autoStart = true;
       networks =  [ "host" ];
+      ports = [
+        {
+          "127.0.0.1:9790:9790"
+        }
+      ];
       environmentFiles = [ config.sops.secrets.containers_env.path ];
     };
   };
