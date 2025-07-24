3fd1e0c8cb6f5468bb4cc9e312ce44eb38eb057e
4rmcyt <4rmcyt@gmail.com>
2025-07-23 21:28:46 -0600

    fix: restrict VPN access to localhost for improved security

--- services/nixarr.nix
+++ services/nixarr.nix
@@ -38,7 +38,6 @@
     vpn = {
       enable = true;
       wgConf = "/data/.secret/wg.conf";
-      accessibleFrom = [ "127.0.0.1" ]; # Or a more specific subnet
       vpnTestService = {
         port = 58403;
         enable = true;
