26fec4bbdce1f30805d68612b3884ce99b441fc3
4rmcyt <4rmcyt@gmail.com>
2025-07-24 19:10:34 -0600

    fix: add 'dive' to system packages in configuration

--- configuration.nix
+++ configuration.nix
@@ -39,7 +39,7 @@
 
   environment.systemPackages = with pkgs; [
     zsh git neovim direnv pass vim wget curl jq coreutils gawk gnugrep podman podman-compose
-    iproute2 mc htop btop lsof age sops ssh-to-age openssh wireguard-tools ollama
+    iproute2 mc htop btop lsof age sops ssh-to-age openssh wireguard-tools ollama dive
     apacheHttpd meslo-lgs-nf yamllint nix-index iotop podman-tui
     tuptime smartmontools fzf ffmpeg nmap trash-cli zip unar unzip p7zip
     calibre go nextdns nixfmt-rfc-style nil deploy-rs just nixpkgs-fmt tree git-crypt 
