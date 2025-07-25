aa6f9416c5157437375c8280a96243be2f23f213
4rmcyt <4rmcyt@gmail.com>
2025-07-25 17:28:49 -0600

    fix: set RUN_MIGRATIONS to 0 in miniflux service configuration

--- services/miniflux.nix
+++ services/miniflux.nix
@@ -20,7 +20,7 @@
       CREATE_ADMIN = 1;
       LISTEN_ADDR = "localhost:8086";
       DATABASE_URL = lib.mkDefault "user=miniflux dbname=miniflux sslmode=disable host=/run/postgresql";
-      RUN_MIGRATIONS = 1;
+      RUN_MIGRATIONS = 0;
       # DATABASE_URL = lib.mkForce "postgres://postgres:${config.sops.secrets.miniflux_db_password.path}@localhost/miniflux?sslmode=disable";
       # OAUTH2_PROVIDER = "oidc";
       # OAUTH2_CLIENT_ID = "miniflux";
