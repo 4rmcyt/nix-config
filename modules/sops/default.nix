caea91df277b684ae8984c2733225d199ed768e4
4rmcyt <4rmcyt@gmail.com>
2025-07-24 20:04:48 -0600

    fix: remove binary format from tplinkExporterConfig in sops configuration

--- modules/sops/default.nix
+++ modules/sops/default.nix
@@ -19,8 +19,8 @@
 
       # --- Containers Credentials ---
       containers_env = { sopsFile = ../../secrets/.env; owner = "root"; group = "root"; mode = "0400";  format = "dotenv"; };
-      tplinkExporterConfig = { sopsFile = ../../secrets/tplink_exporter.yaml; owner = "root"; group = "root"; mode = "0400"; format = "binary"; };      # --- Service Secrets ---
-      
+      tplinkExporterConfig = { sopsFile = ../../secrets/tplink_exporter.yaml; owner = "root"; group = "root"; mode = "0400"; };
+      # --- Service Secrets ---
       keycloak_admin_password = { sopsFile = ../../secrets/keycloak.yaml; key = "keycloak_admin_password"; owner = "keycloak"; };
       grafana_admin_password = { sopsFile = ../../secrets/grafana.yaml; key = "grafana_admin_password"; owner = "grafana"; };
       miniflux_admin_password = { sopsFile = ../../secrets/miniflux.yaml; key = "miniflux_admin_password"; owner = "miniflux"; };
