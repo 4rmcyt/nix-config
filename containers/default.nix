bb53bd812464b9e540eeddb3a181401d953e7dbe
4rmcyt <4rmcyt@gmail.com>
2025-07-25 13:32:11 -0600

    feat: add hearing-aid container configuration and update allowed TCP ports

--- containers/default.nix
+++ containers/default.nix
@@ -19,5 +19,12 @@
       ports = [ "127.0.0.1:9948:9948" ];
       environmentFiles = [ config.sops.secrets.containers_env.path ];
     };
+    hearing-aid = {
+      image = "blampe/lidarr:latest";
+      name = "hearing-aid";
+      autoStart = true;
+      networks = [ "podman" ];
+      ports = [ "127.0.0.1:18686:18686" ];
+    };
   };
 }
