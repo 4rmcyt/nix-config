4b12ba76bf3f12f552ca90e01a11d0960c968227
4rmcyt <4rmcyt@gmail.com>
2025-07-24 20:34:24 -0600

    fix: format code for consistency in default.nix

--- containers/default.nix
+++ containers/default.nix
@@ -2,20 +2,20 @@
 {
   virtualisation.oci-containers.backend = "podman";
   virtualisation.oci-containers.containers = {
-   tl-sg-prometheus-exporter = {
+    tl-sg-prometheus-exporter = {
       image = "ghcr.io/mad-ady/tl-sg-prometheus-exporter:main";
       autoStart = true;
-      networks =  [ "podman" ];
+      networks = [ "podman" ];
       ports = [ "127.0.0.1:8000:8000" ];
       volumes = [
-          "${config.sops.secrets.tplinkExporterConfig.path}:/app/config.yaml:ro"
+        "${config.sops.secrets.tplinkExporterConfig.path}:/app/config.yaml:ro"
       ];
     };
 
     nextdns-exporter = {
-      image = "ghcr.io/raylas/nextdns-exporter"; 
+      image = "ghcr.io/raylas/nextdns-exporter";
       autoStart = true;
-      networks =  [ "podman" ];
+      networks = [ "podman" ];
       ports = [ "127.0.0.1:9948:9948" ];
       environmentFiles = [ config.sops.secrets.containers_env.path ];
     };
