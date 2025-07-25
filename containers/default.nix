719343d09045bb5d7c7ec38bfd27df58f82e583e
4rmcyt <4rmcyt@gmail.com>
2025-07-24 19:52:19 -0600

    fix: refactor TP-Link exporter configuration and add secrets management

--- containers/default.nix
+++ containers/default.nix
@@ -1,31 +1,4 @@
 { config, pkgs, ... }:
-let
-  # Read the content of the secret files
-  tplink_office_ip = builtins.readFile config.sops.secrets.tplink_office_ip.path;
-  tplink_living_room_ip = builtins.readFile config.sops.secrets.tplink_living_room_ip.path;
-  tplink_office_password = builtins.readFile config.sops.secrets.tplink_office_password.path;
-  tplink_living_room_password = builtins.readFile config.sops.secrets.tplink_living_room_password.path;
-
-  # Generate the config.yml for the new exporter using the secrets
-  tplinkExporterConfig = pkgs.writeTextFile {
-    name = "tl-sg-exporter-config.yml";
-    text = ''
-      # Exporter listen address and port
-      listen: 0.0.0.0:9266
-
-      # List of switches to scrape
-      switches:
-        - name: living_room
-          host: ${tplink_living_room_ip}
-          user: admin
-          pass: ${tplink_living_room_password}
-        - name: office
-          host: ${tplink_office_ip}
-          user: admin
-          pass: ${tplink_office_password}
-    '';
-  };
-in
 {
   virtualisation.oci-containers.backend = "podman";
   virtualisation.oci-containers.containers = {
