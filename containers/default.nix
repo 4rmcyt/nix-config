c75a53f21c7001254cd13fb3ae6ade83034a4321
4rmcyt <4rmcyt@gmail.com>
2025-07-24 19:46:41 -0600

    fix: update TP-Link exporter configurations to consolidate port usage

--- containers/default.nix
+++ containers/default.nix
@@ -1,23 +1,40 @@
 { config, pkgs, ... }:
+let
+  # Read the content of the secret files
+  tplink_office_ip = builtins.readFile config.sops.secrets.tplink_office_ip.path;
+  tplink_living_room_ip = builtins.readFile config.sops.secrets.tplink_living_room_ip.path;
+  tplink_office_password = builtins.readFile config.sops.secrets.tplink_office_password.path;
+  tplink_living_room_password = builtins.readFile config.sops.secrets.tplink_living_room_password.path;
+
+  # Generate the config.yml for the new exporter using the secrets
+  tplinkExporterConfig = pkgs.writeTextFile {
+    name = "tl-sg-exporter-config.yml";
+    text = ''
+      # Exporter listen address and port
+      listen: 0.0.0.0:9266
+
+      # List of switches to scrape
+      switches:
+        - name: living_room
+          host: ${tplink_living_room_ip}
+          user: admin
+          pass: ${tplink_living_room_password}
+        - name: office
+          host: ${tplink_office_ip}
+          user: admin
+          pass: ${tplink_office_password}
+    '';
+  };
+in
 {
   virtualisation.oci-containers.backend = "podman";
   virtualisation.oci-containers.containers = {
-    # tplink-exporter-living-room = {
-    #   image = "thelastguardian/tplinkexporter";
-    #   networks =  [ "podman" ];
-    #   ports = [ "127.0.0.1:9266:9717" ];
-    #   cmd = [
-    #     "--host=192.168.1.101" "--username=admin" "--password=sw2_SeptuagintA"
-    #   ];
-    # };
-
-    tplink-exporter-office = {
-      image = "thelastguardian/tplinkexporter";
+   tl-sg-prometheus-exporter = {
+      image = "ghcr.io/mad-ady/tl-sg-prometheus-exporter:latest";
       autoStart = true;
-      networks =  [ "podman" ];
-      ports = [ "127.0.0.1:9267:9717" ];
-      cmd = [
-        "--host=192.168.1.100" "--username=admin" "--password=sw1_SeptuagintA"
+      ports = [ "127.0.0.1:9266:8000" ];
+      volumes = [
+        "${tplinkExporterConfig}:/app/config.yml:ro"
       ];
     };
 
