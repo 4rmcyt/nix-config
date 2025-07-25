eb0951abc43ca1d580aaf50f84db6c6d26ca5b61
4rmcyt <4rmcyt@gmail.com>
2025-07-25 15:35:16 -0600

    fix: update PostgreSQL identMap to use local authentication method

--- services/database.nix
+++ services/database.nix
@@ -39,8 +39,8 @@
       # ArbitraryMapName systemUser DBUser
         superuser_map      root      postgres
         superuser_map      postgres  postgres
-      # Let other names login as themselves
-        superuser_map      /^(.*)$   \1
+      # type database  DBuser  auth-method
+        local all       all     trust
     '';
   };
 }
