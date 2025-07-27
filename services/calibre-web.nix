a7d467d0bbfc1eea85701f35e89d0e2025f4c204
4rmcyt <4rmcyt@gmail.com>
2025-07-26 22:21:13 -0600

    feat: relax unidecode version pin in calibre-web package

--- services/calibre-web.nix
+++ services/calibre-web.nix
@@ -5,6 +5,19 @@
   ...
 }:
 {
+  nixpkgs.overlays = [
+  (final: prev: {
+    # Override the calibre-web package
+    calibre-web = prev.calibre-web.overrideAttrs (oldAttrs: {
+      # Add a post-patch step to modify the dependency list
+      postPatch = (oldAttrs.postPatch or "") + ''
+        # Find the requirements file and relax the unidecode version pin
+        substituteInPlace ./requirements.txt \
+          --replace "unidecode<1.4.0" "unidecode<=1.4.0"
+      '';
+    });
+  })
+];
   services.calibre-web = {
     enable = true;
     listen = {
