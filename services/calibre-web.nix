1a99b6c9da8d451a332331c1b93692c4882b1743
4rmcyt <4rmcyt@gmail.com>
2025-07-26 23:05:39 -0600

    fix: update calibre-web service to include unidecode dependency

--- services/calibre-web.nix
+++ services/calibre-web.nix
@@ -5,7 +5,20 @@
   ...
 }:
 
-{
+{ 
+  unidecode-old = prev.python3Packages.unidecode.overrideAttrs (old: {
+        pname = "Unidecode";
+        version = "1.4.0";
+        src = prev.fetchPypi {
+          inherit pname version;
+          hash = "sha256-c3c7606c27503ad8d501270406e345ddb480a7b5f38827eafe4fa82a137f0021";
+        };
+      });
+  calibre-web = prev.calibre-web.overridePythonAttrs (old: {
+        propagatedBuildInputs =
+          (lib.filter (p: p.pname or "" != "unidecode") old.propagatedBuildInputs)
+          ++ [ final.unidecode-old ];
+      });
   services.calibre-web = {
     enable = true;
     listen = {
