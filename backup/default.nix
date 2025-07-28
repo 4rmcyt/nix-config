63f0cc37d49ced67864eb50f0d20130ef1ede1ac
4rmcyt <4rmcyt@gmail.com>
2025-07-28 14:16:03 -0600

    fix: update SSH public key for knownHosts configuration in default.nix

--- backup/default.nix
+++ backup/default.nix
@@ -6,7 +6,7 @@
 { 
   programs.ssh.knownHosts."u478963.your-storagebox.de" = {
     "u478963.your-storagebox.de".publicKey =
-    "ssh-ed25519 AAAAB3NzaC1yc2EAAAABIwAAAQEA5EB5p/5Hp3hGW1oHok+PIOH9Pbn7cnUiGmUEBrCVjnAw+HrKyN8bYVV0dIGllswYXwkG/+bgiBlE6IVIBAq+JwVWu1Sss3KarHY3OvFJUXZoZyRRg/Gc/+LRCE7lyKpwWQ70dbelGRyyJFH36eNv6ySXoUYtGkwlU5IVaHPApOxe4LHPZa/qhSRbPo2hwoh0orCtgejRebNtW5nlx00DNFgsvn8Svz2cIYLxsPVzKgUxs8Zxsxgn+Q/UvR7uq4AbAhyBMLxv7DjJ1pc7PJocuTno2Rw9uMZi1gkjbnmiOh6TTXIEWbnroyIhwc8555uto9melEUmWNQ+C+PwAK+MPw==";
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINqWND9TV1kHdHx5b1slLau2pLJhEsxGTm1nBqFKP6G9";
   };
   
   services.restic.backups.full = {
