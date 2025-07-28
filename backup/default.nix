6ca2ada3b85f3fcf5c36f6df087347ff7db6be83
4rmcyt <4rmcyt@gmail.com>
2025-07-28 14:30:39 -0600

    fix: remove SSH knownHosts configuration from default.nix

--- backup/default.nix
+++ backup/default.nix
@@ -4,11 +4,6 @@
   ...
 }:
 { 
-  programs.ssh.knownHosts = {
-    "u478963.your-storagebox.de".publicKey =
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINqWND9TV1kHdHx5b1slLau2pLJhEsxGTm1nBqFKP6G9";
-  };
-
   services.restic.backups.full = {
     initialize = true;
     user = "restic";
