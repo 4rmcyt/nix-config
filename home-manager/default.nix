0da5f1c26ba06d690f6ebf219854533e1e58acd4
4rmcyt <4rmcyt@gmail.com>
2025-07-29 12:54:53 -0600

    fix: remove unused registry configuration from nix settings

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -22,11 +22,7 @@
       download-buffer-size = 10737418240; # 10 GiB
       max-jobs = 4;
     };
-    registry.nixpkgs = {
-      from = { id = "nixpkgs"; type = "indirect"; };
-      flake = inputs.nixpkgs;
-    };
-    
+
   };
 
   programs = {
