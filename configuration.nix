ecbfc2bc19bc85040f00d2069f85c0c87cfd9803
4rmcyt <4rmcyt@gmail.com>
2025-07-29 13:14:52 -0600

    fix: replace extra substituters and trusted public keys with standard fields for improved clarity

--- configuration.nix
+++ configuration.nix
@@ -25,17 +25,19 @@
       trusted-users = [
         "zeev"
       ];
-      extra-substituters = [
+      substituters = [
+        "https://4rmcyt.cachix.org"
         "https://nix-community.cachix.org"
         "https://nixpkgs-update.cachix.org"
         "https://cache.nixos.org/"
       ];
-      extra-trusted-public-keys = [
+      trusted-public-keys = [
         "4rmcyt.cachix.org-1:uKI766iybXD8uDBVexbc5BCYAfdBJ262ID4C+dl2hws="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nixpkgs-update.cachix.org-1:6y6Z2JdoL3APdu6/+Iy8eZX2ajf09e4EE9SnxSML1W8="
       ];
+      auto-optimise-store = true;
       warn-dirty = false;
       cores = 4;
       show-trace = true;
