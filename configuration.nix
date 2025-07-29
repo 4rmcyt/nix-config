a42733bb614a1b9e40622d1e4e478b4342ccaeb4
4rmcyt <4rmcyt@gmail.com>
2025-07-29 08:34:30 -0600

    fix: remove restic private key from SOPS secrets configuration

--- configuration.nix
+++ configuration.nix
@@ -68,10 +68,6 @@
           bits = 4096;
           path = config.sops.secrets.ssh_host_rsa_key.path;
         }
-        {
-          type = "ed25519";
-          path = config.sops.secrets.restic_private_key.path;
-        }
       ];
       settings = {
         PermitRootLogin = "no";
