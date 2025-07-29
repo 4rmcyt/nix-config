570f6e4b91de9bd3564a0d9c170dc167e4205425
4rmcyt <4rmcyt@gmail.com>
2025-07-29 12:36:36 -0600

    fix: add missing extra substituters and trusted public keys in nixConfig

--- flake.nix
+++ flake.nix
@@ -3,9 +3,13 @@
   nixConfig = {
       extra-substituters = [
         "https://4rmcyt.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org/"
       ];
       extra-trusted-public-keys = [
         "4rmcyt.cachix.org-1:uKI766iybXD8uDBVexbc5BCYAfdBJ262ID4C+dl2hws="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       ];
   };
   inputs = {
