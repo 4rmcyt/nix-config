0174a2a83b5b0b742fb80fe2032242c613dbc479
4rmcyt <4rmcyt@gmail.com>
2025-07-29 15:18:45 -0600

    fix: add database passwords for Keycloak and Hass, and remove unused authentication settings

--- services/postgresql.nix
+++ services/postgresql.nix
@@ -40,18 +40,7 @@
       # Let other names login as themselves
         superuser_map      /^(.*)$   \1
     '';
-    authentication = pkgs.lib.mkOverride 10 ''
-      #type database  DBuser  auth-method optional_ident_map
-      local sameuser  all     peer        map=superuser_map
-      local all postgres ident map=superuser_map
-      #type database DBuser origin-address auth-method
-      host  sameuser  all     ::1/128   md5
-      host  sameuser  all     127.0.0.1/32   md5
-      # for podman only
-      host sameuser all 10.88.0.0/16 md5
-      # for tailscale network
-      host sameuser all 100.75.0.0/10 md5
-    '';
+
 
   };
 
