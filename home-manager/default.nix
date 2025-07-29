d1ecc3f7a8b93dcef70aeac44f50483dfbc87294
4rmcyt <4rmcyt@gmail.com>
2025-07-29 12:53:11 -0600

    fix: add nix settings including trusted-users and registry configuration

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -11,6 +11,24 @@
     zsh-powerlevel10k
   ];
 
+   nix = {
+    package = pkgs.nixVersions.latest;
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      trusted-users = [ "root" "zeev" ];
+      warn-dirty = false;
+      cores = 4;
+      show-trace = true;
+      download-buffer-size = 10737418240; # 10 GiB
+      max-jobs = 4;
+    };
+    registry.nixpkgs = {
+      from = { id = "nixpkgs"; type = "indirect"; };
+      flake = inputs.nixpkgs;
+    };
+    
+  };
+
   programs = {
     git = {
       enable = true;
