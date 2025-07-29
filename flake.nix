469fb8097877d9893be24056f6b8a5761c5a95b0
4rmcyt <4rmcyt@gmail.com>
2025-07-29 12:31:02 -0600

    fix: remove duplicate nixConfig section in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,13 @@
 {
   description = "NixOS configuration for homeserver";
-  
+  nixConfig = {
+      extra-substituters = [
+        "https://4rmcyt.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "4rmcyt.cachix.org-1:uKI766iybXD8uDBVexbc5BCYAfdBJ262ID4C+dl2hws="
+      ];
+  };
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -67,14 +74,7 @@
     };
   };
 
-  nixConfig = {
-      extra-substituters = [
-        "https://4rmcyt.cachix.org"
-      ];
-      extra-trusted-public-keys = [
-        "4rmcyt.cachix.org-1:uKI766iybXD8uDBVexbc5BCYAfdBJ262ID4C+dl2hws="
-      ];
-  };
+  
     
   outputs =
     {
