91db8d6dd3e1fb6d7ef4dac05fe2290a34fb2a33
4rmcyt <4rmcyt@gmail.com>
2025-07-30 08:33:11 -0600

    fix: update PostgreSQL authentication settings to use trust method for local connections

--- services/postgresql.nix
+++ services/postgresql.nix
@@ -33,13 +33,15 @@
       }
     ];
 
-    identMap = ''
-      # ArbitraryMapName systemUser DBUser
-        superuser_map      root      postgres
-        superuser_map      postgres  postgres
-      # Let other names login as themselves
-        superuser_map      /^(.*)$   \1
-    '';
+  authentication = pkgs.lib.mkOverride 10 ''
+    #...
+    #type database DBuser origin-address auth-method
+    local all       all     trust
+    # ipv4
+    host  all      all     127.0.0.1/32   trust
+    # ipv6
+    host all       all     ::1/128        trust
+  '';
 
 
   };
