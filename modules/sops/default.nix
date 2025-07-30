c66a1f775d8b7ab6c8ec25146ed994931ddf57f3
4rmcyt <4rmcyt@gmail.com>
2025-07-30 12:20:36 -0600

    fix: update borgmatic SSH key configuration and add private key to secrets

--- modules/sops/default.nix
+++ modules/sops/default.nix
@@ -42,7 +42,7 @@
 
       #Backup Secrets
       borgmatic_encryption_pass = { sopsFile = ../../secrets/borgmatic.yaml; key = "borgmatic_encryption_pass"; };
-      borg_ssh_key = { sopsFile = ../../secrets/borg_key; };
+      borg_ssh_key = { sopsFile = ../../secrets/system.yaml; key = "borg_private_key"; };
     };
   };
 }
