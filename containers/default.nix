4554b11c5fbafb5fad554202ccc0aab0bee4dd6e
4rmcyt <4rmcyt@gmail.com>
2025-07-30 19:43:24 -0600

    fix: correct podman user configuration for nextdns-exporter

--- containers/default.nix
+++ containers/default.nix
@@ -20,7 +20,7 @@
 
     nextdns-exporter = {
       image = "ghcr.io/raylas/nextdns-exporter";
-      user = "podman:podman";
+      podman.user = "podman:podman";
       autoStart = true;
       networks = [ "podman" ];
       ports = [ "127.0.0.1:9948:9948" ];
