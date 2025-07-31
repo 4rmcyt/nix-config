9ec5cd465b4b6e2928c102d908267f358c07713e
4rmcyt <4rmcyt@gmail.com>
2025-07-31 16:13:17 -0600

    fix: update LLDAP configuration for improved structure and clarity

--- security/lldap.nix
+++ security/lldap.nix
@@ -13,9 +13,12 @@
       http_host = "127.0.0.1";
       ldap_base_dn = "dc=labhome,dc=work";
       ldaps_options.enabled = true;
-      database_url = "postgres://lldap:${config.sops.secrets.lldap_db_password.path}@/localhost/lldap"; 
+      database_url = "postgres://lldap:${config.sops.secrets.lldap_db_password.path}@/localhost/lldap";
+      ldap_user_email = "redacted@example.com"; 
     };
-    initialAdminPasswordFile = config.sops.secrets.lldap_admin_password.path;
-    jwtSecretFile = config.sops.secrets.lldap_jwt_secret.path;
+    environment = {
+      LLDAP_ADMIN_PASSWORD_FILE = config.sops.secrets.lldap_admin_password.path;
+      LLDAP_JWT_SECRET_FILE = config.sops.secrets.lldap_jwt_secret.path;
+    };
   };
 }
