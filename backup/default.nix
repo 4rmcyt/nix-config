0cf91319d19a5bbb7b4f7f6fce821659943cede7
4rmcyt <4rmcyt@gmail.com>
2025-07-30 21:19:52 -0600

    fix: comment out uptime_kuma notification configuration

--- backup/default.nix
+++ backup/default.nix
@@ -123,14 +123,14 @@
       check_last = 3;
 
       # Notifications
-      uptime_kuma = {
-        push_url = "https://kuma.labhome.work/api/push/borgmatic";
-        states = [
-          "start"
-          "finish"
-          "fail"
-        ];
-      };
+      # uptime_kuma = {
+      #   push_url = "https://kuma.labhome.work/api/push/borgmatic";
+      #   states = [
+      #     "start"
+      #     "finish"
+      #     "fail"
+      #   ];
+      # };
     };
   };
   users.users.borgmatic = {
