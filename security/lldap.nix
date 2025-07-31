5e47b7070871c9fbb04f242595ea70adebcb225d
4rmcyt <4rmcyt@gmail.com>
2025-07-31 16:05:05 -0600

    fix: add LLDAP secrets for improved configuration management

--- security/lldap.nix
+++ security/lldap.nix
@@ -1,45 +1,21 @@
 {
+  config,
   pkgs,
   lib,
-  config,
   ...
 }:
+
 {
   services.lldap = {
     enable = true;
-    package = pkgs.lldap;
     settings = {
-      http_host = "localhost";
       http_url = "https://labhome.work";
+      http_host = "127.0.0.1";
       ldap_base_dn = "dc=labhome,dc=work";
-      database_url = "postgres://lldap:${config.sops.secrets.lldap.path}@/localhost/lldap";
-      verbose = false;
+      ldaps_options.enabled = true;
+      database_url = "postgres://lldap:${config.sops.secrets.lldap_db_password.path}@/localhost/lldap"; 
     };
-    environment = {
-      LLDAP_LDAP_USER_PASS_FILE = config.sops.secrets.lldap_user_pass.path;
-      LLDAP_JWT_SECRET_FILE = config.sops.secrets.lldap_jwt_secret.path;
-      LLDAP_LDAPS_OPTIONS__ENABLED = "true";
-    };
+    initialAdminPasswordFile = config.sops.secrets.lldap_admin_password.path;
+    jwtSecretFile = config.sops.secrets.lldap_jwt_secret.path;
   };
-
-  users.users.lldap = {
-    name = "lldap";
-    group = "lldap";
-    isSystemUser = true;
-  };
-
-  users.groups.lldap = { };
-
-  # # setup the backup
-  # my.backups.services.lldap = {
-  #   paths = ["/var/lib/lldap/" "/var/lib/private/lldap"];
-  # };
 }
-
-settings = {
-        storage = {
-        compression = "zstd,1";
-        borg_rsh = "ssh -o 'StrictHostKeyChecking=no' -i /home/zeev/.ssh/zeev";
-        encryption_passcommand = config.sops.secrets.borgmatic_encryption_pass.path;
-        };
-      };
