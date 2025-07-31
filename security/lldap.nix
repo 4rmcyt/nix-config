0e32b69b719e817f1d834a783d67920ad6f65f56
4rmcyt <4rmcyt@gmail.com>
2025-07-31 16:24:00 -0600

    fix: update database_url format and add system user/group for lldap service

--- security/lldap.nix
+++ security/lldap.nix
@@ -13,12 +13,23 @@
       http_host = "127.0.0.1";
       ldap_base_dn = "dc=labhome,dc=work";
       ldaps_options.enabled = true;
-      database_url = "postgres://lldap:${config.sops.secrets.lldap_db_password.path}@/localhost/lldap";
+      database_url = "postgres://lldap:${config.sops.secrets.lldap_db_password.path}?host=/run/postgresql";
       ldap_user_email = "redacted@example.com"; 
     };
     environment = {
       LLDAP_ADMIN_PASSWORD = config.sops.secrets.lldap_admin_password.path;
       LLDAP_JWT_SECRET = config.sops.secrets.lldap_jwt_secret.path;
+      LLDAP_LDAPS_OPTIONS__ENABLED = "true";
     };
+  };
+  users.users.lldap = {
+    isSystemUser = true;
+    group = "lldap";
+  };
+  users.groups.lldap = { };
+
+  systemd.services.lldap = {
+      requires = [ "postgresql.service" ];
+      after = [ "postgresql.service" ];
   };
 }
