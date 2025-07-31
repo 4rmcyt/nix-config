a6611203c7402bb61527623a3afc1bee4c7069a0
4rmcyt <4rmcyt@gmail.com>
2025-07-31 09:32:59 -0600

    fix: update borgmatic configuration to include hooks and enhance error handling

--- backup/default.nix
+++ backup/default.nix
@@ -14,91 +14,97 @@ in
 {
 
   services.borgmatic = {
-    enable = true;
-    settings = {
-      # Sources
-      source_directories = [
-        "/home/zeev"
-        "/var/log"
-        "/var/lib/postgres-backup" # Include the directory where the dump will be.
-        "/var/lib/home-assistant"
-        "/var/lib/kavita"
-        "/var/lib/miniflux"
-        "/var/lib/mosquitto"
-        "/var/lib/paperless"
-        "/var/lib/prometheus2"
-        "/var/lib/radicale"
-        "/var/lib/sops"
-        "/var/lib/calibre-web"
-        "/var/lib/grafana"
-        "/var/lib/microbin"
-        "/var/lib/homepage-dashboard"
-        "/var/lib/nixos"
-        "/data/.secret"
-        "/data/media/.state"
-        "/etc"
-      ];
-      # Excludes
-      exclude_patterns = [
-        # Home directory excludes
-        "/home/zeev/Downloads"
-        "/home/zeev/backups"
-        "/home/zeev/.cache"
-        "/home/zeev/.npm/_cacache"
-        "*/node_modules"
-        "*/venv"
-        "*/.venv"
+  enable = true;
+  settings = {
+    # No changes needed for sources, excludes, repositories,
+    # encryption, ssh_command, compression, retention, etc.
+    source_directories = [
+      "/home/zeev"
+      "/var/log"
+      "/var/lib/postgres-backup"
+      "/var/lib/home-assistant"
+      "/var/lib/kavita"
+      "/var/lib/miniflux"
+      "/var/lib/mosquitto"
+      "/var/lib/paperless"
+      "/var/lib/prometheus2"
+      "/var/lib/radicale"
+      "/var/lib/sops"
+      "/var/lib/calibre-web"
+      "/var/lib/grafana"
+      "/var/lib/microbin"
+      "/var/lib/homepage-dashboard"
+      "/var/lib/nixos"
+      "/data/.secret"
+      "/data/media/.state"
+      "/etc"
+    ];
+    exclude_patterns = [
+      "/home/zeev/Downloads"
+      "/home/zeev/backups"
+      "/home/zeev/.cache"
+      "/home/zeev/.npm/_cacache"
+      "*/node_modules"
+      "*/venv"
+      "*/.venv"
+      "/var/lib/systemd"
+      "/var/lib/containers"
+      "/var/lib/flatpak"
+      "/var/lib/docker"
+      "/var/lib/Podman"
+      "*/.Trash"
+      "*/Cache"
+      "*/cache2"
+      "/home/*/.local/share/Trash"
+      "/home/*/.local/share/containers"
+    ];
+    repositories = [
+      {
+        label = "On Disk Backup";
+        path = "/data/backup/borg/${config.networking.hostName}";
+      }
+      {
+        label = "Hetzner Server Backup";
+        path = "ssh://u478963@u478963.your-storagebox.de:23/./borg/hostname/${config.networking.hostName}";
+      }
+    ];
+    encryption_passcommand = "${pkgs.coreutils}/bin/cat ${config.sops.secrets.borgmatic_encryption_pass.path}";
+    ssh_command = "ssh -i ${config.sops.secrets.borg_private_key.path} -o UserKnownHostsFile=${config.sops.secrets.knownHosts.path}";
+    compression = "zstd";
+    archive_name_format = "backup-{now:%Y-%m-%dT%H:%M:%S.%f}";
+    relocated_repo_access_is_ok = true;
+    keep_hourly = 24;
+    keep_daily = 7;
+    keep_weekly = 4;
+    keep_monthly = 12;
+    keep_yearly = 3;
 
-        # General system excludes
-        "/var/lib/systemd"
-        "/var/lib/containers"
-        "/var/lib/flatpak"
-        "/var/lib/docker"
-        "/var/lib/Podman"
-
-        # Wildcard excludes
-        "*/.Trash"
-        "*/Cache"
-        "*/cache2"
-        "/home/*/.local/share/Trash"
-        "/home/*/.local/share/containers"
-      ];
-      
-      # Repositories
-      repositories = [
-        {
-          label = "On Disk Backup";
-          path = "/data/backup/borg/${config.networking.hostName}";
-        }
-        {
-          label = "Hetzner Server Backup";
-          path = "ssh://u478963@u478963.your-storagebox.de:23/./borg/hostname/${config.networking.hostName}";
-        }
-      ];
-      encryption_passcommand = "${pkgs.coreutils}/bin/cat ${config.sops.secrets.borgmatic_encryption_pass.path}";
-      ssh_command = "ssh -i ${config.sops.secrets.borg_private_key.path} -o UserKnownHostsFile=${config.sops.secrets.knownHosts.path}";
-      compression = "zstd";
-      archive_name_format = "backup-{now:%Y-%m-%dT%H:%M:%S.%f}";
-      relocated_repo_access_is_ok = true;
-
-      # Retention
-      keep_hourly = 24;
-      keep_daily = 7;
-      keep_weekly = 4;
-      keep_monthly = 12;
-      keep_yearly = 3;
       
       # Hooks
-      before_backup = [
-        "echo Starting a backup job."
-        "${pkgs.iputils}/bin/ping -q -c 1 192.168.1.254 > /dev/null || exit 75" 
+      hooks = {
+      before_configuration = [
+        {
+          run = [
+            "echo 'Starting backup job.'"
+            "${pkgs.iputils}/bin/ping -q -c 1 192.168.1.254 > /dev/null"
+          ];
+        }
       ];
-      after_backup = [
-        "echo Backup created."
+
+      after_configuration = [
+        {
+          states = [ "finish" ];
+          run = [ "echo 'Backup job completed successfully.'" ];
+        }
       ];
+      
+      # Runs if any error occurs during the process.
       on_error = [
-        "echo Error while creating a backup."
+        {
+          run = [ "echo 'CRITICAL: A backup job failed.'" ];
+        }
       ];
+    };
 
       
       # # Consistency Checks
@@ -148,3 +154,9 @@ in
      "D /var/lib/borgmatic/cache 770 borgmatic borgmatic - -"
   ];
 }
+
+
+sudo -u borgmatic -- \
+  env BORG_PASSPHRASE=$(cat /run/secrets/borgmatic_encryption_pass) \
+  BORG_RSH="ssh -i /run/secrets/borg_private_key -o UserKnownHostsFile=/run/secrets/knownHosts" \
+  borg init --encryption=repokey -r ssh://u478963@u478963.your-storagebox.de:23/./borg/hostname/homeserver
