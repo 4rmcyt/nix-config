349d74161d4479f921823081c57d5463caa8f638
4rmcyt <4rmcyt@gmail.com>
2025-08-01 17:56:39 -0600

    fix: remove unused authentik secrets and update authentik_env configuration

--- modules/security/authentik/default.nix
+++ modules/security/authentik/default.nix
@@ -3,34 +3,14 @@
 {
   sops.secrets = {
     # --- Authentik Secrets ---
-    authentik_secret_key = {
-      sopsFile = ../../secrets/authentik.yaml;
-      key = "authentik_secret_key";
-      owner = config.users.users.authentik.name;
-      group = config.users.groups.authentik.name;
-      mode = "0400";
-    };
-    authentik_outpost_token = {
-      sopsFile = ../../secrets/authentik.yaml;
-      key = "authentik_outpost_token";
-      owner = config.users.users.authentik.name;
-      group = config.users.groups.authentik.name;
-      mode = "0400";
-    };
     authentik_env = {
       sopsFile = ../../secrets/authentik_env.yaml;
       owner = config.users.users.authentik.name;
       group = config.users.groups.authentik.name;
       mode = "0400";
-      format = "dotenv";
+      format = "binary";
     };
-    authentik_ldap = {
-      sopsFile = ../../secrets/authentik.yaml;
-      key = "authentik_ldap";
-      owner = config.users.users.authentik.name;
-      group = config.users.groups.authentik.name;
-      mode = "0400";
-    };
+    
     gmail_password = {
       sopsFile = ../../secrets/gmail_conf.yaml;
       key = "gmail_password";
@@ -46,11 +26,7 @@
 
   networking.firewall.allowedTCPPorts = [
     9000 # Authentik
-    8080 # Authentik Outpost Proxy
-    9100 # Authentik Metrics
-    3389 # LDAP
-    6636 # LDAPS
-
+    9300 # Prometheus Metrics
   ];
 
   services.nginx.virtualHosts."authentik.labhome.work" = {
@@ -85,9 +61,5 @@
       disable_startup_analytics = true;
       avatars = "initials";
     };
-  };
-  authentik-ldap = {
-    enable = true;
-    environmentFile = config.sops.secrets.authentik_ldap.path;
   };
 }
