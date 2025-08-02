e3bb9ca1ff12826a1fcdadfacd15c24b4f4b3223
4rmcyt <4rmcyt@gmail.com>
2025-08-01 19:13:43 -0600

    refactor: clean up virtual host configurations in nginx settings

--- modules/monitoring/default.nix
+++ modules/monitoring/default.nix
@@ -60,34 +60,34 @@
     recommendedOptimisation = true;
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
-  };
-  virtualHosts = {
-    "prometheus.labhome.work" = {
-      forceSSL = true;
-      enableACME = true;
-      locations."/" = {
-        proxyPass = "http://localhost:9090";
-        proxyWebsockets = true;
-      };
-    };
-    "uptime-kuma.labhome.work" = {
-      forceSSL = true;
-      enableACME = true;
-      locations."/" = {
-        proxyPass = "http://localhost:3001";
-        proxyWebsockets = true;
-      };
-    };
-    "grafana.labhome.work" = {
-      forceSSL = true;
-      enableACME = true;
-      locations."/" = {
-        proxyPass = "http://localhost:3000";
-        proxyWebsockets = true;
-      };
-    };
-  };
 
+    virtualHosts = {
+      "prometheus.labhome.work" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          proxyPass = "http://localhost:9090";
+          proxyWebsockets = true;
+        };
+      };
+      "uptime-kuma.labhome.work" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          proxyPass = "http://localhost:3001";
+          proxyWebsockets = true;
+        };
+      };
+      "grafana.labhome.work" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          proxyPass = "http://localhost:3000";
+          proxyWebsockets = true;
+        };
+      };
+    };
+  };
   environment.systemPackages = [
     pkgs.grafana
     pkgs.prometheus
