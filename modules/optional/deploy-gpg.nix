feb1126c14abd2730aac107b6c7d48e1f213dee4
4rmcyt <4rmcyt@gmail.com>
2025-08-01 20:02:15 -0600

    refactor: comment out gnupg.agent configuration in deploy-gpg.nix

--- modules/optional/deploy-gpg.nix
+++ modules/optional/deploy-gpg.nix
@@ -52,9 +52,9 @@ in
   };
 
   services.pcscd.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    pinentryPackage = pkgs.pinentry-gtk2;
-    enableSSHSupport = true;
-  };
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   pinentryPackage = pkgs.pinentry-gtk2;
+  #   enableSSHSupport = true;
+  # };
 }
