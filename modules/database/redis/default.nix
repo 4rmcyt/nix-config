9401c9c67a863f44155f6e5666c97f81564def2f
4rmcyt <4rmcyt@gmail.com>
2025-08-02 10:03:33 -0600

    refactor: add Redis configuration and secrets management

--- modules/database/redis/default.nix
+++ modules/database/redis/default.nix
@@ -5,39 +5,29 @@
   ...
 }:
 
-{
-  services.redis = {
-    enable = true;
-    package = pkgs.redis.overrideAttrs (oldAttrs: {
-      doCheck = false; # Disable tests to speed up build
-    });
-    settings = {
-      bind = "127.0.0.1:6379";
-      protected-mode = "yes";
-      dir = "/var/lib/redis";
-      user = "redis";
-      group = "redis";
-      maxmemory = "256mb"; # Limit memory usage
-      maxmemory-policy = "allkeys-lru"; # Evict least recently used keys
-      appendonly = "yes"; # Enable AOF for persistence
-      appendfsync = "everysec"; # Sync every second
-      save = [
-        "900 1"
-        "300 10"
-        "60 10000"
-      ]; # Save snapshots
-      requirepass = config.sops.secrets.redis_password.path; # Use a secure password
+{ 
+  sops.secrets = {
+    redis_password = {
+      sopsFile = ../../../secrets/redis.yaml;
+      key = "redis_password";
+      owner = config.users.users.redis.name;
+      group = config.users.groups.redis.name;
+      mode = "0400";
     };
   };
-  
+  users.users.redis = {
+    isSystemUser = true;
+    group = "redis";
+    extraGroups = [ "users" ];
+  };
+  users.groups.redis = { };
+
   networking.firewall.allowedTCPPorts = [
     6379 # Redis
   ];
-  sops.secrets = {
-    redis_password = {
-      owner = config.users.users.redis.name;
-      group = config.users.groups.redis.name;
-      mode = "0400"; # Read-only for owner
-    };
+
+  services.redis = {
+    enable = true;
+    requirepass = config.sops.secrets.redis_password.path; # Use a secure password
   };
 }
