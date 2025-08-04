950e67b26f216d6d19e6bdfa023bd2125352d022
4rmcyt <4rmcyt@gmail.com>
2025-08-04 17:30:28 -0600

    refactor: update macOS system defaults for Finder, Screenshot, Safari, Mail, and more

--- hosts/macbook/default.nix
+++ hosts/macbook/default.nix
@@ -111,9 +111,8 @@
     zsh.enable = true;
   };
 
-
   system.defaults = {
-    finder = {
+    "com.apple.finder" = {
       _FXShowPosixPathInTitle = true;
       _FXSortFoldersFirst = true;
       AppleShowAllExtensions = true;
@@ -129,13 +128,44 @@
       ShowRemovableMediaOnDesktop = true;
       ShowStatusBar = true;
     };
-    screencapture = {
-      location = "~/Pictures/Screenshots";
+    "com.apple.screencapture" = {
+      location = "~/Desktop";
       type = "png";
     };
-    desktopservices = {
+    "com.apple.desktopservices" = {
       DSDontWriteNetworkStores = true;
       DSDontWriteUSBStores = true;
+    };
+    "com.apple.Safari" = {
+      # Privacy: don’t send search queries to Apple
+      UniversalSearchEnabled = false;
+      SuppressSearchSuggestions = true;
+      # Press Tab to highlight each item on a web page
+      WebKitTabToLinksPreferenceKey = true;
+      ShowFullURLInSmartSearchField = true;
+      # Prevent Safari from opening ‘safe’ files automatically after downloading
+      AutoOpenSafeDownloads = false;
+      ShowFavoritesBar = false;
+      IncludeInternalDebugMenu = true;
+      IncludeDevelopMenu = true;
+      WebKitDeveloperExtrasEnabledPreferenceKey = true;
+      WebContinuousSpellCheckingEnabled = true;
+      WebAutomaticSpellingCorrectionEnabled = false;
+      AutoFillFromAddressBook = false;
+      AutoFillCreditCardData = false;
+      AutoFillMiscellaneousForms = false;
+      WarnAboutFraudulentWebsites = true;
+      WebKitJavaEnabled = false;
+      WebKitJavaScriptCanOpenWindowsAutomatically = false;
+      "com.apple.Safari.ContentPageGroupIdentifier.WebKit2TabsToLinks" = true;
+      "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" = true;
+      "com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled" = false;
+      "com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabled" = false;
+      "com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabledForLocalFiles" = false;
+      "com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaScriptCanOpenWindowsAutomatically" = false;
+    };
+    "com.apple.mail" = {
+      DisableInlineAttachmentViewing = true;
     };
     "com.apple.SoftwareUpdate" = {
       AutomaticCheckEnabled = true;
@@ -144,17 +174,27 @@
       CriticalUpdateInstall = 1;
       AutomaticallyInstallMacOSUpdates = false;
     };
-    commerce.AutoUpdate = true;
+    "com.apple.commerce" = {
+      AutoUpdate = true;
+    };
     menuExtraClock = {
       ShowAMPM = false;
       ShowDate = 1; # Always
       ShowSeconds = false;
       Show24Hour = true;
     };
-    "com.apple.AdLib".allowApplePersonalizedAdvertising = false;
-    "com.apple.controlcenter".BatteryShowPercentage = true;
-    "com.apple.ImageCapture".disableHotPlug = true;
-    "com.apple.TimeMachine".DoNotOfferNewDisksForBackup = true;
+    "com.apple.AdLib" = {
+      allowApplePersonalizedAdvertising = false;
+    };
+    "com.apple.controlcenter" = {
+      BatteryShowPercentage = true;
+    };
+    "com.apple.ImageCapture" = {
+      disableHotPlug = true;
+    };
+    "com.apple.TimeMachine" = {
+      DoNotOfferNewDisksForBackup = true;
+    };
     NSGlobalDomain = {
       AppleICUForce24HourTime = true;
       AppleInterfaceStyle = "Dark";
