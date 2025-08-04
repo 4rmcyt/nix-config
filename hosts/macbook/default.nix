8f21457054d976fb18bfa24ad169d29f92c80af9
4rmcyt <4rmcyt@gmail.com>
2025-08-04 16:13:19 -0600

    refactor: streamline zsh and git configurations, enhance system defaults structure

--- hosts/macbook/default.nix
+++ hosts/macbook/default.nix
@@ -165,9 +165,7 @@
   };
 
   nixpkgs.config.allowUnfree = true;
-  programs.zsh.enable = true;
   programs.nix-index.enable = true;
-  system.stateVersion = 4;
   nixpkgs.hostPlatform = "aarch64-darwin";
 
   fonts = {
@@ -178,149 +176,151 @@
     ];
   };
   programs = {
-      git = {
-        enable = true;
-        userName = "volodymyr.kondratenko@datos.live";
-        userEmail = "volodymyr.kondratenko@datos.live";
-        signing.key = "129B4C451BE08617E579CF8A625FD6A8899D566D";
-      };
+    git = {
+      enable = true;
+      userName = "volodymyr.kondratenko@datos.live";
+      userEmail = "volodymyr.kondratenko@datos.live";
+      signing.key = "129B4C451BE08617E579CF8A625FD6A8899D566D";
+    };
 
-      fzf = {
-        enable = true;
-        enableZshIntegration = true;
-        colors = {
-          fg = "#D8DEE9";
-          bg = "#2E3440";
-          hl = "#A3BE8C";
-          "fg+" = "#D8DEE9";
-          "bg+" = "#434C5E";
-          "hl+" = "#A3BE8C";
-          pointer = "#BF616A";
-          info = "#4C566A";
-          spinner = "#4C566A";
-          header = "#4C566A";
-          prompt = "#81A1C1";
-          marker = "#EBCB8B";
-        };
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+      colors = {
+        fg = "#D8DEE9";
+        bg = "#2E3440";
+        hl = "#A3BE8C";
+        "fg+" = "#D8DEE9";
+        "bg+" = "#434C5E";
+        "hl+" = "#A3BE8C";
+        pointer = "#BF616A";
+        info = "#4C566A";
+        spinner = "#4C566A";
+        header = "#4C566A";
+        prompt = "#81A1C1";
+        marker = "#EBCB8B";
       };
-      zsh = {
+    };
+    zsh = {
+      enable = true;
+      syntaxHighlighting.enable = true;
+      autosuggestion.enable = true;
+      enableCompletion = true;
+      initContent = "source ~/.p10k.zsh";
+      plugins = [
+        {
+          name = "powerlevel10k";
+          src = pkgs.zsh-powerlevel10k;
+          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        }
+        {
+          name = "zsh-history-substring-search";
+          src = pkgs.zsh-history-substring-search;
+          file = "share/zsh-history-substring-search/zsh-history-substring-search.zsh";
+        }
+        {
+          name = "zsh-you-should-use";
+          src = pkgs.zsh-you-should-use;
+          file = "share/zsh-you-should-use/zsh-you-should-use.plugin.zsh";
+        }
+      ];
+      oh-my-zsh = {
         enable = true;
-        syntaxHighlighting.enable = true;
-        autosuggestion.enable = true;
-        enableCompletion = true;
-        initContent = "source ~/.p10k.zsh";
         plugins = [
-          {
-            name = "powerlevel10k";
-            src = pkgs.zsh-powerlevel10k;
-            file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-          }
-          {
-            name = "zsh-history-substring-search";
-            src = pkgs.zsh-history-substring-search;
-            file = "share/zsh-history-substring-search/zsh-history-substring-search.zsh";
-          }
-          {
-            name = "zsh-you-should-use";
-            src = pkgs.zsh-you-should-use;
-            file = "share/zsh-you-should-use/zsh-you-should-use.plugin.zsh";
-          }
+          "git"
+          "sudo"
+          "direnv"
         ];
-        oh-my-zsh = {
-          enable = true;
-          plugins = [
-            "git"
-            "sudo"
-            "direnv"
-          ];
+      };
+    };
+
+    # Fix defaults structure - needs to be under system
+    system.defaults = {
+      CustomUserPreferences = {
+        "com.apple.AdLib" = {
+          allowApplePersonalizedAdvertising = false;
         };
+        "com.apple.controlcenter" = {
+          BatteryShowPercentage = true;
+        };
+        "com.apple.desktopservices" = {
+          DSDontWriteNetworkStores = true;
+          DSDontWriteUSBStores = true;
+        };
+        "com.apple.finder" = {
+          _FXSortFoldersFirst = true;
+          FXDefaultSearchScope = "SCcf";
+          ShowExternalHardDrivesOnDesktop = true;
+          ShowHardDrivesOnDesktop = false;
+          ShowMountedServersOnDesktop = true;
+          ShowRemovableMediaOnDesktop = true;
+        };
+        "com.apple.ImageCapture".disableHotPlug = true;
+        "com.apple.screencapture" = {
+          location = "~/Pictures/Screenshots";
+          type = "png";
+        };
+        "com.apple.SoftwareUpdate" = {
+          AutomaticCheckEnabled = true;
+          ScheduleFrequency = 1;
+          AutomaticDownload = 0;
+          CriticalUpdateInstall = 1;
+        };
+        "com.apple.TimeMachine".DoNotOfferNewDisksForBackup = true;
+        "com.apple.commerce".AutoUpdate = true;
+      };
+      NSGlobalDomain = {
+        AppleICUForce24HourTime = true;
+        AppleInterfaceStyle = "Dark";
+        AppleInterfaceStyleSwitchesAutomatically = false;
+        AppleMeasurementUnits = "Centimeters";
+        AppleMetricUnits = 1;
+        AppleTemperatureUnit = "Celsius";
+        InitialKeyRepeat = 15;
+        KeyRepeat = 2;
+        NSAutomaticCapitalizationEnabled = false;
+        NSAutomaticDashSubstitutionEnabled = false;
+        NSAutomaticPeriodSubstitutionEnabled = false;
+        NSAutomaticQuoteSubstitutionEnabled = false;
+        NSAutomaticSpellingCorrectionEnabled = true;
+        NSNavPanelExpandedStateForSaveMode = true;
+        NSNavPanelExpandedStateForSaveMode2 = true;
+      };
+      SoftwareUpdate = {
+        AutomaticallyInstallMacOSUpdates = false;
+      };
+      finder = {
+        _FXShowPosixPathInTitle = true;
+        FXEnableExtensionChangeWarning = false;
+        FXPreferredViewStyle = "Nlsv";
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        QuitMenuItem = true;
+        ShowPathbar = true;
+        ShowStatusBar = true;
+      };
+      menuExtraClock = {
+        ShowAMPM = false;
+        ShowDate = 1; # Always
+        ShowSeconds = false;
+        Show24Hour = true;
       };
 
-  # Fix defaults structure - needs to be under system
-  system.defaults = {
-    CustomUserPreferences = {
-      "com.apple.AdLib" = {
-        allowApplePersonalizedAdvertising = false;
+      programs.gnupg.agent = {
+        enable = true;
+        pinentryPackage = pkgs.pinentry_mac;
+        enableSSHSupport = true;
       };
-      "com.apple.controlcenter" = {
-        BatteryShowPercentage = true;
-      };
-      "com.apple.desktopservices" = {
-        DSDontWriteNetworkStores = true;
-        DSDontWriteUSBStores = true;
-      };
-      "com.apple.finder" = {
-        _FXSortFoldersFirst = true;
-        FXDefaultSearchScope = "SCcf";
-        ShowExternalHardDrivesOnDesktop = true;
-        ShowHardDrivesOnDesktop = false;
-        ShowMountedServersOnDesktop = true;
-        ShowRemovableMediaOnDesktop = true;
-      };
-      "com.apple.ImageCapture".disableHotPlug = true;
-      "com.apple.screencapture" = {
-        location = "~/Pictures/Screenshots";
-        type = "png";
-      };
-      "com.apple.SoftwareUpdate" = {
-        AutomaticCheckEnabled = true;
-        ScheduleFrequency = 1;
-        AutomaticDownload = 0;
-        CriticalUpdateInstall = 1;
-      };
-      "com.apple.TimeMachine".DoNotOfferNewDisksForBackup = true;
-      "com.apple.commerce".AutoUpdate = true;
-    };
-    NSGlobalDomain = {
-      AppleICUForce24HourTime = true;
-      AppleInterfaceStyle = "Dark";
-      AppleInterfaceStyleSwitchesAutomatically = false;
-      AppleMeasurementUnits = "Centimeters";
-      AppleMetricUnits = 1;
-      AppleTemperatureUnit = "Celsius";
-      InitialKeyRepeat = 15;
-      KeyRepeat = 2;
-      NSAutomaticCapitalizationEnabled = false;
-      NSAutomaticDashSubstitutionEnabled = false;
-      NSAutomaticPeriodSubstitutionEnabled = false;
-      NSAutomaticQuoteSubstitutionEnabled = false;
-      NSAutomaticSpellingCorrectionEnabled = true;
-      NSNavPanelExpandedStateForSaveMode = true;
-      NSNavPanelExpandedStateForSaveMode2 = true;
-    };
-    SoftwareUpdate = {
-      AutomaticallyInstallMacOSUpdates = false;
-    };
-    finder = {
-      _FXShowPosixPathInTitle = true;
-      FXEnableExtensionChangeWarning = false;
-      FXPreferredViewStyle = "Nlsv";
-      AppleShowAllExtensions = true;
-      AppleShowAllFiles = true;
-      QuitMenuItem = true;
-      ShowPathbar = true;
-      ShowStatusBar = true;
-    };
-    menuExtraClock = {
-      ShowAMPM = false;
-      ShowDate = 1; # Always
-      ShowSeconds = false;
-      Show24Hour = true;
-    };
 
-    programs.gnupg.agent = {
-      enable = true;
-      pinentryPackage = pkgs.pinentry_mac;
-      enableSSHSupport = true;
-    };
+      programs.nh = {
+        enable = true;
+        clean.enable = true;
+        clean.extraArgs = "--keep-since 10d --keep 3";
+        flake = "/Users/vk/.config/nixos-config";
+      };
 
-    programs.nh = {
-      enable = true;
-      clean.enable = true;
-      clean.extraArgs = "--keep-since 10d --keep 3";
-      flake = "/Users/vk/.config/nixos-config";
+      security.pam.enableSudoTouchId = true;
     };
-
-    security.pam.enableSudoTouchId = true;
   };
+  system.stateVersion = 25.05;
 }
