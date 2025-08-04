ed50dbfa6df88c1166aedb4b859191eefde826ce
4rmcyt <4rmcyt@gmail.com>
2025-08-03 20:00:31 -0600

    refactor: change miniflux database creation setting to false for consistency

--- modules/services/miniflux/default.nix
+++ modules/services/miniflux/default.nix
@@ -54,7 +54,7 @@
   services.miniflux = {
     enable = true;
     adminCredentialsFile = config.sops.secrets.miniflux_creds.path; # path to admin credentials file
-    createDatabaseLocally = true; # create database on first run
+    createDatabaseLocally = false; # create database on first run
     config = {
       WORKER_POOL_SIZE = "5"; # number of background workers
       POLLING_FREQUENCY = "60"; # feed refresh interval in minutes
