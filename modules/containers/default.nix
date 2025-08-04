9b4a5fc653aa4b1a065283ebb724f6ece8df7639
4rmcyt <4rmcyt@gmail.com>
2025-08-03 20:37:18 -0600

    refactor: remove user specification for nextdns-exporter container

--- modules/containers/default.nix
+++ modules/containers/default.nix
@@ -57,7 +57,6 @@
     # };
     nextdns-exporter = {
       image = "ghcr.io/raylas/nextdns-exporter";
-      user = "podman:podman";
       autoStart = true;
       networks = [ "podman" ];
       ports = [ "127.0.0.1:9948:9948" ];
