1897723a1f64b513758df6e4153a20fe6bc3fe1f
4rmcyt <4rmcyt@gmail.com>
2025-08-04 15:28:55 -0600

    refactor: streamline NixOS configuration by removing deprecated blocks and enhancing structure

--- hosts/macbook/default.nix
+++ hosts/macbook/default.nix
@@ -2,14 +2,19 @@
   pkgs,
   lib,
   username,
+  homebrew-core,
+  homebrew-cask,
+  homebrew-bundle,
   ...
 }:
 {
-
   imports = [
     ../../modules/users/vk.nix
     ../../modules/iterm2
   ];
+
+  # This configuration is now handled in the flake's `mkDarwinSystem` helper.
+  # You can safely REMOVE the nix-homebrew block from this file.
 
   sops.age.keyFile = "/Users/vk/.config/sops/age/keys.txt";
   sops.defaultSopsFormat = "yaml";
@@ -17,41 +22,22 @@
   nix = {
     package = pkgs.nixVersions.latest;
     settings = {
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-      trusted-users = [
-        "vk"
-      ];
+      experimental-features = [ "nix-command" "flakes" ];
+      trusted-users = [ "vk" ];
       auto-optimise-store = true;
       warn-dirty = false;
-      cores = 4;
+      cores = lib.systems.cpuCoreCount; # Use lib to get core count dynamically
+      max-jobs = lib.systems.cpuCoreCount;
       show-trace = true;
       download-buffer-size = 1073741824; # 1 GiB
-      max-jobs = 4;
     };
-
-    optimise = {
-      automatic = true;
-      dates = [ "weekly" ];
-    };
+    optimise.automatic = true;
   };
 
-  nix-homebrew = {
-    enable = true;
-    enableRosetta = true;
-    user = "vk";
-    taps = {
-      "homebrew/homebrew-core" = homebrew-core;
-      "homebrew/homebrew-cask" = homebrew-cask;
-      "homebrew/homebrew-bundle" = homebrew-bundle;
-    };
-    mutableTaps = false;
-  };
-
+  # This enables the daemon for multi-user builds.
   services.nix-daemon.enable = true;
 
+  # Declarative Homebrew package management via nix-darwin
   homebrew = {
     enable = true;
     onActivation = {
@@ -59,144 +45,169 @@
       cleanup = "zap";
       upgrade = true;
     };
-    brewPrefix = "/opt/homebrew/bin";
-    taps = [
-      "amar1729/formulae"
-    ];
-    caskArgs = {
-      no_quarantine = true;
-    };
+    taps = [ "amar1729/formulae" ];
+    caskArgs.no_quarantine = true;
     casks = [
+      "alt-tab"
       "displaylink"
-      "meetingbar"
-      "pycharm-ce"
-      "yubico-authenticator"
+      "docker" # docker-desktop is now just 'docker'
+      "emclient"
+      "fbreader"
+      "font-hack-nerd-font"
+      "google-chrome"
       "linearmouse"
       "logitech-g-hub"
-      "fbreader"
-      "alt-tab"
-      "docker-desktop"
-      "google-chrome"
-      "font-hack-nerd-font"
-      "emclient"
-      "sublime-text"
+      "meetingbar"
+      "pycharm-ce"
       "raycast"
+      "sublime-text"
+      "yubico-authenticator"
     ];
     brews = [
-      "curl"
-      "go"
       "browserpass"
-      "python"
+      "curl"
+      "gnupg"
+      "gnutls"
+      "go"
+      "libgcrypt"
+      "libusb"
+      "p11-kit"
       "pinentry"
       "pinentry-mac"
-      "libusb"
-      "gnupg"
-      "libgcrypt"
-      "p11-kit"
-      "gnutls"
+      "python"
       "unbound"
     ];
-    masApps = {
-    };
+    masApps = { };
   };
 
-  # The settings moved from your flake.nix
+  # Nix-managed packages available in the system profile
   environment.systemPackages = with pkgs; [
-    mas
-    fzf
-    pet
-    direnv
-    git
-    pyenv
-    gh
-    tenv
-    delta
-    jq
-    yq
-    pandoc
-    lorri
-    btop
-    tree
-    jetbrains-mono
-    neofetch
-    nixfmt-rfc-style
-    opentofu
-    age-plugin-yubikey
-    yubikey-manager
-    tailscale
-    jellyfin-media-player
-    dbeaver-bin
-    slack
-    telegram-desktop
-    iterm2
-    the-unarchiver
-    appcleaner
-    vscode
-    wireguard-tools
-    zoom-us
-    youtube-music
-    neovim
-    pinentry-tty
-    deploy-rs
-    git-crypt
-    pass
-    mc
-    nixos-generators
-    fd
-    yubico-piv-tool
-    yubikey-personalization
-    pcsc-tools
-    git-crypt
-    gpgme
-    wget
-    docker
-    just
-    cargo
-    firefox
-    sops
     age
-    ssh-to-age
     age-plugin-yubikey
-    pipx
-    poetry
+    appcleaner
     bison
+    btop
+    cargo
+    dbeaver-bin
+    delta
+    deploy-rs
+    direnv
+    docker
+    fd
+    firefox
     flex
     fontforge
-    utm
-    srecord
+    fzf
+    gh
+    git
+    git-crypt
+    gpgme
+    iterm2
+    jetbrains-mono
+    jellyfin-media-player
+    jq
+    just
+    lorri
+    mas
+    mc
+    m-cli
     minipro
-    pwgen
+    neofetch
+    neovim
     nh
     nix-output-monitor
+    nixos-generators
+    nixfmt-rfc-style
     nvd
+    opentofu
+    pandoc
+    pass
+    pcsc-tools
+    pet
+    pinentry-tty
+    pipx
     plistwatch
-    m-cli
+    poetry
+    pwgen
+    pyenv
+    sops
+    srecord
+    ssh-to-age
+    slack
+    tailscale
+    telegram-desktop
+    tenv
+    the-unarchiver
+    tree
+    utm
+    vscode
+    wget
+    wireguard-tools
+    yq
+    yubico-piv-tool
+    yubikey-manager
+    yubikey-personalization
+    youtube-music
+    zoom-us
   ];
 
-  # Fix variables to environment.variables
   environment.variables = {
     EDITOR = "nvim";
     SHELL = "${pkgs.zsh}/bin/zsh";
-    SYSTEMD_EDITOR = "nvim";
     VISUAL = "nvim";
   };
 
-  nixpkgs.config.allowUnfree = true;
-  programs.zsh.enable = true;
   programs.nix-index.enable = true;
+  nixpkgs.config.allowUnfree = true;
   system.stateVersion = 4;
-  nixpkgs.hostPlatform = "aarch64-darwin";
+  # This is set by the flake, no need to set it here.
+  # nixpkgs.hostPlatform = "aarch64-darwin";
 
-  fonts = {
-    packages = with pkgs; [
-      material-design-icons
-      font-awesome
-      fira-code
-    ];
-  };
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "FiraCode" "Hack" ]; })
+    material-design-icons
+    font-awesome
+  ];
 
-  # Fix defaults structure - needs to be under system
+  # --- Correctly Structured Blocks ---
+  # These were previously inside 'system.defaults', which was incorrect.
+
   system.defaults = {
+    # Settings for macOS GUI applications
+    finder = {
+      _FXShowPosixPathInTitle = true;
+      FXEnableExtensionChangeWarning = false;
+      FXPreferredViewStyle = "Nlsv";
+      AppleShowAllExtensions = true;
+      AppleShowAllFiles = true;
+      QuitMenuItem = true;
+      ShowPathbar = true;
+      ShowStatusBar = true;
+    };
+    menuExtraClock = {
+      ShowAMPM = false;
+      ShowDate = 1; # Always
+      ShowSeconds = false;
+      Show24Hour = true;
+    };
+    # Settings for system-wide preferences
+    NSGlobalDomain = {
+      AppleICUForce24HourTime = true;
+      AppleInterfaceStyle = "Dark";
+      AppleInterfaceStyleSwitchesAutomatically = false;
+      AppleMeasurementUnits = "Centimeters";
+      AppleMetricUnits = 1;
+      AppleTemperatureUnit = "Celsius";
+      InitialKeyRepeat = 15;
+      KeyRepeat = 2;
+      NSAutomaticCapitalizationEnabled = false;
+      NSAutomaticDashSubstitutionEnabled = false;
+      NSAutomaticPeriodSubstitutionEnabled = false;
+      NSAutomaticQuoteSubstitutionEnabled = false;
+      NSAutomaticSpellingCorrectionEnabled = true;
+      NSNavPanelExpandedStateForSaveMode = true;
+      NSNavPanelExpandedStateForSaveMode2 = true;
+    };
     CustomUserPreferences = {
       "com.apple.AdLib" = {
         allowApplePersonalizedAdvertising = false;
@@ -229,122 +240,83 @@
       };
       "com.apple.TimeMachine".DoNotOfferNewDisksForBackup = true;
       "com.apple.commerce".AutoUpdate = true;
-    };
-    NSGlobalDomain = {
-      AppleICUForce24HourTime = true;
-      AppleInterfaceStyle = "Dark";
-      AppleInterfaceStyleSwitchesAutomatically = false;
-      AppleMeasurementUnits = "Centimeters";
-      AppleMetricUnits = 1;
-      AppleTemperatureUnit = "Celsius";
-      InitialKeyRepeat = 15;
-      KeyRepeat = 2;
-      NSAutomaticCapitalizationEnabled = false;
-      NSAutomaticDashSubstitutionEnabled = false;
-      NSAutomaticPeriodSubstitutionEnabled = false;
-      NSAutomaticQuoteSubstitutionEnabled = false;
-      NSAutomaticSpellingCorrectionEnabled = true;
-      NSNavPanelExpandedStateForSaveMode = true;
-      NSNavPanelExpandedStateForSaveMode2 = true;
-    };
-    SoftwareUpdate = {
-      AutomaticallyInstallMacOSUpdates = false;
-    };
-    finder = {
-      _FXShowPosixPathInTitle = true;
-      FXEnableExtensionChangeWarning = false;
-      FXPreferredViewStyle = "Nlsv";
-      AppleShowAllExtensions = true;
-      AppleShowAllFiles = true;
-      QuitMenuItem = true;
-      ShowPathbar = true;
-      ShowStatusBar = true;
-    };
-    menuExtraClock = {
-      ShowAMPM = false;
-      ShowDate = 1; # Always
-      ShowSeconds = false;
-      Show24Hour = true;
-    };
-
-    programs.gnupg.agent = {
-      enable = true;
-      pinentryPackage = pkgs.pinentry-mac;
-      enableSSHSupport = true;
-    };
-
-    programs.nh = {
-      enable = true;
-      clean.enable = true;
-      clean.extraArgs = "--keep-since 10d --keep 3";
-      flake = "/Users/vk/.config/nixos-config";
-    };
-
-    programs = {
-      git = {
-        enable = true;
-        userName = "volodymyr.kondratenko@datos.live";
-        userEmail = "volodymyr.kondratenko@datos.live";
-        signing.key = "129B4C451BE08617E579CF8A625FD6A8899D566D";
-      };
-
-      fzf = {
-        enable = true;
-        enableZshIntegration = true;
-        colors = {
-          fg = "#D8DEE9";
-          bg = "#2E3440";
-          hl = "#A3BE8C";
-          "fg+" = "#D8DEE9";
-          "bg+" = "#434C5E";
-          "hl+" = "#A3BE8C";
-          pointer = "#BF616A";
-          info = "#4C566A";
-          spinner = "#4C566A";
-          header = "#4C566A";
-          prompt = "#81A1C1";
-          marker = "#EBCB8B";
-        };
-      };
-      zsh = {
-        enable = true;
-        syntaxHighlighting.enable = true;
-        autosuggestion.enable = true;
-        enableCompletion = true;
-        initContent = "source ~/.p10k.zsh";
-        plugins = [
-          {
-            name = "powerlevel10k";
-            src = pkgs.zsh-powerlevel10k;
-            file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-          }
-          {
-            name = "zsh-history-substring-search";
-            src = pkgs.zsh-history-substring-search;
-            file = "share/zsh-history-substring-search/zsh-history-substring-search.zsh";
-          }
-          {
-            name = "zsh-you-should-use";
-            src = pkgs.zsh-you-should-use;
-            file = "share/zsh-you-should-use/zsh-you-should-use.plugin.zsh";
-          }
-          {
-            name = "nix-zsh-completions";
-            src = pkgs.nix-zsh-completions;
-            file = "share/zsh/site-functions/_nix";
-          }
-        ];
-        oh-my-zsh = {
-          enable = true;
-          plugins = [
-            "git"
-            "sudo"
-            "direnv"
-          ];
-        };
-      };
-
-      security.pam.enableSudoTouchId = true;
+      "com.apple.SoftwareUpdate".AutomaticallyInstallMacOSUpdates = false;
     };
   };
+
+  programs.gnupg.agent = {
+    enable = true;
+    pinentryPackage = pkgs.pinentry-mac;
+    enableSSHSupport = true;
+  };
+
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 10d --keep 3";
+    flake = "/Users/vk/.config/nixos-config";
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "volodymyr.kondratenko@datos.live";
+    userEmail = "volodymyr.kondratenko@datos.live";
+    signing.key = "129B4C451BE08617E579CF8A625FD6A8899D566D";
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+    colors = {
+      fg = "#D8DEE9";
+      bg = "#2E3440";
+      hl = "#A3BE8C";
+      "fg+" = "#D8DEE9";
+      "bg+" = "#434C5E";
+      "hl+" = "#A3BE8C";
+      pointer = "#BF616A";
+      info = "#4C566A";
+      spinner = "#4C566A";
+      header = "#4C566A";
+      prompt = "#81A1C1";
+      marker = "#EBCB8B";
+    };
+  };
+
+  # I combined the two zsh blocks for clarity
+  programs.zsh = {
+    enable = true;
+    syntaxHighlighting.enable = true;
+    autosuggestion.enable = true;
+    enableCompletion = true;
+    initContent = "source ~/.p10k.zsh";
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" "sudo" "direnv" ];
+    };
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "zsh-history-substring-search";
+        src = pkgs.zsh-history-substring-search;
+        file = "share/zsh-history-substring-search/zsh-history-substring-search.zsh";
+      }
+      {
+        name = "zsh-you-should-use";
+        src = pkgs.zsh-you-should-use;
+        file = "share/zsh-you-should-use/zsh-you-should-use.plugin.zsh";
+      }
+      {
+        name = "nix-zsh-completions";
+        src = pkgs.nix-zsh-completions;
+        file = "share/zsh/site-functions/_nix";
+      }
+    ];
+  };
+
+  security.pam.enableSudoTouchId = true;
 }
