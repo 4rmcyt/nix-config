f159bb6bb3fa007d07fc493a2d6c9add91666de6
4rmcyt <4rmcyt@gmail.com>
2025-08-04 20:04:50 -0600

    feat: add RPC bind address configuration for download service

--- modules/services/nixarr/default.nix
+++ modules/services/nixarr/default.nix
@@ -280,6 +280,7 @@ in
         download-queue-size = 10;
         download-queue-enabled = true;
         rpc-whitelist-enabled = false;
+        rpc-bind-address = "127.0.0.1";
         rpc-enabled = true;
         rpc-port = 9091;
         download-dir = "/data/Downloads";
