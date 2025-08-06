700215ed30e08c86b382d45e578215e7f2f16d55
4rmcyt <4rmcyt@gmail.com>
2025-08-05 20:31:17 -0600

    fix: add user configuration for zeev in home-manager

--- flake.nix
+++ flake.nix
@@ -133,6 +133,9 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
+              users.users.zeev = {
+                home = "/modules/users/zeev";
+              };
               home-manager.users.zeev = {
                 imports = [
                   ./modules/home-manager/homeserver
