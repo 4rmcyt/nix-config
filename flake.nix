3995da1898ae4f1595cb7fddf33403b1effe7fa7
4rmcyt <4rmcyt@gmail.com>
2025-08-05 20:42:49 -0600

    fix: add nixvim home-manager module to zeev's configuration

--- flake.nix
+++ flake.nix
@@ -136,6 +136,7 @@
               home-manager.users.zeev = {
                 imports = [
                   ./modules/home-manager/homeserver
+                  inputs.nixvim.home-managerModules.default
                 ];
                 _module.args.self = self;
                 _module.args.host = "homeserver";
