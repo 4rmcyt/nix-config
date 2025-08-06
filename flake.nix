12fb84d993fa52589087e864b1b5e20ac52f9242
4rmcyt <4rmcyt@gmail.com>
2025-08-05 20:17:29 -0600

    fix: add sops-nix module to outputs in flake.nix

--- flake.nix
+++ flake.nix
@@ -126,6 +126,7 @@
             nixarr.nixosModules.default
             authentik-nix.nixosModules.default
             nix-index-database.nixosModules.nix-index
+            sops-nix.nixosModules.sops
             vscode-server.nixosModules.default
             home-manager.nixosModules.home-manager
             {
