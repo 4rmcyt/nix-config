ce40e76609e3170cfdfbc4e47fa3829ac9985f35
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-08-07 11:29:32 -0600

    refactor: streamline firewall rules and Nginx configuration for improved security and performance

--- modules/networking/base/default.nix
+++ modules/networking/base/default.nix
@@ -20,22 +20,17 @@
       logReversePathDrops = true;
       logRefusedConnections = false; # Avoid log spam
 
-      extraCommands = ''
+      extraRules = ''
         # Drop invalid packets
-        iptables -A nixos-fw -m conntrack --ctstate INVALID -j DROP
+        -m conntrack --ctstate INVALID -j DROP
 
-        # Rate limit SSH
-        iptables -A nixos-fw -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --set --name SSH
-        iptables -A nixos-fw -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 4 --name SSH -j DROP
+        # Block common TCP scan techniques
+        -p tcp --tcp-flags ALL NONE -j DROP
+        -p tcp --tcp-flags ALL ALL -j DROP
 
-        # Block common attack vectors
-        iptables -A nixos-fw -p tcp --tcp-flags ALL NONE -j DROP
-        iptables -A nixos-fw -p tcp --tcp-flags ALL ALL -j DROP
-        iptables -A nixos-fw -p tcp --tcp-flags ALL FIN,URG,PSH -j DROP
-        iptables -A nixos-fw -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
-
-        # Geo-blocking (example for known bad actors)
-        # iptables -A nixos-fw -m geoip --src-cc CN,RU,KP -j DROP
+        # Rate limit new SSH connections
+        -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --set --name SSH
+        -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 4 --name SSH -j DROP
       '';
 
       allowedTCPPorts = [
@@ -62,46 +57,7 @@
 
       ];
 
-      # rejectPackets = true;
-
-      # # Rate limiting for SSH
-      # extraCommands = ''
-      #   # Rate limit SSH connections
-      #   iptables -A nixos-fw -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --set --name SSH
-      #   iptables -A nixos-fw -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 4 --name SSH -j DROP
-
-      #   # Block common attack ports
-      #   iptables -A nixos-fw -p tcp --dport 23 -j DROP  # Telnet
-      #   iptables -A nixos-fw -p tcp --dport 135 -j DROP # RPC
-      #   iptables -A nixos-fw -p tcp --dport 445 -j DROP # SMB
-      # '';
-
-      # kernel.sysctl = {
-      #   # IP Spoofing protection
-      #   "net.ipv4.conf.default.rp_filter" = 1;
-      #   "net.ipv4.conf.all.rp_filter" = 1;
-
-      #   # IP redirects
-      #   "net.ipv4.conf.all.accept_redirects" = 0;
-      #   "net.ipv6.conf.all.accept_redirects" = 0;
-      #   "net.ipv4.conf.all.send_redirects" = 0;
-
-      #   # Source packet routing
-      #   "net.ipv4.conf.all.accept_source_route" = 0;
-      #   "net.ipv6.conf.all.accept_source_route" = 0;
-
-      #   # Log Martians
-      #   "net.ipv4.conf.all.log_martians" = 1;
-
-      #   # ICMP
-      #   "net.ipv4.icmp_echo_ignore_broadcasts" = 1;
-      #   "net.ipv4.icmp_ignore_bogus_error_responses" = 1;
-
-      #   # TCP hardening
-      #   "net.ipv4.tcp_syncookies" = 1;
-      #   "net.ipv4.tcp_rfc1337" = 1;
-      # };
-
+      rejectPackets = true;
       trustedInterfaces = [ "tailscale0" ];
     };
   };
