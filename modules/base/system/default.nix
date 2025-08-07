365f5c9f92b27244bd8bef42db15efcce979a5a5
4rmcyt <4rmcyt@gmail.com>
2025-08-06 22:08:47 -0600

    fix: enhance server security by disabling unnecessary services and adding audit rules

--- modules/base/system/default.nix
+++ modules/base/system/default.nix
@@ -47,25 +47,114 @@
 
   security = {
     pam.loginLimits = [
-      { domain = "@users"; type = "soft"; item = "nproc"; value = "1024"; }
-      { domain = "@users"; type = "hard"; item = "nproc"; value = "2048"; }
-      { domain = "@users"; type = "soft"; item = "nofile"; value = "4096"; }
-      { domain = "@users"; type = "hard"; item = "nofile"; value = "8192"; }
-      
+      {
+        domain = "@users";
+        type = "soft";
+        item = "nproc";
+        value = "1024";
+      }
+      {
+        domain = "@users";
+        type = "hard";
+        item = "nproc";
+        value = "2048";
+      }
+      {
+        domain = "@users";
+        type = "soft";
+        item = "nofile";
+        value = "4096";
+      }
+      {
+        domain = "@users";
+        type = "hard";
+        item = "nofile";
+        value = "8192";
+      }
+
       # Security: disable core dumps
-      { domain = "*"; type = "hard"; item = "core"; value = "0"; }
-      
+      {
+        domain = "*";
+        type = "hard";
+        item = "core";
+        value = "0";
+      }
+
       # Security: memory limits
-      { domain = "@users"; type = "soft"; item = "memlock"; value = "64"; }
-      { domain = "@users"; type = "hard"; item = "memlock"; value = "64"; }
+      {
+        domain = "@users";
+        type = "soft";
+        item = "memlock";
+        value = "64";
+      }
+      {
+        domain = "@users";
+        type = "hard";
+        item = "memlock";
+        value = "64";
+      }
     ];
 
     auditd.enable = true;
-    audit.enable = true;
+    audit = {
+      enable = true;
+
+      # Add audit rules for server security
+      rules = [
+        # Monitor file access
+        "-w /etc/passwd -p wa -k identity"
+        "-w /etc/group -p wa -k identity"
+        "-w /etc/shadow -p wa -k identity"
+        "-w /etc/sudoers -p wa -k identity"
+
+        # Monitor system calls
+        "-a always,exit -F arch=b64 -S adjtimex -S settimeofday -k time-change"
+        "-a always,exit -F arch=b64 -S clock_settime -k time-change"
+        "-w /etc/localtime -p wa -k time-change"
+
+        # Monitor network configuration
+        "-w /etc/hosts -p wa -k network"
+        "-w /etc/sysconfig/network -p wa -k network"
+
+        # Monitor login/logout events
+        "-w /var/log/faillog -p wa -k logins"
+        "-w /var/log/lastlog -p wa -k logins"
+        "-w /var/log/tallylog -p wa -k logins"
+
+        # Monitor process and session initiation
+        "-w /var/run/utmp -p wa -k session"
+        "-w /var/log/wtmp -p wa -k logins"
+        "-w /var/log/btmp -p wa -k logins"
+
+        # Monitor privileged commands
+        "-a always,exit -F path=/usr/bin/passwd -F perm=x -F auid>=1000 -F auid!=4294967295 -k privileged-passwd"
+        "-a always,exit -F path=/usr/bin/sudo -F perm=x -F auid>=1000 -F auid!=4294967295 -k privileged-sudo"
+
+        # Monitor kernel module loading
+        "-w /sbin/insmod -p x -k modules"
+        "-w /sbin/rmmod -p x -k modules"
+        "-w /sbin/modprobe -p x -k modules"
+
+        # Monitor systemd
+        "-w /bin/systemctl -p x -k systemd"
+        "-w /etc/systemd/ -p wa -k systemd"
+      ];
+    };
+
     protectKernelImage = true;
-    lockKernelModules = false;
+    lockKernelModules = true; # Changed: Enable kernel module locking
+
+    # Add additional server security
+    allowUserNamespaces = false;
+    forcePageTableIsolation = true;
+    virtualisation.flushL1DataCache = "always";
+
+    # Add AppArmor for additional security
+    apparmor = {
+      enable = true;
+      killUnconfinedConfinables = true;
+    };
   };
-  services.haveged.enable = true;
 
   # console = {
   #   font = "Lat2-Terminus16";
