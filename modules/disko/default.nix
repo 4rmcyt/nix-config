14cde2eea7bfcf716c630fd88aa25fdcc0237c08
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-08-08 10:23:35 -0600

    refactor: restructure disk configuration for improved clarity and functionality

--- modules/disko/default.nix
+++ modules/disko/default.nix
@@ -1,125 +1,173 @@
 {
   disko.devices = {
-    disk = {
-      nvme = {
-        type = "disk";
-        device = "/dev/disk/by-id/nvme-SAMSUNG_MZVLW256HEHP-000L7_S35ENX0K543315";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              size = "1G";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
-              };
+    one = {
+      type = "disk";
+      device = "/dev/disk/by-id/nvme-SAMSUNG_MZVLW256HEHP-000L7_S35ENX0K543315";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            label = "EFI";
+            name = "ESP";
+            size = "2048M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [
+                "defaults"
+                "umask=0077"
+              ];
             };
-            zfs = {
-              size = "100%";
+          };
+          luks = lib.mkIf cfg.zfs.root.encrypt {
+            size = "100%";
+            content = {
+              type = "luks";
+              name = "crypted1";
+              settings.allowDiscards = true;
+              passwordFile = "/tmp/secret.key";
               content = {
                 type = "zfs";
-                pool = "system";
+                pool = "zroot";
               };
+            };
+          };
+          notluks = lib.mkIf (!cfg.zfs.root.encrypt) {
+            size = "100%";
+            content = {
+              type = "zfs";
+              pool = "zroot";
             };
           };
         };
       };
-      sata = {
-        type = "disk";
-        device = "/dev/disk/by-id/ata-Patriot_P210_1024GB_P210EDCB23011109345";
-        content = {
-          type = "gpt";
-          partitions = {
-            zfs = {
-              size = "100%";
+    };
+    two = {
+      type = "disk";
+      device = "/dev/disk/by-id/ata-Patriot_P210_1024GB_P210EDCB23011109345";
+      content = {
+        type = "gpt";
+        partitions = {
+          luks = {
+            size = "100%";
+            content = {
+              type = "luks";
+              name = "crypted2";
+              settings.allowDiscards = true;
+              passwordFile = "/tmp/secret.key";
               content = {
                 type = "zfs";
-                pool = "data"; 
+                pool = "zroot";
               };
             };
           };
         };
       };
     };
-    zpool = {
-      system = {
+  };
+  zpool = {
+      zroot = {
         type = "zpool";
+        mode = mkIf cfg.zfs.root.mirror "mirror";
         rootFsOptions = {
-          mountpoint = "none";
+          canmount = "off";
+          checksum = "edonr";
           compression = "zstd";
-          acltype = "posixacl";
-          xattr = "sa";
+          dnodesize = "auto";
+          mountpoint = "none";
+          normalization = "formD";
+          relatime = "on";
+          "com.sun:auto-snapshot" = "false";
         };
-        options.ashift = "12";
-
+        options = {
+          ashift = "12";
+          autotrim = "on";
+        };
         datasets = {
-          "root" = { type = "zfs_fs"; mountpoint = "/"; };
-          "root/nix" = { type = "zfs_fs"; mountpoint = "/nix"; };
-          "root/home" = { type = "zfs_fs"; mountpoint = "/home"; };
-          "root/var" = { type = "zfs_fs"; mountpoint = "/var"; };
-          "root/var/log" = { type = "zfs_fs"; mountpoint = "/var/log"; };
-          "root/var/lib" = { type = "zfs_fs"; mountpoint = "none"; };
-          "root/var/lib/postgresql" = { type = "zfs_fs"; mountpoint = "/var/lib/postgresql"; options.recordsize = "16k"; };
-          "root/var/lib/containers" = { type = "zfs_fs"; mountpoint = "/var/lib/containers"; };
-          "root/var/lib/redis-authentik" = { type = "zfs_fs"; mountpoint = "/var/lib/redis-authentik"; };
-          "root/var/lib/redis-paperless" = { type = "zfs_fs"; mountpoint = "/var/lib/redis-paperless"; };
-          "root/var/lib/redis-redis" = { type = "zfs_fs"; mountpoint = "/var/lib/redis-redis"; };
-          "root/var/lib/postgres-backup" = { type = "zfs_fs"; mountpoint = "/var/lib/postgres-backup"; };
-          "root/var/lib/paperless" = { type = "zfs_fs"; mountpoint = "/var/lib/paperless"; };
-          "root/var/lib/home-assistant" = { type = "zfs_fs"; mountpoint = "/var/lib/home-assistant"; };
-          "root/var/lib/microbin" = { type = "zfs_fs"; mountpoint = "/var/lib/microbin"; };
-          "root/var/lib/ldap" = { type = "zfs_fs"; mountpoint = "/var/lib/ldap"; };
-          "root/var/lib/authentik" = { type = "zfs_fs"; mountpoint = "/var/lib/authentik"; };
-          "root/var/lib/vaultwarden" = { type = "zfs_fs"; mountpoint = "/var/lib/vaultwarden"; };
-          "root/var/lib/grafana" = { type = "zfs_fs"; mountpoint = "/var/lib/grafana"; };
-          "root/var/lib/prometheus2" = { type = "zfs_fs"; mountpoint = "/var/lib/prometheus2"; };
-          "root/var/lib/acme" = { type = "zfs_fs"; mountpoint = "/var/lib/acme"; };
-          "root/var/lib/nginx" = { type = "zfs_fs"; mountpoint = "/var/lib/nginx"; };
-
-          "root/reserved" = {
-            type = "zfs_fs";
-            mountpoint = "none";
-            options.reservation = "12G";
-          };
-          "root/swap" = {
-            type = "zfs_volume";
-            size = "16G";
-            content = { type = "swap"; };
+          # zfs uses cow free space to delete files when the disk is completely filled
+          reserved = {
             options = {
-              volblocksize = "4k";
-              compression = "zle";
-              logbias = "throughput";
-              sync = "always";
-              primarycache = "metadata";
-              secondarycache = "none";
+              canmount = "off";
+              mountpoint = "none";
+              reservation = "20G";
+            };
+            type = "zfs_fs";
+          };
+          # nixos-anywhere currently has issues with impermanence so agenix keys are lost during the install process.
+          # as such we give /etc/ssh its own zfs dataset rather than using impermanence to save the keys when we wipe the root directory on boot
+          # not needed if you don't use agenix or don't use nixos-anywhere to install
+          etcssh = {
+            type = "zfs_fs";
+            mountpoint = "/etc/ssh";
+            options."com.sun:auto-snapshot" = "false";
+            postCreateHook = "zfs snapshot zroot/etcssh@empty";
+          };          
+          nix = {
+            type = "zfs_fs";
+            mountpoint = "/nix";
+            options = {
+              atime = "off";
+              canmount = "on";
               "com.sun:auto-snapshot" = "false";
             };
+            postCreateHook = "zfs snapshot zroot/nix@empty";
+          };
+          # Where everything else lives, and is wiped on reboot by restoring a blank zfs snapshot.
+          root = {
+            type = "zfs_fs";
+            options.mountpoint = "legacy";
+            options."com.sun:auto-snapshot" = "false";
+            mountpoint = "/";
+            postCreateHook = ''
+                zfs snapshot zroot/root@empty
+            '';
+          home = {
+            type = "zfs_fs";
+            options."com.sun:auto-snapshot" = "false";
+            mountpoint = "/home";
+            postCreateHook = "zfs snapshot zroot/home@empty";
+          };
+          var = {
+            type = "zfs_fs";
+            options."com.sun:auto-snapshot" = "false";
+            mountpoint = "/var";
+            postCreateHook = "zfs snapshot zroot/var@empty";
+          };
+          postgresql = {
+            type = "zfs_fs";
+            options."com.sun:auto-snapshot" = "false";
+            mountpoint = "/var/lib/postgresql";
+            options."recordsize" = "16K";
+            postCreateHook = "zfs snapshot zroot/postgresql@empty";
+          };
+          containers = {
+            type = "zfs_fs";
+            options."com.sun:auto-snapshot" = "false";
+            mountpoint = "/var/lib/containers";
+            postCreateHook = "zfs snapshot zroot/containers@empty";
+          };
+          authentik = {
+            type = "zfs_fs";
+            options."com.sun:auto-snapshot" = "false";
+            mountpoint = "/var/lib/authentik";
+            postCreateHook = "zfs snapshot zroot/authentik@empty";
+          };
+          vaultwarden = {
+            type = "zfs_fs";
+            options."com.sun:auto-snapshot" = "false";
+            mountpoint = "/var/lib/vaultwarden";
+            postCreateHook = "zfs snapshot zroot/vaultwarden@empty";
+          };
+          data = {
+            type = "zfs_fs";
+            options."com.sun:auto-snapshot" = "false";
+            mountpoint = "/data";
+            postCreateHook = "zfs snapshot zroot/data@empty";
           };
         };
       };
-
-      data = {
-        type = "zpool";
-        rootFsOptions = { canmount = "off"; };
-        datasets = {
-          "data" = { type = "zfs_fs"; mountpoint = "/data"; };
-          "data/media" = { type = "zfs_fs"; mountpoint = "/data/media"; options.recordsize = "1M"; };
-          "data/media/movies" = { type = "zfs_fs"; mountpoint = "/data/media/movies"; };
-          "data/media/shows" = { type = "zfs_fs"; mountpoint = "/data/media/shows"; };
-          "data/media/music" = { type = "zfs_fs"; mountpoint = "/data/media/music"; };
-          "data/media/audiobooks" = { type = "zfs_fs"; mountpoint = "/data/media/audiobooks"; };
-          "data/media/books" = { type = "zfs_fs"; mountpoint = "/data/media/books"; };
-          "data/media/comics" = { type = "zfs_fs"; mountpoint = "/data/media/comics"; };
-          "data/media/manga" = { type = "zfs_fs"; mountpoint = "/data/media/manga"; };
-          "data/media/torrents" = { type = "zfs_fs"; mountpoint = "/data/media/torrents"; };
-          "data/media/usenet" = { type = "zfs_fs"; mountpoint = "/data/media/usenet"; };
-          "data/media/.state" = { type = "zfs_fs"; mountpoint = "/data/media/.state"; };
-          "data/Downloads" = { type = "zfs_fs"; mountpoint = "/data/Downloads"; };
-        };
-      };
-    };
+    }
   };
 }
