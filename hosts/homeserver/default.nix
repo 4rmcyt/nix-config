6aeecf14b5c7e020458c619e64763aa1579d03de
4rmcyt <4rmcyt@gmail.com>
2025-08-15 10:23:20 -0600

    fix: add activation script for nixos-needsreboot

--- hosts/homeserver/default.nix
+++ hosts/homeserver/default.nix
@@ -132,6 +132,13 @@
   # 6. System Services
   # =================================================================
   # This is the single, merged services block.
+  system.activationScripts = {
+    nixos-needsreboot = {
+      supportsDryActivation = true;
+      text = "${lib.getExe nixos-needsreboot.packages.${pkgs.system}.default} \"$systemConfig\" || true";
+    };
+  };
+
   services = {
     # --- SSH Server ---
     openssh = {
