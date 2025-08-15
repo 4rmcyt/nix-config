06609f3df8874484776f47f7d4c92c4311f26146
4rmcyt <4rmcyt@gmail.com>
2025-08-15 09:08:35 -0600

    fix: update git configuration to use the correct GPG program path

--- modules/home-manager/homeserver/default.nix
+++ modules/home-manager/homeserver/default.nix
@@ -66,6 +66,7 @@
       userName = "4rmcyt";
       userEmail = "4rmcyt@gmail.com";
       signing.key = "FD1AA16D16ACD8A003AD6D7AD85B52C9288A138E";
+      gpg.program = "/run/current-system/sw/bin/gpgOD";
     };
 
     nixvim = {
