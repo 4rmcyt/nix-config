c096e0ac094e13d62077480bde1fe77637734ab1
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-08-21 11:24:56 -0600

    refactor: improve code formatting and structure for better readability across multiple scripts

--- cpu_flags.sh
+++ cpu_flags.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-CPUFEATURES="$(wget -qO- "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/arch/x86/include/asm/cpufeatures.h")" 
+CPUFEATURES="$(wget -qO- "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/arch/x86/include/asm/cpufeatures.h")"
 grep "^flags" /proc/cpuinfo |
-        uniq |
-        sed 's/^.*: //' |
-        tr ' ' '\n' |
-        while read -r line; do
-                echo -n "${line}: "
-                echo "$CPUFEATURES" |
-                grep "FEATURE_${line^^}" |
-                        sed 's|.*) /\* ||' |
-                        sed 's| \*/$||'
-        done
+  uniq |
+  sed 's/^.*: //' |
+  tr ' ' '\n' |
+  while read -r line; do
+    echo -n "${line}: "
+    echo "$CPUFEATURES" |
+      grep "FEATURE_${line^^}" |
+      sed 's|.*) /\* ||' |
+      sed 's| \*/$||'
+  done
--- modules/users/vk/default.nix
+++ modules/users/vk/default.nix
@@ -1,16 +1,6 @@
 # File: nixos-config/modules/users/vk/default.nix
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 let
-  user-ed25519 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAokdbrMinZjhDnVLnrXOjNn9SvzsPdlP6P3T9hAtGG8 vk@Volodymyr-Kondratenko-Mac.local";
-  user-rsa = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDc3zaVdT+TBJdjBWbN2fwSricHc7yJFGPxB9PB2sR4mkCmv6FPBd8vGZ1pYLJWEqgPU0C76IWAiSpwRrYu4Da0JKyEITh69sT+ndufTsrXJwPPxFKsUnmm2XQE0O2M2dM3wx+sMnBxWc1AMlfkWDnpP2N1Rl33ridumzEAGvJGqrn/ScpHGSgEkpZwVAnO5U8S9EjuO0h+nUJUSfLJVcl/cLeqHuF5zE8mSxsrj1FjiymZSquOEVAwNOhbCLuFVsYSEb8qujFsD7M9Umd0qvPQwCY9zN/Hb37TrNebhJ32kjIOlrWO3fnreMetIVRtTC1/cvKnGV16S32/YGiIUb2zLTfxKp2bn2qvXgLwocKf/M56fobQ6LOt60dUG1y3QwRLI1uAQggzp2N3/shQRb89nCQ/Zq67h941U2Z/RnNx7Hzl6n9DHkiKmkvXQuld0DWgh6wwG775gR2wBZHgpqtLqoRhwFVrvwIL9UkrLL4PE9A5iBEmypWsCWUomi5St+k= vk@Volodymyr-Kondratenko-Mac.local";
-  zeev = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJLqJ3YhcAyUW6cnSPyuLp5+zCF3ULTGjkxcKNqeBzks redacted@example.com";
-  user-keys = [
-    user-ed25519
-    user-rsa
-    zeev
-  ];
-
-  server-keys = user-keys;
   username = "vk";
 in
 {
