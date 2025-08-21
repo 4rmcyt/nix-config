a641c69a8d978f599e5b8ef087adb183c969ac19
4rmcyt <4rmcyt@gmail.com>
2025-08-21 07:41:05 -0600

    feat: update flake.nix and flakeHelpers.nix for improved configuration
    - Added cachix as an extra substituter and its public key for trusted builds.
    - Updated various inputs to use shallow clones for faster fetching.
    - Refactored the NixOS configuration to streamline module imports and improve organization.
    - Enhanced home-manager configuration to use global and user packages.
    - Removed unused inputs and consolidated common NixOS modules for clarity.

--- flakeHelpers.nix
+++ flakeHelpers.nix
@@ -1,59 +1,41 @@
 inputs:
 let
-  homeManagerCfg = userPackages: extraImports: {
-    home-manager.useGlobalPkgs = false;
-    home-manager.extraSpecialArgs = {
-      inherit inputs;
+  homeManagerCfg = user: host: {
+    home-manager.useGlobalPkgs = true;
+    home-manager.useUserPackages = true;
+    home-manager.users.${user} = {
+      imports = [
+        ./modules/home-manager/${host}
+        inputs.sops-nix.homeManagerModules.sops
+      ];
+      sops.age.keyFile = "/home/zeev/.config/sops/age/keys.txt";
     };
-    home-manager.users.notthebee.imports = [
-      inputs.agenix.homeManagerModules.default
-      inputs.nix-index-database.homeModules.nix-index
-      ./users/notthebee/dots.nix
-      ./users/notthebee/age.nix
-    ]
-    ++ extraImports;
-    home-manager.backupFileExtension = "bak";
-    home-manager.useUserPackages = userPackages;
   };
+
+  commonNixOSModules = [
+    inputs.sops-nix.nixosModules.sops
+    { sops.age.keyFile = "/var/lib/sops/age.key"; }
+    inputs.home-manager.nixosModules.home-manager
+    inputs.disko.nixosModules.disko
+    inputs.nix-index-database.nixosModules.nix-index
+    inputs.auto-cpufreq.nixosModules.default
+    inputs.nixvim.nixosModules.nixvim
+    "${inputs.linkwarden-pr}/nixos/modules/services/web-apps/linkwarden.nix"
+    inputs.nixos-facter-modules.nixosModules.facter
+    { facter.reportPath = ./facter.json; }
+  ];
 in
 {
-
-  mkDarwin = machineHostname: nixpkgsVersion: extraHmModules: _extraModules: {
-    darwinConfigurations.${machineHostname} = inputs.nix-darwin.lib.darwinSystem {
-      system = "aarch64-darwin";
-      specialArgs = {
-        inherit inputs;
-      };
-      modules = [
-        inputs.agenix.darwinModules.default
-        ./machines/darwin
-        ./machines/darwin/${machineHostname}
-        inputs.home-manager-unstable.darwinModules.home-manager
-        (nixpkgsVersion.lib.attrsets.recursiveUpdate (homeManagerCfg true extraHmModules) {
-          home-manager.users.notthebee.home.homeDirectory = nixpkgsVersion.lib.mkForce "/Users/notthebee";
-        })
-      ];
-    };
-  };
   mkNixos = machineHostname: nixpkgsVersion: extraModules: {
     nixosConfigurations.${machineHostname} = nixpkgsVersion.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = {
         inherit inputs;
+        host = machineHostname;
       };
-      modules = [
-        ./homelab
-        ./machines/nixos/_common
-        ./machines/nixos/${machineHostname}
-        ./modules/email
-        ./modules/tg-notify
-        ./modules/auto-aspm
-        ./modules/mover
-        inputs.agenix.nixosModules.default
-        ./users/notthebee
-        (homeManagerCfg false [ ])
-      ]
-      ++ extraModules;
+      modules = commonNixOSModules ++ [
+        (homeManagerCfg "zeev" machineHostname)
+      ] ++ extraModules;
     };
   };
   mkMerge = inputs.nixpkgs.lib.lists.foldl' (
