5203d3ae0ba8f4f32e431ee73555fd7366925f38
volodymyr.kondratenko <volodymyr.kondratenko@datos.live>
2025-08-22 11:26:27 -0600

    Remove iTerm2 shell integration from initContent in home-manager configuration

--- modules/home-manager/homeserver/default.nix
+++ modules/home-manager/homeserver/default.nix
@@ -129,11 +129,6 @@
         fi
       '';
       initContent = ''
-        # iTerm2 Shell Integration
-        if [ -f "''${HOME}/.iterm2_shell_integration.zsh" ]; then
-          source "''${HOME}/.iterm2_shell_integration.zsh"
-        fi
-
         autoload -Uz compinit && compinit
 
         bindkey -v
