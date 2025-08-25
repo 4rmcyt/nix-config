4d98770dc06b431f1c779d6a0ccec7df0968f6a4
4rmcyt <4rmcyt@gmail.com>
2025-08-25 13:41:02 -0600

    Refactor firewall configuration in default.nix for clarity and organization

--- modules/networking/base/default.nix
+++ modules/networking/base/default.nix
@@ -4,41 +4,26 @@
     hostName = "homeserver";
     hostId = "0b8d0f5a";
     networkmanager.enable = true;
-    useDHCP = lib.mkForce true;
     enableIPv6 = false;
 
     firewall = {
       enable = true;
-
-      # Logging
       logReversePathDrops = true;
-      logRefusedConnections = false; # Avoid log spam
-
+      logRefusedConnections = false;
+      rejectPackets = true;
+      trustedInterfaces = [ "tailscale0" ];
       allowedTCPPorts = [
-        # Base services
         22 # SSH
         80 # HTTP
         443 # HTTPS
-
-        # 11434 # Ollama API
-        # 11435 # Ollama WebUI
-
-        # Monitoring (from monitoring.nix)
         3000 # Grafana
         9090 # Prometheus
         9100 # Node Exporter
-        # 8000  # TP-Link Exporter
-        # 9948  # Nextdns Exporter
         27196 # Cloudflare Exporter
         3001 # Uptime Kuma
-
-        # Database & Infrastructure
-        5432 # PostgreSQL (if needed externally)
+        5432 # PostgreSQL
         9091
       ];
-
-      rejectPackets = true;
-      trustedInterfaces = [ "tailscale0" ];
     };
   };
 }
