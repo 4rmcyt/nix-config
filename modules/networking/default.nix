10537e42059f7fef60aa6c131d25fd685f6500e2
4rmcyt <4rmcyt@gmail.com>
2025-08-25 13:43:48 -0600

    Refactor networking configuration in default.nix by integrating firewall settings and removing base import

--- modules/networking/default.nix
+++ modules/networking/default.nix
@@ -2,9 +2,34 @@
 {
   imports = [
     ./acme
-    ./base
     ./cloudflared
     ./nginx
     ./tailscale
   ];
+  networking = {
+    hostName = "homeserver";
+    hostId = "0b8d0f5a";
+    networkmanager.enable = true;
+    enableIPv6 = false;
+
+    firewall = {
+      enable = true;
+      logReversePathDrops = true;
+      logRefusedConnections = false;
+      rejectPackets = true;
+      trustedInterfaces = [ "tailscale0" ];
+      allowedTCPPorts = [
+        22 # SSH
+        80 # HTTP
+        443 # HTTPS
+        3000 # Grafana
+        9090 # Prometheus
+        9100 # Node Exporter
+        27196 # Cloudflare Exporter
+        3001 # Uptime Kuma
+        5432 # PostgreSQL
+        9091
+      ];
+    };
+  };
 }
