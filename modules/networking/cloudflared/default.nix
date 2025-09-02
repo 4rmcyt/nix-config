8ad2b2fe0709a8b3b1e48cb59c8e67a850a37667
4rmcyt <4rmcyt@gmail.com>
2025-09-01 18:45:56 -0600

    Add Readarr service configuration and enable it in Nixarr

--- modules/networking/cloudflared/default.nix
+++ modules/networking/cloudflared/default.nix
@@ -49,6 +49,7 @@
           "grafana.labhome.work" = "http://localhost:3000";
           "lazylibrarian.labhome.work" = "http://localhost:5299";
           "tdarr.labhome.work" = "http://localhost:8265";
+          "readarr.labhome.work" = "http://localhost:8787";
         };
       };
     };
