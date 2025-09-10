d65666cb7b235eb9c0149d2aa36720e32730e48d
4rmcyt <4rmcyt@gmail.com>
2025-09-10 16:04:15 -0600

    Remove stable NVIDIA package reference from hardware configuration

--- hosts/nixos/desktop/default.nix
+++ hosts/nixos/desktop/default.nix
@@ -123,7 +123,6 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
   hardware.opengl = {
     enable = true;
