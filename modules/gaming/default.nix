83293c51aa4f786eed96505563e1f18dcd60de51
4rmcyt <4rmcyt@gmail.com>
2025-09-10 12:20:49 -0600

    Refactor GameMode configuration to use mkForce for conflict resolution and include GPU settings

--- modules/gaming/default.nix
+++ modules/gaming/default.nix
@@ -16,7 +16,19 @@
   
 
   # GameMode
-  programs.gamemode.enable = true;
+  programs.gamemode = {
+    enable = lib.mkForce true;  # Use mkForce to resolve the conflict
+    settings = {
+      general = {
+        renice = 10;
+      };
+      gpu = {
+        apply_gpu_optimisations = "accept-responsibility";
+        gpu_device = 0;
+        amd_performance_level = "high";
+      };
+    };
+  };
 
 
   # Gaming packages
