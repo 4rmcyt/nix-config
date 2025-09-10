3255ddfa15aa8adf54abdd2be3e85cb341655a17
4rmcyt <4rmcyt@gmail.com>
2025-09-10 10:41:58 -0600

    Fix import paths in default.nix for module structure consistency

--- hosts/nixos/desktop/default.nix
+++ hosts/nixos/desktop/default.nix
@@ -2,9 +2,9 @@
 {
   imports = [
     ./hardware-configuration.nix
-    ../../../../modules/hyprland
-    ../../../../modules/gaming
-    ../../../../modules/users/zeev
+    ../../../modules/hyprland
+    ../../../modules/gaming  
+    ../../../modules/users/zeev
     ../../../modules/disko/desktop
   ];
 
