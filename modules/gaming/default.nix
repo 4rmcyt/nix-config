c85405955eebac8c7fc372caa306c8f4b712d797
4rmcyt <4rmcyt@gmail.com>
2025-09-10 12:20:10 -0600

    Simplify GameMode configuration by removing unnecessary settings and using default enable option

--- modules/gaming/default.nix
+++ modules/gaming/default.nix
@@ -16,19 +16,7 @@
   
 
   # GameMode
-  programs.gamemode = {
-    enable = lib.mkForce true;  # Use mkForce to resolve the conflict
-    settings = {
-      general = {
-        renice = 10;
-      };
-      gpu = {
-        apply_gpu_optimisations = "accept-responsibility";
-        gpu_device = 0;
-        amd_performance_level = "high";
-      };
-    };
-  };
+  programs.gamemode.enable = true;
 
 
   # Gaming packages
