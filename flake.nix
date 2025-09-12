6f89476faf5cfe0f6421ccbad31386f24e53cb2d
4rmcyt <4rmcyt@gmail.com>
2025-09-12 01:35:54 -0600

    refactor: comment out unused lix and lix-module configurations

--- flake.nix
+++ flake.nix
@@ -32,16 +32,16 @@
     #   config.allowUnfree = true;
     # };
 
-    lix = {
-      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
-      flake = false;
-    };
+    # lix = {
+    #   url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+    #   flake = false;
+    # };
 
-    lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.lix.follows = "lix";
-    };
+    # lix-module = {
+    #   url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    #   inputs.lix.follows = "lix";
+    # };
 
     nixos-wsl = {
       url = "github:nix-community/NixOS-WSL/main";
