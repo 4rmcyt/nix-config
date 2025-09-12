838e269b441f7e2e688f7f3dc57279ef2710b382
4rmcyt <4rmcyt@gmail.com>
2025-09-12 15:23:14 -0600

    fix: correct syntax error in binary-caches.json

--- flake.nix
+++ flake.nix
@@ -7,13 +7,14 @@
       "https://hyprland.cachix.org"
       "https://homeserver.cachix.org"
       "https://chaotic-nyx.cachix.org"
+      "https://4rmcyt.cachix.org"
     ];
 
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "homeserver.cachix.org-1:0vStm6koDUwET/iWYhbKpsuVO4v3UgN3510zYH9YpZU="
+      "4rmcyt.cachix.org-1:IzZEPOd8aKavFKw3BuUBAI/T93XUUWoS/n2M+LG65/0="
       "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
     ];
   };
