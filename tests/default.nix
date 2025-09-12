e1e8d14d52037f364bb7e4c4d16c3a91316bd2ea
4rmcyt <4rmcyt@gmail.com>
2025-09-12 00:33:51 -0600

    Update flake.lock and flakeHelpers.nix; remove hyprland module and tests

--- tests/default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:
-{
-  # This test runs in a VM, suitable for NixOS
-  homeserverTest = import ./homeserver.nix { inherit pkgs; };
-
-  # This creates a script package that can be run on the live system
-  macbookTestScript = import ./macbook.nix { inherit pkgs; };
-}
