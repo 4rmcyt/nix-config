6a5a5a48a62d80f826467ba0f521f236b56d34d8
4rmcyt <4rmcyt@gmail.com>
2025-09-12 15:41:27 -0600

    refactor: comment out aarch64-darwin system in flake.nix

--- flake.nix
+++ flake.nix
@@ -108,7 +108,7 @@
       {
         systems = [
           "x86_64-linux"
-          "aarch64-darwin"
+          # "aarch64-darwin"
         ];
         imports = [ treefmt-nix.flakeModule ];
         perSystem =
