a15de0f7466c65899c018e4d210086e52bd22886
4rmcyt <4rmcyt@gmail.com>
2025-09-17 13:04:28 -0600

    refactor: add nix-software-center and nixos-conf-editor to package inputs

--- hosts/nixos/desktop/default.nix
+++ hosts/nixos/desktop/default.nix
@@ -138,6 +138,8 @@
     dockfmt
     nix-diff
     dockerfile-language-server
+    inputs.nix-software-center.packages.${system}.nix-software-center
+    inputs.nixos-conf-editor.packages.${system}.nixos-conf-editor
   ];
 
   # Nix settings
