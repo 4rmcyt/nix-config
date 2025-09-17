30c77c46d1a7227486360790dca07ca5ae68b04f
4rmcyt <4rmcyt@gmail.com>
2025-09-16 21:13:30 -0600

    refactor: update Jellyfin service configuration to use systemd structure

--- modules/services/nixarr/default.nix
+++ modules/services/nixarr/default.nix
@@ -339,7 +339,7 @@ in
   };
 
 
-  services.jellyfin.serviceConfig = {
+  systemd.services.jellyfin.serviceConfig = {
     Environment = [
       "JELLYFIN_FFmpeg__analyzeduration=10000000"
       "JELLYFIN_FFmpeg__probesize=10000000"
