73c83ea54da38b7a00f6387619e2727dcdba2b68
4rmcyt <4rmcyt@gmail.com>
2025-09-16 20:49:44 -0600

    refactor: enable services in Nixarr configuration

--- modules/services/nixarr/default.nix
+++ modules/services/nixarr/default.nix
@@ -323,15 +323,15 @@ in
       };
     };
 
-    audiobookshelf = true;
+    audiobookshelf.enable = true;
     jellyfin = {
       enable = true;
       ffmpegExtraConfig = "--analyzeduration 10000000 -probesize 10000000";
     };
-    bazarr = true;
-    lidarr = true;
-    prowlarr = true;
-    radarr = true;
+    bazarr.enable = true;
+    lidarr.enable = true;
+    prowlarr.enable = true;
+    radarr.enable = true;
     sonarr.enable = true;
     jellyseerr.enable = true;
     readarr.enable = true;
