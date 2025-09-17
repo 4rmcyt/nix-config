c8b14148eaa303ed35c376f4a13f8d46b68381e9
4rmcyt <4rmcyt@gmail.com>
2025-09-17 13:04:28 -0600

    refactor: add nix-software-center and nixos-conf-editor to package inputs

--- flake.nix
+++ flake.nix
@@ -30,6 +30,9 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    nix-software-center.url = "github:snowfallorg/nix-software-center";
+    nixos-conf-editor.url = "github:snowfallorg/nixos-conf-editor";
+    
     # Performance & hardware
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     auto-cpufreq = {
