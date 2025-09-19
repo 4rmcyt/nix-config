a9a7f53393fe823d493909fa415efad20379c459
4rmcyt <4rmcyt@gmail.com>
2025-09-19 12:32:10 -0600

    refactor: fix tmux service import path in default.nix

--- hosts/nixos/desktop/default.nix
+++ hosts/nixos/desktop/default.nix
@@ -10,7 +10,7 @@
     ../../../modules/users/zeev
     ../../../modules/disko/desktop
     ../../../modules/base
-    /../../../modules/services/tmux
+    ../../../modules/services/tmux
   ];
 
   users.groups.git = { };
