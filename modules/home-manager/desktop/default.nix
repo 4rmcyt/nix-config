b38d048e5ec40d265aadf0c68e8b4fb52768b666
4rmcyt <4rmcyt@gmail.com>
2025-09-19 14:24:20 -0600

    refactor: simplify pyenv initialization in ZSH configuration

--- modules/home-manager/desktop/default.nix
+++ modules/home-manager/desktop/default.nix
@@ -94,9 +94,7 @@
       profileExtra = ''
         export PYENV_ROOT="$HOME/.pyenv"
         export PATH="$PYENV_ROOT/bin:$PATH"
-        if command -v pyenv >/dev/null 2>&1; then
-          eval "$(pyenv init --path)"
-        fi
+        eval "$(pyenv init --path)"
       '';
       initContent = ''
         autoload -Uz compinit && compinit
