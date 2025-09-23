2be1da4f1a2ee2252a6baf14a0e92ecd648fe660
4rmcyt <4rmcyt@gmail.com>
2025-09-23 11:43:18 -0600

    refactor: update DNS settings and adjust ghostty background opacity for improved performance and aesthetics

--- hosts/nixos/desktop/default.nix
+++ hosts/nixos/desktop/default.nix
@@ -63,9 +63,11 @@
       domains = [ "~." ];
       fallbackDns = [
         "45.90.28.0#Desktop-nextdns0.dns.nextdns.io"
+        "2a07:a8c0::#nextdns0.dns.nextdns.io"
         "45.90.30.0#Desktop-nextdns0.dns.nextdns.io"
+        "2a07:a8c1::#nextdns0.dns.nextdns.io"
       ];
-      dnsovertls = "opportunistic";
+      dnsovertls = "true";
     };
 
     tailscale.enable = true;
