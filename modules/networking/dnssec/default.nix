f578db97d36483f8a134ea59f48bc0809f02186f
4rmcyt <4rmcyt@gmail.com>
2025-09-24 09:55:50 -0600

    refactor: enhance DNSSEC configuration with dynamic NextDNS host generation

--- modules/networking/dnssec/default.nix
+++ modules/networking/dnssec/default.nix
@@ -1,22 +1,44 @@
-_: {
-  dhcpcd.extraConfig = "nohook resolv.conf";
-  networkmanager.dns = "none";
+{
+  config,
+  lib,
+  ...
+}:
+with lib;
+let
+  cfg = config.networking.dnssec;
+  inherit (config.networking) hostName;
+  inherit (cfg) profileId;
+  nextdnsHost = "${hostName}-${profileId}.dns.nextdns.io";
+in
+{
+  options.networking.dnssec = {
+    enable = mkEnableOption "DNSSEC/NextDNS configuration";
+    profileId = mkOption {
+      type = types.str;
+      description = "NextDNS profile ID (e.g., 2bffa2)";
+    };
+  };
 
-  networking.nameservers = [
-    "45.90.28.0#Desktop-2bffa2.dns.nextdns.io"
-    "45.90.30.0#Desktop-2bffa2.dns.nextdns.io"
-  ];
+  config = mkIf cfg.enable {
+    dhcpcd.extraConfig = "nohook resolv.conf";
+    networkmanager.dns = "none";
 
-  services.resolved = {
-    enable = true;
-    dnssec = "true";
-    domains = [ "~." ];
-    fallbackDns = [
-      "45.90.28.0#Desktop-2bffa2.dns.nextdns.io"
-      "2a07:a8c0::#2bffa2.dns.nextdns.io"
-      "45.90.30.0#Desktop-2bffa2.dns.nextdns.io"
-      "2a07:a8c1::#2bffa2.dns.nextdns.io"
+    networking.nameservers = [
+      "45.90.28.0#${nextdnsHost}"
+      "45.90.30.0#${nextdnsHost}"
     ];
-    dnsovertls = "true";
+
+    services.resolved = {
+      enable = true;
+      dnssec = "true";
+      domains = [ "~." ];
+      fallbackDns = [
+        "45.90.28.0#${nextdnsHost}"
+        "2a07:a8c0::#${nextdnsHost}"
+        "45.90.30.0#${nextdnsHost}"
+        "2a07:a8c1::#${nextdnsHost}"
+      ];
+      dnsovertls = "true";
+    };
   };
 }
