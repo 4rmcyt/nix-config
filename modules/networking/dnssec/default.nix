a484860e5c095d23af860481e5cf18a320606728
4rmcyt <4rmcyt@gmail.com>
2025-09-24 09:29:40 -0600

    refactor: add DNSSEC module and update imports for improved networking configuration

---
+++ modules/networking/dnssec/default.nix
@@ -0,0 +1,20 @@
+_:
+{
+  networking.nameservers = [
+    "45.90.28.0#Desktop-nextdns0.dns.nextdns.io"
+    "45.90.30.0#Desktop-nextdns0.dns.nextdns.io"
+  ];
+
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [
+      "45.90.28.0#Desktop-nextdns0.dns.nextdns.io"
+      "2a07:a8c0::#nextdns0.dns.nextdns.io"
+      "45.90.30.0#Desktop-nextdns0.dns.nextdns.io"
+      "2a07:a8c1::#nextdns0.dns.nextdns.io"
+    ];
+    dnsovertls = "true";
+  };
+}
