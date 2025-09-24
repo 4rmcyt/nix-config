2876d5ba64b47816a5c94455895f55def281f990
4rmcyt <4rmcyt@gmail.com>
2025-09-24 09:30:56 -0600

    refactor: remove NextDNS configuration from homeserver and clean up unused settings

--- hosts/nixos/desktop/default.nix
+++ hosts/nixos/desktop/default.nix
@@ -64,19 +64,6 @@
       ];
     };
 
-    resolved = {
-      enable = true;
-      dnssec = "true";
-      domains = [ "~." ];
-      fallbackDns = [
-        "45.90.28.0#Desktop-2bffa2.dns.nextdns.io"
-        "2a07:a8c0::#2bffa2.dns.nextdns.io"
-        "45.90.30.0#Desktop-2bffa2.dns.nextdns.io"
-        "2a07:a8c1::#2bffa2.dns.nextdns.io"
-      ];
-      dnsovertls = "true";
-    };
-
     desktopManager.plasma6.enable = true;
     displayManager.sddm = {
       enable = true;
