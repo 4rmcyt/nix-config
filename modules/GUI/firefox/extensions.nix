7aa229d578904d1f9e0cb623f63d0a52cf7ba777
4rmcyt <4rmcyt@gmail.com>
2025-09-25 13:54:34 -0600

    refactor: update Firefox configuration and extensions for improved performance

--- modules/GUI/firefox/extensions.nix
+++ modules/GUI/firefox/extensions.nix
@@ -8,23 +8,17 @@ let
   # nix flake show --json "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons" --all-systems | jq -r '.packages."x86_64-linux" | keys[]' | rg QUERY
   ryceeAddons = with inputs.firefox-addons.packages.${pkgs.system}; [
     ublock-origin
-    sponsorblock
     return-youtube-dislikes
     indie-wiki-buddy
-
-    modrinthify
     refined-github
+    undoclosetabbutton
     movie-web
-
-    # bypass-paywalls-clean (can't use, was creating popups)
+    tree-style-tab
     terms-of-service-didnt-read
-
     auto-tab-discard
-    # clearurls # Buggy with github search, not sure why
-    link-cleaner
-
     redirector # For nixos wiki
     darkreader
+    plasma-integration
   ];
 
   customAddons = [
