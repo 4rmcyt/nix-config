23326edbffa062f4f300a0fbb6ff8545cfca231a
4rmcyt <4rmcyt@gmail.com>
2025-10-07 11:28:57 -0600

    fix: refine gamemode configuration for improved GPU optimization and notification handling

--- modules/gaming/default.nix
+++ modules/gaming/default.nix
@@ -14,7 +14,6 @@
     platformOptimizations.enable = true;
   };
 
-  # Enable gamemode service properly
   programs.gamemode = {
     enable = true;
     settings = {
@@ -23,43 +22,40 @@
         ioprio = 7;
         inhibit_screensaver = 1;
       };
-      
-      # Remove GPU optimizations that are causing issues
+
       gpu = {
-        apply_gpu_optimisations = "reject-responsibility"; # Changed from accept-responsibility
-        # Remove nvidia_performance_level as it's not supported
-      };
-      
-      custom = {
-        start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
-        end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+        apply_gpu_optimisations = "accept-responsibility";
+        nvidia_performance_level = "high";
+        # gpu_device = 0; # Let gamemode auto-detect the GPU
+
+        custom = {
+          start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+          end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+        };
       };
     };
+
+    # Gaming packages
+    environment.systemPackages =
+      with pkgs;
+      [
+        # Gaming utilities
+        # lutris # TODO: Re-enable when allegro CMake issue is fixed
+        heroic
+        bottles
+        wine
+        winetricks
+
+        # Performance tools
+        gamescope
+        mangohud
+        gamemode
+      ]
+      ++ lib.optionals (inputs ? nix-gaming) [
+        inputs.nix-gaming.packages.${pkgs.system}.wine-ge
+      ];
+
+    # Enable 32-bit support for games
+    hardware.graphics.enable32Bit = true;
   };
-
-  # Gaming packages
-  environment.systemPackages =
-    with pkgs;
-    [
-      # Gaming utilities
-      # lutris # TODO: Re-enable when allegro CMake issue is fixed
-      heroic
-      bottles
-      wine
-      winetricks
-
-      # Performance tools
-      gamescope
-      mangohud
-      # gamemode is now handled by programs.gamemode above
-    ]
-    ++ lib.optionals (inputs ? nix-gaming) [
-      inputs.nix-gaming.packages.${pkgs.system}.wine-ge
-    ];
-
-  # Enable 32-bit support for games
-  hardware.graphics.enable32Bit = true;
-
-  # Add user to gamemode group
-  users.groups.gamemode.members = [ "zeev" ];
 }
