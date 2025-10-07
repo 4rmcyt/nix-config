7733defdb24b4d715d05545e76921a91cb0a468d
4rmcyt <4rmcyt@gmail.com>
2025-10-07 11:31:06 -0600

    fix: reorganize gamemode configuration for improved clarity and GPU access

--- modules/gaming/default.nix
+++ modules/gaming/default.nix
@@ -27,35 +27,55 @@
         apply_gpu_optimisations = "accept-responsibility";
         nvidia_performance_level = "high";
         # gpu_device = 0; # Let gamemode auto-detect the GPU
+      };
 
-        custom = {
-          start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
-          end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
-        };
+      # Move custom section to top level (outside of gpu)
+      custom = {
+        start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+        end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
       };
     };
-
-    # Gaming packages
-    environment.systemPackages =
-      with pkgs;
-      [
-        # Gaming utilities
-        # lutris # TODO: Re-enable when allegro CMake issue is fixed
-        heroic
-        bottles
-        wine
-        winetricks
-
-        # Performance tools
-        gamescope
-        mangohud
-        gamemode
-      ]
-      ++ lib.optionals (inputs ? nix-gaming) [
-        inputs.nix-gaming.packages.${pkgs.system}.wine-ge
-      ];
-
-    # Enable 32-bit support for games
-    hardware.graphics.enable32Bit = true;
   };
+
+  # Gaming packages
+  environment.systemPackages =
+    with pkgs;
+    [
+      # Gaming utilities
+      # lutris # TODO: Re-enable when allegro CMake issue is fixed
+      heroic
+      bottles
+      wine
+      winetricks
+
+      # Performance tools
+      gamescope
+      mangohud
+      gamemode
+    ]
+    ++ lib.optionals (inputs ? nix-gaming) [
+      inputs.nix-gaming.packages.${pkgs.system}.wine-ge
+    ];
+
+  # Enable 32-bit support for games
+  hardware.graphics.enable32Bit = true;
+
+  # Add udev rules for gamemode GPU access
+  services.udev.extraRules = ''
+    # Allow gamemode to access GPU vendor information
+    KERNEL=="card[0-9]*", SUBSYSTEM=="drm", GROUP="video", MODE="0664"
+    KERNEL=="controlD[0-9]*", SUBSYSTEM=="drm", GROUP="video", MODE="0664"
+
+    # NVIDIA specific rules
+    KERNEL=="nvidia*", GROUP="video", MODE="0664"
+    KERNEL=="nvidiactl", GROUP="video", MODE="0664"
+    KERNEL=="nvidia-modeset", GROUP="video", MODE="0664"
+    KERNEL=="nvidia-uvm", GROUP="video", MODE="0664"
+  '';
+
+  # Ensure your user is in the video group
+  users.users.zeev.extraGroups = [
+    "video"
+    "gamemode"
+  ];
 }
