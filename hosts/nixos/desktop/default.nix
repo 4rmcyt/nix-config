bd7a613dd366f224a2cd1cc82e452b5098b46eea
4rmcyt <4rmcyt@gmail.com>
2025-10-07 11:02:04 -0600

    fix: reorder cachix substituters for improved configuration consistency

--- hosts/nixos/desktop/default.nix
+++ hosts/nixos/desktop/default.nix
@@ -110,10 +110,10 @@
     settings = {
       substituters = [
         "https://cache.nixos.org"
-        "https://nix-community.cachix.org"
-        "https://nix-gaming.cachix.org"
         "https://4rmcyt.cachix.org"
+        "https://nix-gaming.cachix.org"
         "https://cuda-maintainers.cachix.org"
+        "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
