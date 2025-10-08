8f54a3a7a7b36199e194ef0713b8b1cd6f9afdb7
4rmcyt <4rmcyt@gmail.com>
2025-10-08 14:17:08 -0600

    fix: add microcode-intel package and update kernel package selection logic for ZFS compatibility

--- hosts/nixos/homeserver/hardware-configuration.nix
+++ hosts/nixos/homeserver/hardware-configuration.nix
@@ -5,6 +5,19 @@
   modulesPath,
   ...
 }:
+let
+  zfsCompatibleKernelPackages = lib.filterAttrs (
+    name: kernelPackages:
+    (builtins.match "linux_[0-9]+_[0-9]+" name) != null
+    && (builtins.tryEval kernelPackages).success
+    && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
+  ) pkgs.linuxKernel.packages;
+  latestKernelPackage = lib.last (
+    lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
+      builtins.attrValues zfsCompatibleKernelPackages
+    )
+  );
+in
 {
   # =================================================================
   # 1. Imports
@@ -28,7 +41,7 @@
     };
 
     # Kernel configuration
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = latestKernelPackage;
     supportedFilesystems = [
       "vfat"
       "zfs"
