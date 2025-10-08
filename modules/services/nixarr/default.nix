4b4e890f4dde0462f29c9f9b0eb9ed82d1eae7d2
4rmcyt <4rmcyt@gmail.com>
2025-10-08 14:36:10 -0600

    fix: remove add-trackers.sh script from Nixarr configuration

--- modules/services/nixarr/default.nix
+++ modules/services/nixarr/default.nix
@@ -249,31 +249,7 @@ in
     pkgs.jellyfin-ffmpeg
   ];
 
-  environment.etc."nixos/scripts/add-trackers.sh" = {
-    mode = "0755";
-    text = ''
-      #!${pkgs.stdenv.shell}
-
-      TRANSMISSION_REMOTE="${pkgs.transmission}/bin/transmission-remote"
-      WGET="${pkgs.wget}/bin/wget"
-      SED="${pkgs.gnused}/bin/sed"
-      WC="${pkgs.coreutils}/bin/wc"
-
-      TRACKERLIST="/tmp/trackers.list"
-      trap "rm -f $TRACKERLIST" EXIT
-
-      $WGET https://newtrackon.com/api/stable -O "$TRACKERLIST"
-      $WGET https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt -O - >> "$TRACKERLIST"
-
-      $SED -i '/^$/d' "$TRACKERLIST"
-      echo "[+] Got $($WC -l < "$TRACKERLIST") trackers"
-
-      while IFS= read -r TRACKER; do
-        "$TRANSMISSION_REMOTE" -t all -td "$TRACKER"
-      done < "$TRACKERLIST"
-    '';
-  };
-
+  
   nixarr = {
     enable = true;
     mediaUsers = [ "zeev" ];
