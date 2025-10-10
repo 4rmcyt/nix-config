e2fb3f9659eec7dda6b6f6ef184af14a8e9e779c
4rmcyt <4rmcyt@gmail.com>
2025-10-10 11:53:52 -0600

    fix: remove unused Movie-web extension for improved performance

--- modules/GUI/firefox/extensions.nix
+++ modules/GUI/firefox/extensions.nix
@@ -39,12 +39,6 @@ in
       origins = [ "<all_urls>" ];
     };
 
-    # Movie-web
-    "{b0a674f9-f848-9cfd-0feb-583d211308b0}" = {
-      permissions = [ "<all_urls>" ];
-      origins = [ "<all_urls>" ];
-    };
-
     # GDPR/Cookie consent
     "gdpr@cavi.au.dk" = {
       permissions = [ "<all_urls>" ];
