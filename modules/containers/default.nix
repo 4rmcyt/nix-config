65947baefa5bc6ac65033ad95b75ae8446763acc
4rmcyt <4rmcyt@gmail.com>
2025-10-10 10:47:48 -0600

    fix: change format of linkwarden_env configuration from dotenv to yaml

--- modules/containers/default.nix
+++ modules/containers/default.nix
@@ -24,7 +24,7 @@
       owner = config.users.users.podman.name;
       group = config.users.groups.podman.name;
       mode = "0400";
-      format = "dotenv";
+      format = "yaml";
     };
   };
 
