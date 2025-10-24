b5e8b312ce8575e4df61d215e6d90e53f691d562
4rmcyt <4rmcyt@gmail.com>
2025-10-24 11:53:26 -0600

    fix(postgresql): update PostgreSQL host configuration to allow connections from podman network

--- modules/database/postgresql/default.nix
+++ modules/database/postgresql/default.nix
@@ -142,7 +142,7 @@
       # Require a password for network connections from localhost (both IPv4 and IPv6)
       host  all all 127.0.0.1/32 scram-sha-256
       host  all all ::1/128      scram-sha-256
-      host  all all 10.88.0.1/16 scram-sha-256 # Allow connections from podman containers
+      host  all all ${config.virtualisation.podman.networks."podman".subnet} scram-sha-256
     '';
   };
 
