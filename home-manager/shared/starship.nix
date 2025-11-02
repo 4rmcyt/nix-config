2920dc6e90ffb09ae49cd612821311c09b48099d
4rmcyt <4rmcyt@gmail.com>
2025-11-02 09:00:54 -0700

    tokyoNightSettings

--- home-manager/shared/starship.nix
+++ home-manager/shared/starship.nix
@@ -1,89 +1,140 @@
+{ pkgs, lib, ... }:
+let
+  # Set to true to use Tokyo Night theme, false for default theme
+  useTokyoNight = true;
+
+  defaultSettings = {
+    add_newline = true;
+    command_timeout = 500;
+    scan_timeout = 30;
+    aws = {
+      disabled = true;
+    };
+    docker_context = {
+      disabled = true;
+    };
+    gcloud = {
+      disabled = true;
+    };
+    memory_usage = {
+      disabled = true;
+      threshold = -1;
+      style = "bold dimmed green";
+    };
+    sudo = {
+      disabled = false;
+    };
+    vagrant = {
+      disabled = true;
+    };
+  };
+
+  tokyoNightSettings = {
+    add_newline = true;
+    command_timeout = 500;
+    scan_timeout = 30;
+
+    format = lib.concatStrings [
+      "$os"
+      "[](bg:v_secondary fg:v_primary)"
+      "$directory"
+      "[ ](bg:v_base fg:v_secondary)"
+      "$git_branch"
+      "$git_status"
+      "[](fg:v_base)"
+      "$line_break$character"
+    ];
+
+    right_format = lib.concatStrings [
+      "$localip"
+    ];
+
+    palette = "tokyo-night";
+
+    palettes.tokyo-night = {
+      git-green = "#6CA53A";
+      v_base = "#202334";
+      v_primary = "#83A1F1";
+      v_secondary = "#3D425F";
+      v_tertiary = "#3D4A6D";
+      t_base = "#769ff0";
+      t_primary = "#1E202E";
+      t_secondary = "#7892DA";
+      t_tertiary = "#D9DBE3";
+    };
+
+    os = {
+      disabled = false;
+      format = "[  $symbol  ](bg:v_primary fg:t_primary)";
+      symbols = {
+        Arch = "";
+        CentOS = "";
+        Debian = "";
+        Fedora = "";
+        FreeBSD = "";
+        Gentoo = "";
+        Linux = "";
+        Macos = "";
+        Mint = "";
+        NixOS = "";
+        Ubuntu = "";
+        Unknown = "";
+        Windows = "";
+      };
+    };
+
+    localip = {
+      ssh_only = true;
+      format = "[ $localipv4 ](bg:v_primary fg:t_primary bold)";
+      disabled = false;
+    };
+
+    directory = {
+      format = "[  $path ](bg:v_secondary fg:t_secondary)";
+      truncation_length = 3;
+      truncation_symbol = "…/";
+      substitutions = {
+        "Documents" = "󰈙 ";
+        "Downloads" = " ";
+        "Music" = "󰝚 ";
+        "Pictures" = " ";
+      };
+    };
+
+    git_branch = {
+      symbol = "";
+      format = "[[$symbol $branch ](fg:git-green bg:v_base bold)](bg:v_base)";
+    };
+
+    git_status = {
+      format = "[[($all_status$ahead_behind )](fg:t_base bg:v_base)](bg:v_base$style)";
+    };
+
+    aws = {
+      disabled = true;
+    };
+    docker_context = {
+      disabled = true;
+    };
+    gcloud = {
+      disabled = true;
+    };
+    memory_usage = {
+      disabled = true;
+      threshold = -1;
+      style = "bold dimmed green";
+    };
+    sudo = {
+      disabled = false;
+    };
+    vagrant = {
+      disabled = true;
+    };
+  };
+in
 {
   programs.starship = {
     enable = true;
-
-    settings = {
-      add_newline = false;
-
-      directory = {
-        style = "purple";
-        read_only = " ro";
-      };
-
-      git_branch = {
-        style = "yellow";
-        symbol = "";
-      };
-
-      character = {
-        success_symbol = "[>](red)[>](green)[>](blue)";
-        error_symbol = "[>](cyan)[>](purple)[>](yellow)";
-        vicmd_symbol = "[<](bold green)";
-      };
-
-      line_break.disabled = true;
-
-      nodejs = {
-        format = "with [$symbol($version )]($style)";
-        symbol = "node ";
-        version_format = "\${major}";
-        disabled = true;
-      };
-
-      git_commit.tag_symbol = " tag ";
-
-      git_status = {
-        ahead = ">";
-        behind = "<";
-        diverged = "<>";
-        renamed = "r";
-        deleted = "x";
-      };
-
-      aws.symbol = "aws ";
-      cobol.symbol = "cobol ";
-      conda.symbol = "conda ";
-      crystal.symbol = "cr ";
-      cmake.symbol = "cmake ";
-      dart.symbol = "dart ";
-      deno.symbol = "deno ";
-      dotnet.symbol = ".NET ";
-      docker_context.symbol = "docker ";
-      elixir.symbol = "exs ";
-      elm.symbol = "elm ";
-      golang.symbol = "go ";
-      hg_branch.symbol = "hg ";
-      java.symbol = "java ";
-      julia.symbol = "jl ";
-      kotlin.symbol = "kt ";
-      memory_usage.symbol = "memory ";
-      nim.symbol = "nim ";
-
-      nix_shell = {
-        format = "❄️ ";
-        symbol = "nix ";
-      };
-
-      ocaml.symbol = "ml ";
-      package.symbol = "pkg ";
-      perl.symbol = "pl ";
-      php.symbol = "php ";
-      purescript.symbol = "purs ";
-      python.symbol = "python ";
-      ruby.symbol = "ruby ";
-
-      rust = {
-        symbol = "rust ";
-        disabled = true;
-      };
-
-      bun = {
-        symbol = "bun ";
-        disabled = true;
-      };
-
-      scala.symbol = "scala ";
-      swift.symbol = "swift ";
-    };
+    settings = if useTokyoNight then tokyoNightSettings else defaultSettings;
   };
 }
