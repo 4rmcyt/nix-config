5f001eb3b7b4d5af9c82b703055ac6c990f85780
4rmcyt <4rmcyt@gmail.com>
2025-11-04 09:58:40 -0700

    Refactor defaults configuration: read secrets from files to remove suffix for cleaner values

--- modules/options/defaults.nix
+++ modules/options/defaults.nix
@@ -8,73 +8,73 @@ in {
   options.my.defaults = {
     user = lib.mkOption {
       type = lib.types.str;
-      default = config.sops.secrets.defaults-user.path;
+      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-user.path);
       description = "Primary username for the system";
     };
 
     email = lib.mkOption {
       type = lib.types.str;
-      default = config.sops.secrets.defaults-email.path;
+      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-email.path);
       description = "Primary email address";
     };
 
     gitUsername = lib.mkOption {
       type = lib.types.str;
-      default = config.sops.secrets.defaults-git_username.path;
+      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-git_username.path);
       description = "Git username";
     };
 
     gitSigningKey = lib.mkOption {
       type = lib.types.str;
-      default = config.sops.secrets.defaults-git_signing_key.path;
+      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-git_signing_key.path);
       description = "GPG key ID for git commit signing";
     };
 
     domain = lib.mkOption {
       type = lib.types.str;
-      default = config.sops.secrets.defaults-domain.path;
+      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-domain.path);
       description = "Primary domain for homeserver services";
     };
 
     timezone = lib.mkOption {
       type = lib.types.str;
-      default = config.sops.secrets.defaults-timezone.path;
+      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-timezone.path);
       description = "System timezone";
     };
 
     locale = lib.mkOption {
       type = lib.types.str;
-      default = config.sops.secrets.defaults-locale.path;
+      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-locale.path);
       description = "System locale";
     };
 
     gateway = lib.mkOption {
       type = lib.types.str;
-      default = config.sops.secrets.defaults-gateway.path;
+      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-gateway.path);
       description = "Default gateway IP address";
     };
 
     homeserver_lan = lib.mkOption {
       type = lib.types.str;
-      default = config.sops.secrets.defaults-homeserver_lan.path;
+      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-homeserver_lan.path);
       description = "Local IP address of the homeserver";
     };
 
     desktop_lan = lib.mkOption {
       type = lib.types.str;
-      default = config.sops.secrets.defaults-desktop_lan.path;
+      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-desktop_lan.path);
       description = "Local IP address of the desktop LAN connection";
     };
 
     desktop_wifi = lib.mkOption {
       type = lib.types.str;
-      default = config.sops.secrets.defaults-desktop_wifi.path;
+      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-desktop_wifi.path);
       description = "Local IP address of the desktop WiFi connection";
     };
 
     matebook_wifi = lib.mkOption {
       type = lib.types.str;
-      default = config.sops.secrets.defaults-matebook_wifi.path;
+      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-matebook_wifi.path);
       description = "Local IP address of the Matebook WiFi connection";
     };
   };
