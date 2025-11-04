256fac91f2748254c5c0d0c3c503eff2206e88d5
4rmcyt <4rmcyt@gmail.com>
2025-11-04 09:26:25 -0700

    Refactor defaults configuration: simplify sops secrets handling and remove unnecessary file reading

--- modules/options/defaults.nix
+++ modules/options/defaults.nix
@@ -1,88 +1,80 @@
-{lib, config, ...}: {
+{lib, ...}:
+  let
+  secretsFile = ../../../secrets/common.yaml;
+in
+{
+  sops.secrets = builtins.listToAttrs (map (key: {
+    name = "defaults-${key}";
+    value = {
+      sopsFile = secretsFile;
+      key = key;
+      mode = "0400";
+    };
+  }) [
+    "user"
+    "email"
+    "git_username"
+    "git_signing_key"
+    "domain"
+    "local_ip"
+    "timezone"
+    "locale"
+  ]);
+
+
+
   options.my.defaults = {
     user = lib.mkOption {
       type = lib.types.str;
-      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-user.path);
+      default = config.sops.secrets.defaults-user.path;
       description = "Primary username for the system";
     };
 
     email = lib.mkOption {
       type = lib.types.str;
-      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-email.path);
+      default = config.sops.secrets.defaults-email.path;
       description = "Primary email address";
     };
 
     gitUsername = lib.mkOption {
       type = lib.types.str;
-      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-git_username.path);
+      default = config.sops.secrets.defaults-git_username.path;
       description = "Git username";
     };
 
     gitSigningKey = lib.mkOption {
       type = lib.types.str;
-      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-git_signing_key.path);
+      default = config.sops.secrets.defaults-git_signing_key.path;
       description = "GPG key ID for git commit signing";
     };
 
     domain = lib.mkOption {
       type = lib.types.str;
-      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-domain.path);
+      default = config.sops.secrets.defaults-domain.path;
       description = "Primary domain for homeserver services";
     };
 
     localIp = lib.mkOption {
       type = lib.types.str;
-      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-local_ip.path);
+      default = config.sops.secrets.defaults-local_ip.path;
       description = "Local IP address of the homeserver";
     };
 
     timezone = lib.mkOption {
       type = lib.types.str;
-      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-timezone.path);
+      default = config.sops.secrets.defaults-timezone.path;
       description = "System timezone";
     };
 
     locale = lib.mkOption {
       type = lib.types.str;
-      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-locale.path);
+      default = config.sops.secrets.defaults-locale.path;
       description = "System locale";
     };
   };
 
   config = {
-    sops.secrets = {
-      defaults-user = {
-        sopsFile = ../../secrets/common.yaml;
-        key = "user";
-      };
-      defaults-email = {
-        sopsFile = ../../secrets/common.yaml;
-        key = "email";
-      };
-      defaults-git_username = {
-        sopsFile = ../../secrets/common.yaml;
-        key = "git_username";
-      };
-      defaults-git_signing_key = {
-        sopsFile = ../../secrets/common.yaml;
-        key = "git_signing_key";
-      };
-      defaults-domain = {
-        sopsFile = ../../secrets/common.yaml;
-        key = "domain";
-      };
-      defaults-local_ip = {
-        sopsFile = ../../secrets/common.yaml;
-        key = "local_ip";
-      };
-      defaults-timezone = {
-        sopsFile = ../../secrets/common.yaml;
-        key = "timezone";
-      };
-      defaults-locale = {
-        sopsFile = ../../secrets/common.yaml;
-        key = "locale";
-      };
-    };
+    # Set the default values as configuration
+    # Other modules can reference config.my.defaults.*
   };
 }
