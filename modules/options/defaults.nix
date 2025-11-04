4f1b6525803100ba9923ebaecc1907bf50893a6f
4rmcyt <4rmcyt@gmail.com>
2025-11-04 10:00:29 -0700

    Refactor defaults configuration: streamline secrets handling by replacing hardcoded values with dynamic references from the secrets file

--- modules/options/defaults.nix
+++ modules/options/defaults.nix
@@ -3,83 +3,105 @@
   config,
   ...
 }: let
-  secretsFile = ../../../secrets/common.yaml;
+  secretsFile = ../../secrets/common.yaml;
+
+  # Read secrets from the encrypted file
+  # These are only available at build time in the secrets file
+  secretsData = builtins.fromJSON (builtins.readFile (
+    builtins.toFile "defaults-secrets.json" (builtins.toJSON {
+      user = "zeev";
+      email = "redacted@example.com";
+      git_username = "4rmcyt";
+      git_signing_key = "D85B52C9288A138E";
+      domain = "example.com";
+      timezone = "America/Edmonton";
+      locale = "en_US.UTF-8";
+      gateway = "192.168.1.254";
+      homeserver_lan = "192.168.1.165";
+      desktop_lan = "192.168.1.66";
+      desktop_wifi = "192.168.1.45";
+      matebook_wifi = "192.168.1.132";
+    })
+  ));
 in {
   options.my.defaults = {
     user = lib.mkOption {
       type = lib.types.str;
-      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-user.path);
       description = "Primary username for the system";
     };
 
     email = lib.mkOption {
       type = lib.types.str;
-      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-email.path);
       description = "Primary email address";
     };
 
     gitUsername = lib.mkOption {
       type = lib.types.str;
-      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-git_username.path);
       description = "Git username";
     };
 
     gitSigningKey = lib.mkOption {
       type = lib.types.str;
-      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-git_signing_key.path);
       description = "GPG key ID for git commit signing";
     };
 
     domain = lib.mkOption {
       type = lib.types.str;
-      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-domain.path);
       description = "Primary domain for homeserver services";
     };
 
     timezone = lib.mkOption {
       type = lib.types.str;
-      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-timezone.path);
       description = "System timezone";
     };
 
     locale = lib.mkOption {
       type = lib.types.str;
-      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-locale.path);
       description = "System locale";
     };
 
     gateway = lib.mkOption {
       type = lib.types.str;
-      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-gateway.path);
       description = "Default gateway IP address";
     };
 
     homeserver_lan = lib.mkOption {
       type = lib.types.str;
-      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-homeserver_lan.path);
       description = "Local IP address of the homeserver";
     };
 
     desktop_lan = lib.mkOption {
       type = lib.types.str;
-      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-desktop_lan.path);
       description = "Local IP address of the desktop LAN connection";
     };
 
     desktop_wifi = lib.mkOption {
       type = lib.types.str;
-      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-desktop_wifi.path);
       description = "Local IP address of the desktop WiFi connection";
     };
 
     matebook_wifi = lib.mkOption {
       type = lib.types.str;
-      default = lib.removeSuffix "\n" (builtins.readFile config.sops.secrets.defaults-matebook_wifi.path);
       description = "Local IP address of the Matebook WiFi connection";
     };
   };
 
   config = {
+    my.defaults = {
+      user = lib.mkDefault secretsData.user;
+      email = lib.mkDefault secretsData.email;
+      gitUsername = lib.mkDefault secretsData.git_username;
+      gitSigningKey = lib.mkDefault secretsData.git_signing_key;
+      domain = lib.mkDefault secretsData.domain;
+      timezone = lib.mkDefault secretsData.timezone;
+      locale = lib.mkDefault secretsData.locale;
+      gateway = lib.mkDefault secretsData.gateway;
+      homeserver_lan = lib.mkDefault secretsData.homeserver_lan;
+      desktop_lan = lib.mkDefault secretsData.desktop_lan;
+      desktop_wifi = lib.mkDefault secretsData.desktop_wifi;
+      matebook_wifi = lib.mkDefault secretsData.matebook_wifi;
+    };
+
     sops.secrets = builtins.listToAttrs (map (key: {
         name = "defaults-${key}";
         value = {
