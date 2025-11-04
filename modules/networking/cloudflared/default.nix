f2923e4e7695bc67e804e9a99888777786d19ecb
4rmcyt <4rmcyt@gmail.com>
2025-11-04 08:52:05 -0700

    Refactor cloudflared configuration: streamline tunnel ID handling and expand ingress rules for additional services

--- modules/networking/cloudflared/default.nix
+++ modules/networking/cloudflared/default.nix
@@ -1,56 +1,100 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   sops.secrets = {
     cloudflare_tunnel_credentials = {
       sopsFile = ../../../secrets/cloudflare_tunnel_credentials.bin;
       key = "credentials";
-      owner = config.users.users.cloudflared.name;
-      group = config.users.groups.cloudflared.name;
+      owner = "cloudflared";
+      group = "cloudflared";
       mode = "0400";
       format = "binary";
     };
 
+    cloudflare_tunnel_id = {
+      sopsFile = ../../../secrets/cloudflare.yaml;
+      key = "cloudflare_tunnel_id";
+      owner = "cloudflared";
+      group = "cloudflared";
+      mode = "0400";
+    };
+  };
+
+  sops.templates."cloudflared-config.yml" = {
+    owner = "cloudflared";
+    group = "cloudflared";
+    mode = "0400";
+    content = ''
+      tunnel: ${config.sops.placeholder.cloudflare_tunnel_id}
+      credentials-file: ${config.sops.secrets.cloudflare_tunnel_credentials.path}
+
+      ingress:
+        - hostname: jellyfin.${config.my.defaults.domain}
+          service: http://localhost:8096
+        - hostname: paperless.${config.my.defaults.domain}
+          service: http://localhost:8888
+        - hostname: home.${config.my.defaults.domain}
+          service: http://localhost:8082
+        - hostname: hass.${config.my.defaults.domain}
+          service: http://localhost:8123
+        - hostname: miniflux.${config.my.defaults.domain}
+          service: http://localhost:8086
+        - hostname: transmission.${config.my.defaults.domain}
+          service: http://192.168.1.165:9091
+        - hostname: cal.${config.my.defaults.domain}
+          service: http://localhost:5232
+        - hostname: audiobookshelf.${config.my.defaults.domain}
+          service: http://localhost:9292
+        - hostname: kavita.${config.my.defaults.domain}
+          service: http://localhost:5000
+        - hostname: prowlarr.${config.my.defaults.domain}
+          service: http://localhost:9696
+        - hostname: radarr.${config.my.defaults.domain}
+          service: http://localhost:7878
+        - hostname: sonarr.${config.my.defaults.domain}
+          service: http://localhost:8989
+        - hostname: lidarr.${config.my.defaults.domain}
+          service: http://localhost:8686
+        - hostname: bazarr.${config.my.defaults.domain}
+          service: http://localhost:6767
+        - hostname: jellyseerr.${config.my.defaults.domain}
+          service: http://localhost:5055
+        - hostname: ollama.${config.my.defaults.domain}
+          service: http://localhost:11434
+        - hostname: vault.${config.my.defaults.domain}
+          service: http://localhost:8222
+        - hostname: kuma.${config.my.defaults.domain}
+          service: http://localhost:3001
+        - hostname: auth.${config.my.defaults.domain}
+          service: http://localhost:9000
+        - hostname: grafana.${config.my.defaults.domain}
+          service: http://localhost:3003
+        - hostname: tdarr.${config.my.defaults.domain}
+          service: http://localhost:8265
+        - hostname: readarr.${config.my.defaults.domain}
+          service: http://localhost:8787
+        - hostname: link.${config.my.defaults.domain}
+          service: http://localhost:3000
+        - service: http_status:404
+    '';
   };
 
   users.users.cloudflared = {
     isSystemUser = true;
     group = "cloudflared";
-    extraGroups = [ "users" ];
   };
   users.groups.cloudflared = { };
 
-  services.cloudflared = {
-    enable = true;
-    tunnels = {
-      "f7876e26-87a8-4bdd-9798-3986b0f7cebc" = {
-        credentialsFile = config.sops.secrets.cloudflare_tunnel_credentials.path;
-        default = "http_status:404";
-        ingress = {
-          "jellyfin.${config.my.defaults.domain}" = "http://localhost:8096";
-          "paperless.${config.my.defaults.domain}" = "http://localhost:8888";
-          "home.${config.my.defaults.domain}" = "http://localhost:8082";
-          "hass.${config.my.defaults.domain}" = "http://localhost:8123";
-          "miniflux.${config.my.defaults.domain}" = "http://localhost:8086";
-          "transmission.${config.my.defaults.domain}" = "http://192.168.1.165:9091";
-          "cal.${config.my.defaults.domain}" = "http://localhost:5232";
-          "audiobookshelf.${config.my.defaults.domain}" = "http://localhost:9292";
-          "kavita.${config.my.defaults.domain}" = "http://localhost:5000";
-          "prowlarr.${config.my.defaults.domain}" = "http://localhost:9696";
-          "radarr.${config.my.defaults.domain}" = "http://localhost:7878";
-          "sonarr.${config.my.defaults.domain}" = "http://localhost:8989";
-          "lidarr.${config.my.defaults.domain}" = "http://localhost:8686";
-          "bazarr.${config.my.defaults.domain}" = "http://localhost:6767";
-          "jellyseerr.${config.my.defaults.domain}" = "http://localhost:5055";
-          "ollama.${config.my.defaults.domain}" = "http://localhost:11434";
-          "vault.${config.my.defaults.domain}" = "http://localhost:8222";
-          "kuma.${config.my.defaults.domain}" = "http://localhost:3001";
-          "auth.${config.my.defaults.domain}" = "http://localhost:9000";
-          "grafana.${config.my.defaults.domain}" = "http://localhost:3003";
-          "tdarr.${config.my.defaults.domain}" = "http://localhost:8265";
-          "readarr.${config.my.defaults.domain}" = "http://localhost:8787";
-          "link.${config.my.defaults.domain}" = "http://localhost:3000";
-        };
-      };
+  systemd.services.cloudflared = {
+    after = ["network.target" "network-online.target" "sops-nix.service"];
+    wants = ["network.target" "network-online.target"];
+    wantedBy = ["multi-user.target"];
+
+    serviceConfig = {
+      User = "cloudflared";
+      Group = "cloudflared";
+      ExecStart = "${pkgs.cloudflared}/bin/cloudflared tunnel --config ${config.sops.templates."cloudflared-config.yml".path} --no-autoupdate run";
+      Restart = "on-failure";
+      RestartSec = "5s";
     };
   };
 }
