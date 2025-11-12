00479c892759538c59a8fd5eecd002852cb4ad1a
4rmcyt <4rmcyt@gmail.com>
2025-11-12 16:36:26 -0700

    sds

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -7,7 +7,24 @@
     ./msmtp
   ];
 
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+      "en_CA.UTF-8/UTF-8"
+    ];
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_CA.UTF-8";
+      LC_IDENTIFICATION = "en_CA.UTF-8";
+      LC_MEASUREMENT = "en_CA.UTF-8";
+      LC_MONETARY = "en_CA.UTF-8";
+      LC_NAME = "en_CA.UTF-8";
+      LC_NUMERIC = "en_CA.UTF-8";
+      LC_PAPER = "en_CA.UTF-8";
+      LC_TELEPHONE = "en_CA.UTF-8";
+      LC_TIME = "en_CA.UTF-8";
+    };
+  };
 
   services.timesyncd = {
     enable = true;
