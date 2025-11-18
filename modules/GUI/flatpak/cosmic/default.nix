ce16b174e267ae9c7d8158d5bd1e876253adfd1e
4rmcyt <4rmcyt@gmail.com>
2025-11-18 11:48:53 -0700

    klkn

--- modules/GUI/flatpak/cosmic/default.nix
+++ modules/GUI/flatpak/cosmic/default.nix
@@ -6,16 +6,17 @@ _: {
       "flathub-beta" = "https://dl.flathub.org/beta-repo/flathub-beta.flatpakrepo";
     };
     packages = [
-      "flathub:/root/testflatpak.flatpakref"
-      "flathub:app/com.github.iwalton3.jellyfin-media-player//latest"
+      "flathub:app/com.github.iwalton3.jellyfin-media-player//stable"
       # Gaming: 32-bit graphics support for Lutris
       "flathub:runtime/org.freedesktop.Platform.GL32.default/x86_64/24.08"
+      "flathub:runtime/org.freedesktop.Platform.GL32.nvidia-580-105-08/x86_64/1.4"
       "flathub:runtime/org.freedesktop.Platform.GL32.nvidia-580-105-08/x86_64/1.4"
       "flathub:runtime/org.gnome.Platform.Compat.i386/x86_64/46"
       # Gaming tools for Lutris
       "flathub:runtime/org.freedesktop.Platform.VulkanLayer.gamescope/x86_64/24.08"
       "flathub:runtime/org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/24.08"
     ];
+
     # Note: Flatpak overrides for Lutris are applied via flatpak override commands:
     # flatpak override --user net.lutris.Lutris --filesystem=/run/current-system/sw/bin:ro
     # flatpak override --user net.lutris.Lutris --filesystem=~/.steam:ro --filesystem=~/.local/share/Steam:ro --filesystem=xdg-data/Steam:ro
