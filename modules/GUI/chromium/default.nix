4d01d4ae6706ddaa1cd798987e5ae566d87bc9d7
4rmcyt <4rmcyt@gmail.com>
2025-11-23 14:06:08 -0700

    fdf

--- modules/GUI/chromium/default.nix
+++ modules/GUI/chromium/default.nix
@@ -3,8 +3,9 @@
   ...
 }:
 {
-  home.packages = with pkgs; [
-    (chromium.override {
+  programs.chromium = {
+    enable = true;
+    package = pkgs.chromium.override {
       enableWideVine = true;
       commandLineArgs = [
         "--enable-features=VaapiVideoDecodeLinuxGL,VaapiVideoEncoder,Vulkan,VulkanFromANGLE,DefaultANGLEVulkan,VaapiIgnoreDriverChecks,VaapiVideoDecoder,PlatformHEVCDecoderSupport,UseMultiPlaneFormatForHardwareVideo"
@@ -13,13 +14,6 @@
         "--enable-features=UseOzonePlatform"
         "--ozone-platform=wayland"
       ];
-    })
-  ];
-
-  programs.chromium = {
-    enable = true;
-    extraOpts = {
-      "ExtensionManifestV2Availability" = 2;
     };
     extensions = [
       "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
