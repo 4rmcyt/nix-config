fbae218b3cb90d8250e7ebdf7ec7de8661ae6dc3
4rmcyt <4rmcyt@gmail.com>
2025-12-02 17:12:33 -0700

    fix: remove commented-out service user definitions for clarity

--- modules/database/redis/default.nix
+++ modules/database/redis/default.nix
@@ -14,13 +14,6 @@
     # OAuth2 Proxy user - database 0
     # Permissions: all commands except dangerous ones, all keys, select db 0
     user oauth2-proxy on #${config.sops.secrets.redis-oauth2-proxy-password.path} ~* &* +@all -@dangerous resetchannels resetkeys
-
-    # Add more service users here when needed:
-    # Paperless user - database 1
-    # user paperless on #${config.sops.secrets.redis-paperless-password.path} ~paperless:* &* +@all -@dangerous resetchannels resetkeys
-
-    # Authentik user - database 2
-    # user authentik on #${config.sops.secrets.redis-authentik-password.path} ~* &* +@all -@dangerous resetchannels resetkeys
   '';
 in {
   # =================================================================
@@ -183,4 +176,3 @@ in {
 # 3. Add service to users.groups.redis.members
 # 4. Add service group to users.users.redis.extraGroups
 # 5. Configure service to use unix socket with username and password
-
