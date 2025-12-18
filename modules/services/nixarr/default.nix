3ae13a783d3da924cd33dcfb1faf481347acc2a4
4rmcyt <4rmcyt@gmail.com>
2025-12-17 20:26:31 -0700

    sdsds

--- modules/services/nixarr/default.nix
+++ modules/services/nixarr/default.nix
@@ -2,7 +2,8 @@
   lib,
   config,
   ...
-}: let
+}:
+let
   servicesWithMediaAccess = [
     "bazarr"
     "jellyseerr"
@@ -15,11 +16,12 @@
     "audiobookshelf"
     "jellyfin"
   ];
-in {
+in
+{
   imports = [
     ./upnp-fix.nix
-    ./jellyfin
   ];
+  
   # SOPS secrets for nixarr
   sops.secrets = {
     wg_conf = {
@@ -46,7 +48,17 @@ in {
         "media"
       ];
     };
-
+    jellyfin = {
+      isSystemUser = true;
+      group = lib.mkForce "jellyfin";
+      extraGroups = [
+        "users"
+        "media"
+        "render"
+        "video"
+        "input"
+      ];
+    };
     jellyseerr = {
       isSystemUser = true;
       group = lib.mkForce "jellyseerr";
@@ -113,16 +125,17 @@ in {
     };
   };
   users.groups = {
-    audiobookshelf = {};
-    bazarr = {};
-    jellyseerr = {};
-    lidarr = {};
-    prowlarr = {};
-    radarr = {};
-    sonarr = {};
-    transmission = {};
-    readarr = {};
-    recyclarr = {};
+    audiobookshelf = { };
+    bazarr = { };
+    jellyfin = { };
+    jellyseerr = { };
+    lidarr = { };
+    prowlarr = { };
+    radarr = { };
+    sonarr = { };
+    transmission = { };
+    readarr = { };
+    recyclarr = { };
     # headphones = { };
   };
 
@@ -163,9 +176,193 @@ in {
     ];
   };
 
+  # Jellyfin declarative configuration using nixos-jellyfin module
+  services.jellyfin = {
+    enable = true;
+
+    settings = {
+      # Base configuration
+      isStartupWizardCompleted = true;
+      cachePath = "/data/media/.state/nixarr/jellyfin/cache";
+      logFileRetentionDays = 3;
+
+      # Server identity
+      serverName = "homeserver";
+      displayLanguage = "en-US";
+
+      # Hardware acceleration (VAAPI for Intel/AMD)
+      hardwareAccelerationType = "vaapi";
+      vaapiDevice = "/dev/dri/renderD128";
+      enableHardwareEncoding = true;
+      preferSystemNativeHwDecoder = true;
+      hardwareDecodingCodecs = [ "h264" "hevc" "mpeg2video" "vp8" "vp9" "vc1" ];
+
+      # Tone mapping for HDR content
+      enableTonemapping = true;
+      enableVppTonemapping = false;
+      tonemappingAlgorithm = "bt2390";
+      tonemappingMode = "auto";
+      tonemappingRange = "auto";
+      tonemappingDesat = 0;
+      tonemappingPeak = 100;
+      tonemappingParam = 0;
+      vppTonemappingBrightness = 16;
+      vppTonemappingContrast = 1;
+
+      # Encoding quality settings
+      encodingThreadCount = 4;
+      h264Crf = 23;
+      h265Crf = 28;
+      encoderPreset = "superfast";
+      allowHevcEncoding = true;
+      allowAv1Encoding = false;
+
+      # Enable 10-bit codec support
+      enableDecodingColorDepth10Hevc = true;
+      enableDecodingColorDepth10Vp9 = true;
+
+      # Intel low-power encoders
+      enableIntelLowPowerH264HwEncoder = true;
+      enableIntelLowPowerHevcHwEncoder = false;
+
+      # Enhanced decoder
+      enableEnhancedNvdecDecoder = true;
+
+      # Audio settings
+      enableAudioVbr = false;
+      downMixAudioBoost = 2;
+      downMixStereoAlgorithm = "none";
+
+      # Deinterlacing
+      deinterlaceDoubleRate = false;
+      deinterlaceMethod = "yadif";
+
+      # Transcoding settings
+      transcodingTempPath = "/data/media/.state/nixarr/jellyfin/cache/transcodes";
+      maxMuxingQueueSize = 2048;
+      enableThrottling = true;
+      throttleDelaySeconds = 180;
+      enableSegmentDeletion = true;
+      segmentKeepSeconds = 720;
+
+      # Subtitle extraction
+      enableSubtitleExtraction = true;
+      enableFallbackFont = false;
+
+      # Keyframe extraction
+      allowOnDemandMetadataBasedKeyframeExtractionForExtensions = [ "mkv" ];
+
+      # Metadata configuration
+      metadataPath = "/data/media/.state/nixarr/jellyfin/data/metadata";
+      preferredMetadataLanguage = "en";
+      metadataCountryCode = "CA";
+
+      # Library monitoring
+      libraryMonitorDelay = 60;
+      libraryUpdateDuration = 30;
+      libraryScanFanoutConcurrency = 2;
+      libraryMetadataRefreshConcurrency = 0;
+
+      # Playback state tracking
+      minResumePct = 5;
+      maxResumePct = 90;
+      minResumeDurationSeconds = 300;
+      minAudiobookResume = 5;
+      maxAudiobookResume = 5;
+      inactiveSessionThreshold = 0;
+
+      # Images
+      imageSavingConvention = "Legacy";
+      chapterImageResolution = "matchsource";
+      imageExtractionTimeoutMs = 0;
+      parallelImageEncodingLimit = 2;
+      dummyChapterDuration = 0;
+
+      # Network & Security
+      isPortAuthorized = true;
+      quickConnectAvailable = true;
+      corsHosts = [ "*" ];
+      remoteClientBitrateLimit = 120000000;
+
+      # Monitoring & Logging
+      enableMetrics = false;
+      activityLogRetentionDays = 30;
+      enableSlowResponseWarning = true;
+      slowResponseThresholdMs = 500;
+      allowClientLogUpload = true;
+
+      # Feature flags
+      enableNormalizedItemByNameIds = true;
+      enableCaseSensitiveItemIds = true;
+      disableLiveTvChannelUserDataName = true;
+      skipDeserializationForBasicTypes = true;
+      saveMetadataHidden = false;
+      enableFolderView = false;
+      enableGroupingIntoCollections = false;
+      displaySpecialsWithinSeasons = true;
+      enableExternalContentInSuggestions = true;
+
+      # Plugin repositories
+      pluginRepositories = [
+        {
+          name = "Jellyfin Stable";
+          url = "https://repo.jellyfin.org/files/plugin/manifest.json";
+          enabled = true;
+        }
+        {
+          name = "danieladov";
+          url = "https://raw.githubusercontent.com/danieladov/JellyfinPluginManifest/master/manifest.json";
+          enabled = true;
+        }
+        {
+          name = "jellyfin-unstable";
+          url = "https://repo.jellyfin.org/files/plugin-unstable/manifest.json";
+          enabled = true;
+        }
+        {
+          name = "jellyfin-plugin-cinemamode";
+          url = "https://raw.githubusercontent.com/CherryFloors/jellyfin-plugin-cinemamode/main/manifest.json";
+          enabled = true;
+        }
+        {
+          name = "jellyfin-plugin-sso";
+          url = "https://raw.githubusercontent.com/9p4/jellyfin-plugin-sso/manifest-release/manifest.json";
+          enabled = true;
+        }
+        {
+          name = "Intro skipper";
+          url = "https://intro-skipper.org/manifest.json";
+          enabled = true;
+        }
+        {
+          name = "Jellyfin-Enhanced";
+          url = "https://raw.githubusercontent.com/n00bcodr/jellyfin-plugins/main/10.11/manifest.json";
+          enabled = true;
+        }
+        {
+          name = "File Transformation";
+          url = "https://www.iamparadox.dev/jellyfin/plugins/manifest.json";
+          enabled = true;
+        }
+      ];
+
+      # Cast receiver applications
+      castReceiverApplications = [
+        {
+          id = "F007D354";
+          name = "Stable";
+        }
+        {
+          id = "6F511C87";
+          name = "Unstable";
+        }
+      ];
+    };
+  };
+
   nixarr = {
     enable = true;
-    mediaUsers = [config.my.defaults.user];
+    mediaUsers = [ config.my.defaults.user ];
     mediaDir = "/data/media";
     stateDir = "/data/media/.state/nixarr";
 
@@ -197,7 +394,8 @@ in {
         "127.0.0.1"
       ];
       messageLevel = "info";
-      extraSettings = {
+      extraSettings =
+{
         umask = 2;
         download-queue-size = 10;
         download-queue-enabled = true;
@@ -219,7 +417,7 @@ in {
 
     audiobookshelf.enable = true;
     jellyseerr.enable = true;
-    jellyfin.enable = false;
+
     bazarr.enable = true;
     lidarr.enable = true;
     prowlarr.enable = true;
@@ -232,29 +430,32 @@ in {
     };
   };
 
-  systemd.services = lib.genAttrs servicesWithMediaAccess (_serviceName: {
-    serviceConfig = {
-      BindPaths = [
-        "/data/Downloads"
-        "/data/media"
-        "/data/media/movies"
-        "/data/media/audiobooks"
-        "/data/media/music"
-        "/data/media/shows"
-        "/data/media/books"
-        "/data/media/comics"
-        "/data/media/manga"
-        "/data/media/torrents"
-        "/data/media/usenet"
-        "/data/media/audiobooks"
-        "/data/Downloads/radarr"
-        "/data/Downloads/lidarr"
-        "/data/Downloads/tv-sonarr"
-        "/data/media/.state"
-        # "/data/media/torrents/.incomplete"
-      ];
-    };
-  });
+  systemd.services = (
+    lib.genAttrs servicesWithMediaAccess (_serviceName: {
+      serviceConfig = {
+        BindPaths = [
+          "/data/Downloads"
+          "/data/media"
+          "/data/media/movies"
+          "/data/media/audiobooks"
+          "/data/media/music"
+          "/data/media/shows"
+          "/data/media/books"
+          "/data/media/comics"
+          "/data/media/manga"
+          "/data/media/torrents"
+          "/data/media/usenet"
+          "/data/media/audiobooks"
+          "/data/Downloads/radarr"
+          "/data/Downloads/lidarr"
+          "/data/Downloads/tv-sonarr"
+          "/data/media/.state"
+          # "/data/media/torrents/.incomplete"
+        ];
+      };
+    })
+  );
+
 
   systemd.tmpfiles.rules = [
     "d /data 770 root media -"
