4be3b20ecb82f14f8b6370af7284744618e3d78c
4rmcyt <4rmcyt@gmail.com>
2025-12-17 21:16:09 -0700

    sdsd

--- modules/services/nixarr/jellyfin/default.nix
+++ modules/services/nixarr/jellyfin/default.nix
@@ -17,6 +17,11 @@
     ];
   };
 
+  systemd.services.jellyfin = {
+    # Prevent blocking during system activation
+    restartIfChanged = false;
+  };
+
   services.jellyfin = {
     enable = true;
     package = inputs.nixos-jellyfin.packages.${pkgs.system}.jellyfin;
