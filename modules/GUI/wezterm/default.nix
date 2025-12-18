c6c9d7ddb6f1668c876482ba7cd97b3c6f83236b
4rmcyt <4rmcyt@gmail.com>
2025-12-18 10:08:44 -0700

    wezterm config

--- modules/GUI/wezterm/default.nix
+++ modules/GUI/wezterm/default.nix
@@ -3,83 +3,113 @@
   programs.wezterm = {
     enable = true;
     package = pkgs.wezterm;
-
-    # extraConfig = ''
-    #   local wezterm = require 'wezterm'
-    #   local config = wezterm.config_builder()
-    #   -- Color scheme
-    #   config.color_scheme = 'Dracula+'
-    #   config.window_close_confirmation = 'NeverPrompt'
-    #   config.skip_close_confirmation_for_processes_named = {
-    #     'bash',
-    #     'sh',
-    #     'zsh',
-    #     'fish',
-    #     'tmux',
-    #     'nu',
-    #     'zellij',
-    #   }
-    #   return config
-    # '';
     extraConfig = ''
-      local wez = require('wezterm')
-      return {
-        default_prog     = { 'zsh' },
-        cell_width = 0.85,
-        -- Performance
-        --------------
-        front_end        = "WebGpu",
-        enable_wayland   = true,
-        scrollback_lines = 1024,
-        -- Fonts
-        --------
-        font         = wez.font_with_fallback({
-          "Iosevka Nerd Font",
-          "Material Design Icons",
-        }),
-        initial_rows = 18,
-        initial_cols = 85,
-        dpi = 96.0,
-        bold_brightens_ansi_colors = true,
-        font_rules    = {
-          {
-            italic = true,
-            font   = wez.font("Iosevka Nerd Font", { italic = true })
-          }
-        },
-        --font_antialias = "Subpixel",
-        --font_hinting = "VerticalSubpixel",
-        font_size         = 14.0,
-        line_height       = 1.15,
-        harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' },
-        -- Bling
-        --------
-        color_scheme = 'Dracula+',
-        default_cursor_style = "SteadyUnderline",
-        -- Tabbar
-        ---------
-        enable_tab_bar               = true,
-        use_fancy_tab_bar            = true,
-        hide_tab_bar_if_only_one_tab = true,
-        show_tab_index_in_tab_bar    = false,
-        -- Miscelaneous
-        ---------------
-        window_close_confirmation = 'NeverPrompt',
-        skip_close_confirmation_for_processes_named = {
-          'bash',
-          'sh',
-          'zsh',
-          'fish',
-          'tmux',
-          'nu',
-          'zellij',
-        }
-        inactive_pane_hsb         = {
-          saturation = 1.0, brightness = 0.8
-        },
-        check_for_updates = false,
-        window_background_opacity = 1
-      }
+            local wezterm = require 'wezterm'
+            local config = wezterm.config_builder()
+            local act = wezterm.action
+
+            config.default_prog = {'zsh'}
+            config.cell_width = 0.85
+
+            -- Performance
+            --------------
+            config.front_end = "OpenGL"
+            config.enable_wayland = true
+            config.scrollback_lines = 3500
+
+            -- Fonts
+            --------
+            config.font = wezterm.font_with_fallback({
+              "Hack",
+              "Material Design Icons",
+            })
+            config.initial_rows = 24
+            config.initial_cols = 85
+            config.bold_brightens_ansi_colors = true
+            config.font_rules = {
+              {
+                italic = true,
+                font = wezterm.font("Hack", { italic = false })
+              }
+            }
+            --config.font_antialias = "Subpixel"
+            --config.font_hinting = "VerticalSubpixel"
+            config.font_size = 14.0
+            config.line_height = 1.15
+            config.harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' }
+
+            -- Bling
+            --------
+            config.color_scheme = 'Dracula+'
+            config.default_cursor_style = "BlinkingBlock"
+
+            -- Tabbar
+            --------
+            config.enable_tab_bar = true
+            config.use_fancy_tab_bar = true
+            config.hide_tab_bar_if_only_one_tab = false
+            config.show_tab_index_in_tab_bar = false
+            config.show_new_tab_button_in_tab_bar = true
+
+            -- Miscellaneous
+            ---------------
+            config.window_close_confirmation = 'NeverPrompt'
+            config.skip_close_confirmation_for_processes_named = {
+              'bash',
+              'sh',
+              'zsh',
+              'fish',
+              'tmux',
+              'nu',
+              'zellij',
+            }
+
+            config.inactive_pane_hsb = {
+              saturation = 1.0, brightness = 0.8
+            }
+            config.check_for_updates = false
+            config.window_background_opacity = 0.4
+            config.kde_window_background_blur = true
+
+            config.mouse_bindings = {
+              -- Scrolling up while holding CTRL increases the font size
+              {
+                event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+                mods = 'CTRL',
+                action = act.IncreaseFontSize,
+              },
+
+              -- Scrolling down while holding CTRL decreases the font size
+              {
+                event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+                mods = 'CTRL',
+                action = act.DecreaseFontSize,
+              },
+
+               -- Right click sends "woot" to the terminal
+              {
+                event = { Down = { streak = 1, button = 'Right' } },
+                mods = 'NONE',
+                action = act.SendString 'woot',
+              },
+
+              -- Change the default click behavior so that it only selects
+              -- text and doesn't open hyperlinks
+              {
+                event = { Up = { streak = 1, button = 'Left' } },
+                mods = 'NONE',
+                action = act.CompleteSelection 'ClipboardAndPrimarySelection',
+              },
+
+              -- and make CTRL-Click open hyperlinks
+              {
+                event = { Up = { streak = 1, button = 'Left' } },
+                mods = 'CTRL',
+                action = act.OpenLinkAtMouseCursor,
+              },
+            }
+
+            return config
     '';
   };
 }
