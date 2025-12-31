49bc10be4be9c561c0e9d0859b5ae227c59be9b4
4rmcyt <4rmcyt@gmail.com>
2025-12-31 14:32:07 -0700

    removing nginx

--- modules/networking/default.nix
+++ modules/networking/default.nix
@@ -2,7 +2,6 @@
   imports = [
     ./cloudflared
     ./dnssec
-    ./nginx
     ./tailscale
     ./traefik
     ./wireguard
--- modules/networking/traefik/default.nix
+++ modules/networking/traefik/default.nix
@@ -7,6 +7,42 @@
   inherit (config.my.defaults) domain;
   inherit (config.my.security.ssl) certPath keyPath;
 in {
+  # SSL certificate paths configuration
+  my.security.ssl = {
+    certPath = "/var/lib/acme/${domain}/fullchain.pem";
+    keyPath = "/var/lib/acme/${domain}/key.pem";
+  };
+
+  # ACME/Let's Encrypt configuration
+  sops.secrets.cloudflare_acme_credentials = {
+    sopsFile = ../../../secrets/cloudflare_acme_credentials.env;
+    owner = "acme";
+    group = "acme";
+    mode = "0400";
+    format = "dotenv";
+  };
+
+  users.users.acme = {
+    isSystemUser = true;
+    group = "acme";
+  };
+  users.groups.acme = {};
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = config.my.defaults.email;
+
+    certs.${domain} = {
+      domain = "*.${domain}";
+      extraDomainNames = [domain];
+      dnsProvider = "cloudflare";
+      credentialsFile = config.sops.secrets.cloudflare_acme_credentials.path;
+      keyType = "ec256";
+      group = "traefik";
+      postRun = "systemctl reload traefik.service";
+    };
+  };
+
   options.my.traefik = {
     enable = lib.mkEnableOption "Traefik reverse proxy with Authelia integration";
 
