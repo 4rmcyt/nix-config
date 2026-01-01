a24039997c07e76ff589a4935d3dcbcbd9e167be
4rmcyt <4rmcyt@gmail.com>
2025-12-31 18:01:17 -0700

    dsds

--- modules/security/default.nix
+++ modules/security/default.nix
@@ -1,8 +1,7 @@
 {...}: {
   imports = [
-    # ./keycloak
     ./fail2ban
-    # ./lldap      # Uncomment when SOPS secrets are configured
-    # ./authelia   # Uncomment when SOPS secrets are configured (requires lldap, mysql, redis)
+    ./lldap      # Uncomment when SOPS secrets are configured
+    ./authelia   # Uncomment when SOPS secrets are configured (requires lldap, mysql, redis)
   ];
 }
