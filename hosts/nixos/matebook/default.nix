4629e4869dbb77aaf2f43a65ddc80c9d811d72be
4rmcyt <4rmcyt@gmail.com>
2026-01-01 19:04:40 -0700

    jkgjk

--- hosts/nixos/matebook/default.nix
+++ hosts/nixos/matebook/default.nix
@@ -94,8 +94,7 @@
       # Evaluation performance
       eval-cache = true; # Cache evaluation results
 
-      substituters = [
-        "https://cache.nixos.org"
+      extra-substituters = [
         "https://4rmcyt-matebook.cachix.org?priority=1"
         "https://cache.lix.systems?priority=2"
         "https://nix-community.cachix.org?priority=3"
@@ -107,8 +106,7 @@
         "https://nixpkgs-unfree.cachix.org?priority=9"
       ];
 
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      extra-trusted-public-keys = [
         "4rmcyt-matebook.cachix.org-1:OG8MqlfrDlyperVhYk2+va8Cwo/vE6tG/VbTlvq4I0I="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
