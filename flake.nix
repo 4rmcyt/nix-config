5a3cfb8f45da33df77878f5e47f7ddbc1ecf6b2f
4rmcyt <4rmcyt@gmail.com>
2026-01-03 15:54:16 -0700

    dsd

--- flake.nix
+++ flake.nix
@@ -173,28 +173,24 @@
     userName = "zeev";
     system = "x86_64-linux";
     commonSubstituters = [
+      "https://nix-community.cachix.org?priority=0"
+      "https://cache.nixos.org?priority=0"
       "https://4rmcyt-all.cachix.org?priority=1"
       "https://cache.lix.systems?priority=1"
-      "https://nix-community.cachix.org?priority=1"
-      "https://cache.nixos.org?priority=1"
-      "https://attic.xuyh0120.win/lantian?priority=3"
       "https://cache.flox.dev?priority=4"
       "https://nix-gaming.cachix.org?priority=5"
-      "https://devenv.cachix.org?priority=7"
-      "https://nixpkgs-unfree.cachix.org?priority=8"
+      "https://devenv.cachix.org?priority=6"
+      "https://nixpkgs-unfree.cachix.org?priority=7"
     ];
 
     commonTrustedPublicKeys = [
       "4rmcyt-all.cachix.org-1:DCOfHNuSgUNpHS/BwN8zz6zxw4D6izI3VXBDf/vucDc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
       "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
-      "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nqlt4="
       "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
-      "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
 
