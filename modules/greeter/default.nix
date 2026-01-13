b6056b423c77c89de3dbad4d0e95386700bad359
4rmcyt <4rmcyt@gmail.com>
2026-01-13 12:12:04 -0700

    Update dms-greeter script: set XDG_CONFIG_DIRS and simplify command

--- modules/greeter/default.nix
+++ modules/greeter/default.nix
@@ -25,7 +25,8 @@ in {
           command = let
             greeterScript = pkgs.writeShellScript "dms-greeter-mangowc" ''
               export PATH=${lib.makeBinPath [quickshell pkgs.mangowc dmsShell]}:$PATH
-              exec ${dmsShell}/share/quickshell/dms/Modules/Greetd/assets/dms-greeter --command mangowc -C /etc/greetd/mangowc.conf -p ${dmsShell}/share/quickshell/dms --cache-dir /var/lib/dms-greeter
+              export XDG_CONFIG_DIRS=/etc/xdg
+              exec ${dmsShell}/share/quickshell/dms/Modules/Greetd/assets/dms-greeter --command mangowc -C /etc/greetd/mangowc.conf --cache-dir /var/lib/dms-greeter
             '';
           in toString greeterScript;
           user = "greeter";
@@ -38,6 +39,9 @@ in {
       # MangoWC greeter configuration
       # Managed by NixOS configuration
     '';
+
+    # Symlink DMS config so dms-greeter can find it
+    environment.etc."xdg/quickshell/dms-greeter".source = "${dmsShell}/share/quickshell/dms";
 
     # Setup cache directory and config sync for mangowc greeter
     systemd.tmpfiles.settings."10-dmsgreeter-mangowc" = {
