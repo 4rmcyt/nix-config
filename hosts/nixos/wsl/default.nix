5752eadeedeb8cdbc0a6af290139504c007b5b5b
4rmcyt <4rmcyt@gmail.com>
2026-01-15 00:20:27 -0700

    dfdf

--- hosts/nixos/wsl/default.nix
+++ hosts/nixos/wsl/default.nix
@@ -116,27 +116,6 @@
     builders-use-substitutes = true; # Allow builders to use substitutes
     require-sigs = true; # Security: require signatures
 
-    substituters = [
-      "https://cache.nixos.org"
-      "https://4rmcyt-all.cachix.org"
-      "https://nix-community.cachix.org"
-      "https://numtide.cachix.org"
-      "https://cache.flox.dev"
-      "https://cuda-maintainers.cachix.org"
-      "https://cache.lix.systems"
-    ];
-
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "4rmcyt-all.cachix.org-1:DCOfHNuSgUNpHS/BwN8zz6zxw4D6izI3VXBDf/vucDc="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-      "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
-      "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
-      "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
-    ];
-
     trusted-users = [
       "root"
       "@wheel"
