29e542f7d3ea137462c29dcc29bccdacd55ef075
4rmcyt <4rmcyt@gmail.com>
2026-01-21 12:11:53 -0700

    dfdf

--- modules/GUI/pycharm/default.nix
+++ modules/GUI/pycharm/default.nix
@@ -1,6 +1,12 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   home.packages = with pkgs; [
-    jetbrains.pycharm-oss
+    (jetbrains.pycharm-oss.override {
+      vmopts = ''
+        -Dawt.toolkit.name=WLToolkit
+        -Dsun.java2d.uiScale=2
+      '';
+    })
     codeium # Language server for Codeium plugin
   ];
 }
