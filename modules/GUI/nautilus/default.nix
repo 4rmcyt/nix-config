efa7c4c3f87483b04c57d0ad085fe5eaef8cbbb3
4rmcyt <4rmcyt@gmail.com>
2026-03-08 12:39:27 -0600

    refactor(nautilus): simplify config and remove templates setup
    - Remove systemd.tmpfiles rules for Templates directory
    - Remove `show-create-link` from dconf nautilus preferences
    - Flatten dconf settings structure using attribute dot notation
    - Remove inline comments for cleaner configuration

--- modules/GUI/nautilus/default.nix
+++ modules/GUI/nautilus/default.nix
@@ -1,36 +1,30 @@
-{pkgs, ...}: {
-  # Open any terminal from nautilus context menu
+{
+  pkgs,
+  config,
+  ...
+}: {
   programs.nautilus-open-any-terminal = {
     enable = true;
     terminal = "kitty";
   };
 
-  # Quick file preview with Space
   services.gnome.sushi.enable = true;
-
-  # Virtual filesystem (NFS, MTP, trash, etc.)
   services.gvfs.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    nautilus
-    gnome-autoar # archive support
-  ];
-
-  # "New Document" context menu: requires a Templates dir with at least one file
-  systemd.tmpfiles.rules = [
-    "d /home/zeev/Templates 755 zeev users -"
-    "f /home/zeev/Templates/New\\ Document.txt 644 zeev users -"
-  ];
-
-  # Permanent delete (bypass trash) + other nautilus tweaks via dconf
   programs.dconf.profiles.user.databases = [
     {
-      settings = {
-        "org/gnome/nautilus/preferences" = {
-          show-delete-permanently = true;
-          show-create-link = true;
-        };
+      settings."org/gnome/nautilus/preferences" = {
+        show-delete-permanently = true;
       };
     }
   ];
+
+  environment.systemPackages = with pkgs; [
+    nautilus
+    gnome-autoar
+  ];
+
+  home-manager.users.${config.my.defaults.user}.xdg.configFile."gtk-3.0/bookmarks".text = ''
+    nfs://homeserver/data/media Media
+  '';
 }
