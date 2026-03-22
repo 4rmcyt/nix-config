7d733c27acce98004353b61b80c3b4d18f8b8e53
4rmcyt <4rmcyt@gmail.com>
2026-03-22 09:18:33 -0600

    chore: update flake inputs to latest revisions
    Updates multiple flake inputs to their latest versions:
    - firefox-addons (rycee/nur-expressions)
    - home-manager (nix-community)
    - nix-index-database (nix-community)
    - nix-vscode-extensions (nix-community)
    - rust-overlay (oxalica)
    - sops-nix (Mic92)
    - stylix (danth)
    - zed (zed-industries)

--- flake.nix
+++ flake.nix
@@ -111,7 +111,7 @@
       url = "github:AvengeMedia/danksearch";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    dms.url = "github:AvengeMedia/DankMaterialShell/stable";
+    dms.url = "github:AvengeMedia/DankMaterialShell/beta";
     niri-flake = {
       url = "github:sodiboo/niri-flake";
       inputs.nixpkgs.follows = "nixpkgs";
