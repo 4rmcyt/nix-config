3935966b660c761369c4f81772ff3b22eb108b77
4rmcyt <4rmcyt@gmail.com>
2026-03-25 15:52:44 -0600

    feat(desktop): add uefitool packages and fix formatting
    - Add `uefitool` and `uefitoolPackages.old-engine` to home packages
    - Reformat Python `withPackages` block for consistent indentation
    - Reformat `texlive.combine` inherit statement to single line
    - Fix list bracket spacing in dconf `autoconnect` and `uris` settings
    - Minor formatting cleanup for opening brace and closing bracket style

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -2,7 +2,8 @@
   pkgs,
   lib,
   ...
-}: {
+}:
+{
   imports = [
     ../../modules/GUI/terminal
     ../../modules/GUI/IDE
@@ -83,19 +84,17 @@
       proton-pass
       proton-pass-cli
       (python3.withPackages (
-        ps:
-          with ps; [
-            pip
-            pydantic
-            requests
-            black
-            pylint
-            python-lsp-server
-          ]
+        ps: with ps; [
+          pip
+          pydantic
+          requests
+          black
+          pylint
+          python-lsp-server
+        ]
       ))
       (pkgs.texlive.combine {
-        inherit
-          (pkgs.texlive)
+        inherit (pkgs.texlive)
           scheme-medium
           moderncv
           lastpage
@@ -106,6 +105,8 @@
           xcolor
           ;
       })
+      uefitool
+      uefitoolPackages.old-engine
     ];
 
     sessionVariables = {
@@ -130,8 +131,8 @@
 
   dconf.settings = {
     "org/virt-manager/virt-manager/connections" = {
-      autoconnect = ["qemu:///system"];
-      uris = ["qemu:///system"];
+      autoconnect = [ "qemu:///system" ];
+      uris = [ "qemu:///system" ];
     };
   };
 }
