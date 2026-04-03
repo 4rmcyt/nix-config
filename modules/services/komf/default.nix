c1a3709dc9f455b4ec9c290515435547d05dd4ea
4rmcyt <4rmcyt@gmail.com>
2026-04-03 15:33:28 -0600

    sdsds

--- modules/services/komf/default.nix
+++ modules/services/komf/default.nix
@@ -9,13 +9,15 @@
     image = "sndxr/komf:latest";
     extraOptions = [
       "--network=host"
+      "--user=0:${toString config.users.groups.media.gid}"
       "--env=JAVA_TOOL_OPTIONS=-XX:+UnlockExperimentalVMOptions -XX:+UseShenandoahGC -XX:ShenandoahGCHeuristics=compact -XX:ShenandoahGuaranteedGCInterval=3600000 -XX:TrimNativeHeapInterval=3600000"
     ];
     volumes = [
       "/var/lib/komf:/config"
+      "/data/media/manga:/data/media/manga"
+      "/data/media/comics:/data/media/comics"
     ];
   };
-
   systemd.tmpfiles.rules = [
     "d /var/lib/komf 0750 root root -"
   ];
