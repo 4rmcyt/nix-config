ef06311e217835da2c8518b7b23e03fdd5564ece
4rmcyt <4rmcyt@gmail.com>
2026-04-07 14:19:23 -0600

    dsds

--- overlays/claw-code.nix
+++ overlays/claw-code.nix
@@ -7,12 +7,12 @@ final: _prev: {
       owner = "ultraworkers";
       repo = "claw-code";
       rev = "aee5263aefcb460f4b051baef7008400a5a0db2b";
-      hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+      hash = "sha256-NErbWplKiWoG4LWJmqa+G2JnCoI9fDMCk0LneSq+Ntw=";
     };
 
     sourceRoot = "source/rust";
 
-    cargoHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    cargoHash = "sha256-P8QqUM1s/fNv7Fb4dmpJWDfTNumgUu1Cdiln8ybSDUU=";
 
     buildInputs = final.lib.optionals final.stdenv.isLinux [
       final.openssl
