10ae7f1cbf933d8cb24524aac6edfcd316e30355
4rmcyt <4rmcyt@gmail.com>
2026-04-14 14:01:28 -0600

    chore(nixarr): adjust ExecStart override for recyclarr service
    chore(nixarr): adjust ExecStart override for recyclarr service

--- modules/services/nixarr/default.nix
+++ modules/services/nixarr/default.nix
@@ -177,7 +177,7 @@ in {
   systemd.services = lib.mkMerge [
     # recyclarr 8.x dropped --app-data; override nixarr's hardcoded ExecStart
     {
-      recyclarr.serviceConfig.ExecStart = lib.mkForce (
+      recyclarr.serviceConfig.ExecStart = lib.mkOverride 0 (
         "${config.nixarr.recyclarr.package}/bin/recyclarr sync --config ${config.nixarr.recyclarr.configFile}"
       );
     }
