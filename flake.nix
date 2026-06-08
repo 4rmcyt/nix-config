c94753fc8dcbeeff2377a045e5d92530ec7d1a9d
4rmcyt <4rmcyt@gmail.com>
2026-06-08 12:39:38 -0600

    chore(dev): remove unused noctalia-qs dependency from flake.nix

--- flake.nix
+++ flake.nix
@@ -95,11 +95,6 @@
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.noctalia-qs.follows = "noctalia-qs";
-    };
-    noctalia-qs = {
-      url = "github:noctalia-dev/noctalia-qs";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
     nur = {
       url = "github:nix-community/NUR";
