23420f5bc923551f1ad1a9012a92c3d822b44d79
4rmcyt <4rmcyt@gmail.com>
2026-06-10 11:52:58 -0600

    chore(config): remove automatic nixos configuration checks
    chore(config): remove automatic nixos configuration checks

--- parts/configurations/nixos.nix
+++ parts/configurations/nixos.nix
@@ -29,15 +29,5 @@
       )
       config.configurations.nixos;
 
-    checks = lib.mkMerge (
-      lib.mapAttrsToList (
-        name: nixos: {
-          ${nixos.config.nixpkgs.hostPlatform.system} = {
-            "configurations/nixos/${name}" = nixos.config.system.build.toplevel;
-          };
-        }
-      )
-      config.flake.nixosConfigurations
-    );
   };
 }
