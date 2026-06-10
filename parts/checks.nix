1c0fb6db121e3ae9c7c479e21253b866c35405f4
4rmcyt <4rmcyt@gmail.com>
2026-06-10 11:37:46 -0600

    chore(deps): remove unused checks configuration from Nix flake
    chore(deps): remove unused checks configuration from Nix flake

--- parts/checks.nix
+++ parts/checks.nix
@@ -1,12 +1,1 @@
-{
-  inputs,
-  config,
-  lib,
-  ...
-}: {
-  perSystem = {system, ...}: {
-    checks = lib.filterAttrs (_: lib.isDerivation) (
-      inputs.deploy-rs.lib.${system}.deployChecks config.flake.deploy
-    );
-  };
-}
+{...}: {}
