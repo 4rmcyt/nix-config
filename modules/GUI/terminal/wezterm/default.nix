02e210b37c4aabf017b08397a673897923c527a4
4rmcyt <4rmcyt@gmail.com>
2026-06-29 13:45:25 -0600

    refactor(wezterm): update configuration for modern terminal features

--- modules/GUI/terminal/wezterm/default.nix
+++ modules/GUI/terminal/wezterm/default.nix
@@ -7,100 +7,90 @@
       local config = wezterm.config_builder()
       local act = wezterm.action
 
-      config.default_prog = {'zsh'}
-      config.cell_width = 0.85
-
       -- Performance
-      --------------
-      config.front_end = "OpenGL"
+      config.front_end = "WebGpu"
       config.enable_wayland = true
-      config.scrollback_lines = 3500
+      config.scrollback_lines = 50000
 
       -- Fonts
-      --------
       config.font = wezterm.font_with_fallback({
-         "MesloLGS Nerd Font",
-         "Symbols Nerd Font",
-         "Material Design Icons",
+        "MesloLGS Nerd Font",
+        "Symbols Nerd Font",
       })
-      config.initial_rows = 24
-      config.initial_cols = 85
-      config.bold_brightens_ansi_colors = true
-      config.font_rules = {
-        {
-          font = wezterm.font("MesloLGS Nerd Font", { italic = false })
-        }
-      }
-      --config.font_antialias = "Subpixel"
-      --config.font_hinting = "VerticalSubpixel"
       config.font_size = 14.0
       config.line_height = 1.15
       config.harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' }
+      config.bold_brightens_ansi_colors = true
 
-      -- Bling
-      --------
-      config.color_scheme = 'Dracula (Official)'
+      -- Colors (catppuccin mocha)
+      config.color_scheme = 'Catppuccin Mocha'
+
+      -- Window
+      config.window_background_opacity = 0.4
+      config.window_decorations = "NONE"
+      config.window_padding = { left = 10, right = 10, top = 10, bottom = 10 }
+
+      -- Cursor
       config.default_cursor_style = "BlinkingBlock"
 
-      -- Tabbar
-      --------
+      -- Tabs
       config.enable_tab_bar = true
-      config.use_fancy_tab_bar = true
-      config.hide_tab_bar_if_only_one_tab = false
+      config.use_fancy_tab_bar = false
+      config.hide_tab_bar_if_only_one_tab = true
       config.show_tab_index_in_tab_bar = false
-      config.show_new_tab_button_in_tab_bar = true
+      config.show_new_tab_button_in_tab_bar = false
+      config.tab_bar_at_bottom = true
 
-      -- Miscellaneous
-      ---------------
+      -- Misc
       config.window_close_confirmation = 'NeverPrompt'
-      config.skip_close_confirmation_for_processes_named = {
-        'bash',
-        'sh',
-        'zsh',
-        'fish',
-        'tmux',
-        'nu',
-        'zellij',
-      }
+      config.audible_bell = "Disabled"
 
       config.inactive_pane_hsb = {
-        saturation = 1.0, brightness = 0.8
+        saturation = 1.0,
+        brightness = 0.8,
       }
-      config.check_for_updates = false
-      config.window_background_opacity = 0.4
-      config.kde_window_background_blur = true
 
+      -- Keys
+      config.keys = {
+        { key = 'c', mods = 'CTRL|SHIFT', action = act.CopyTo 'Clipboard' },
+        { key = 'v', mods = 'CTRL|SHIFT', action = act.PasteFrom 'Clipboard' },
+        { key = 't', mods = 'CTRL|SHIFT', action = act.SpawnTab 'CurrentPaneDomain' },
+        { key = 'w', mods = 'CTRL|SHIFT', action = act.CloseCurrentTab { confirm = false } },
+        { key = 'Tab', mods = 'CTRL', action = act.ActivateTabRelative(1) },
+        { key = 'Tab', mods = 'CTRL|SHIFT', action = act.ActivateTabRelative(-1) },
+        { key = '1', mods = 'ALT', action = act.ActivateTab(0) },
+        { key = '2', mods = 'ALT', action = act.ActivateTab(1) },
+        { key = '3', mods = 'ALT', action = act.ActivateTab(2) },
+        { key = '4', mods = 'ALT', action = act.ActivateTab(3) },
+        { key = '5', mods = 'ALT', action = act.ActivateTab(4) },
+        { key = '+', mods = 'CTRL', action = act.IncreaseFontSize },
+        { key = '-', mods = 'CTRL', action = act.DecreaseFontSize },
+        { key = '0', mods = 'CTRL', action = act.ResetFontSize },
+        { key = 'F11', mods = 'NONE', action = act.ToggleFullScreen },
+      }
+
+      -- Mouse
       config.mouse_bindings = {
-        -- Scrolling up while holding CTRL increases the font size
         {
           event = { Down = { streak = 1, button = { WheelUp = 1 } } },
           mods = 'CTRL',
           action = act.IncreaseFontSize,
         },
-
-        -- Scrolling down while holding CTRL decreases the font size
         {
           event = { Down = { streak = 1, button = { WheelDown = 1 } } },
           mods = 'CTRL',
           action = act.DecreaseFontSize,
         },
-
-         -- Right click pastes from clipboard
         {
           event = { Down = { streak = 1, button = 'Right' } },
           mods = 'NONE',
           action = act.PasteFrom 'Clipboard',
         },
-
-        -- Change the default click behavior so that it only selects
-        -- text and doesn't open hyperlinks
         {
           event = { Up = { streak = 1, button = 'Left' } },
           mods = 'NONE',
           action = act.CompleteSelection 'ClipboardAndPrimarySelection',
         },
-
-        -- and make CTRL-Click open hyperlinks
         {
           event = { Up = { streak = 1, button = 'Left' } },
           mods = 'CTRL',
