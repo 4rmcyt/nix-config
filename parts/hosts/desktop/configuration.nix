4192742d76a9b0cc69d987a69a6bf214153562fe
4rmcyt <4rmcyt@gmail.com>
2026-06-30 12:31:37 -0600

    refactor(desktop): restructure home-manager user configuration

--- parts/hosts/desktop/configuration.nix
+++ parts/hosts/desktop/configuration.nix
@@ -48,12 +48,14 @@ in {
     facter.reportPath = ../../../hosts/nixos/desktop/facter.json;
 
     # Host-specific HM imports
-    home-manager.users.${owner.username}.nixpkgs.config.permittedInsecurePackages = ["pnpm-10.29.2"];
-    home-manager.users.${owner.username}.imports = [
-      ../../../home/desktop
-      # inputs.stylix.homeModules.stylix
-      # inputs.pam-shim.homeModules.default
-      inputs.noctalia.homeModules.default
-    ];
+    home-manager.users.${owner.username} = {
+      nixpkgs.config.permittedInsecurePackages = ["pnpm-10.29.2"];
+      imports = [
+        ../../../home/desktop
+        # inputs.stylix.homeModules.stylix
+        # inputs.pam-shim.homeModules.default
+        inputs.noctalia.homeModules.default
+      ];
+    };
   };
 }
