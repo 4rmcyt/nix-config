7b5673efea39ff7700138ab753f179f94b4d883d
4rmcyt <4rmcyt@gmail.com>
2026-07-17 16:06:30 -0600

    refactor(firefox): remove hardcoded browser extensions from UI definition

--- modules/GUI/firefox/ui.nix
+++ modules/GUI/firefox/ui.nix
@@ -11,6 +11,11 @@ _: {
         "stop-reload-button"
         "urlbar-container"
         "downloads-button"
+        "addon_darkreader_org-browser-action"
+        "ublock0_raymondhill_net-browser-action"
+        "_a4c4eda4-fb84-4a84-b4a1-f7c1cbf2a1ad_-browser-action"
+        "_762f9885-5a13-4abd-9c77-433dcd38b8fd_-browser-action"
+        "indie-wiki-buddy_einaregilsson_com-browser-action"
         "unified-extensions-button"
       ];
       TabsToolbar = [
@@ -24,18 +29,7 @@ _: {
         "restore-button"
         "close-button"
       ];
-      unified-extensions-area = [
-        "ublock0_raymondhill_net-browser-action"
-        "gdpr_cavi_au_dk-browser-action"
-        "redirector_einaregilsson_com-browser-action"
-        "_5183707f-8a46-4092-8c1f-e4515bcebbad_-browser-action"
-        "_b0a674f9-f848-9cfd-0feb-583d211308b0_-browser-action"
-        "jid0-3guet1r69sqnsrca5p8kx9ezc3u_jetpack-browser-action"
-        "_74145f27-f039-47ce-a470-a662b129930a_-browser-action"
-        "_762f9885-5a13-4abd-9c77-433dcd38b8fd_-browser-action"
-        "_c2c003ee-bd69-42a2-b0e9-6f34222cb046_-browser-action"
-        "_cb31ec5d-c49a-4e5a-b240-16c767444f62_-browser-action"
-      ];
+      unified-extensions-area = [];
     };
     currentVersion = 20;
     newElementCount = 3;
