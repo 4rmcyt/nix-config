61d2cf1cef4c382673474e1c0619e7a5e180e454
4rmcyt <4rmcyt@gmail.com>
2026-07-18 12:57:11 -0600

    chore(deps): update Hyprland dependency specification in flakes

--- flake.nix
+++ flake.nix
@@ -88,7 +88,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland = {
-      url = "github:hyprwm/Hyprland?submodules=1";
+      url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     niri-flake = {
