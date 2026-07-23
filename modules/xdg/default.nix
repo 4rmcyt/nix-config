1609d08f4d4749bf701f2f9b5c321b4eaf97dd3a
4rmcyt <4rmcyt@gmail.com>
2026-07-23 10:03:37 -0600

    feat(xdg): add hyprland specific portal backend definitions

--- modules/xdg/default.nix
+++ modules/xdg/default.nix
@@ -16,6 +16,18 @@
         "org.freedesktop.impl.portal.Notification" = ["gtk"];
         "org.freedesktop.impl.portal.Secret" = ["gnome"];
       };
+      hyprland = {
+        default = [
+          "hyprland"
+          "gtk"
+        ];
+        "org.freedesktop.impl.portal.Access" = ["gtk"];
+        "org.freedesktop.impl.portal.FileChooser" = ["gtk"];
+        "org.freedesktop.impl.portal.Notification" = ["gtk"];
+        "org.freedesktop.impl.portal.Secret" = ["gnome"];
+        "org.freedesktop.impl.portal.Screenshot" = ["hyprland"];
+        "org.freedesktop.impl.portal.ScreenCast" = ["hyprland"];
+      };
     };
   };
 }
