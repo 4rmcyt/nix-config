83b26271a804ae907cc22cd343c04b6640505555
4rmcyt <4rmcyt@gmail.com>
2026-07-24 14:02:24 -0600

    chore(caddy): update Cloudflare plugin hash

--- modules/networking/caddy/default.nix
+++ modules/networking/caddy/default.nix
@@ -26,7 +26,7 @@ in {
       enable = true;
       package = pkgs.caddy.withPlugins {
         plugins = ["github.com/caddy-dns/cloudflare@v0.2.4"];
-        hash = "sha256-hEHgAG0F0ozHRAPuxEqLyTATBrE+pajeXDiSNwniorg=";
+        hash = "sha256-7GoH8YLCoPmPExQxoga2FHB58zQDoZVf1BBwkVi0SsQ=";
       };
       globalConfig = ''
         email ${email}
