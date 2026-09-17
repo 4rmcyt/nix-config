234d14214039a6b4fd058edcc06392089722a4ea
4rmcyt <4rmcyt@gmail.com>
2026-09-17 12:48:08 -0600

    chore(tui): set default TTY environment variables

--- modules/TUI/tty.nix
+++ modules/TUI/tty.nix
@@ -11,6 +11,11 @@
       font-size = 16;
       hwaccel = true;
       gpus = "aux";
+      xkb-layout = "us";
+      mode = "1920x1080";
+      sb-size = 50000;
+      mouse = true;
+      palette = "base16-dark";
     };
   };
 }
