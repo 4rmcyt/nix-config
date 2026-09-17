67c5204a6fbc5c08a1133f9d948dcbfb292f02bb
4rmcyt <4rmcyt@gmail.com>
2026-09-17 13:03:56 -0600

    refactor(homeserver): remove hardcoded nixarr patch for jellyfin versioning

--- parts/hosts/homeserver/configuration.nix
+++ parts/hosts/homeserver/configuration.nix
@@ -9,7 +9,7 @@
   nixosBareMetal = config.modules.nixos.bareMetal;
   nixosNixMineral = config.modules.nixos.nixMineral;
 in {
-  configurations.nixos.homeserver.module = {pkgs, ...}: {
+  configurations.nixos.homeserver.module = {...}: {
     imports = [
       nixosBase
       nixosHm
@@ -35,18 +35,6 @@ in {
           ;
       })
     ];
-
-    # nixarr's jellyfin-version -> openapi hash map lags upstream; patched in until it catches up.
-    nixarr.nixarr-py.package = pkgs.callPackage
-      (pkgs.runCommand "nixarr-py-patched" {} ''
-        cp -r ${inputs.nixarr}/nixarr/lib/nixarr-py $out
-        chmod -R u+w $out
-        substituteInPlace $out/python-deps.nix \
-          --replace-fail \
-            '"12.0" = "sha256-hu9rbOp+R0tbsjT0Tl639uj0WM5tfYT6uZ6NH0p1zjk=";' \
-            '"12.0" = "sha256-hu9rbOp+R0tbsjT0Tl639uj0WM5tfYT6uZ6NH0p1zjk="; "12.1" = "sha256-bMc4br+KUqcmSWn6Y6eG3HRunjevVzur+LNbz6G1Ah0=";'
-      '')
-      {jellyfin = pkgs.jellyfin;};
 
     home-manager.users.${owner.username}.imports = [
       ../../../home/homeserver
